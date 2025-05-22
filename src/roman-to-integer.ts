01511a359305e45c843c3bb20a7e32271d1543f2
nao-United92 <naoyauver@icloud.com>
2025-05-22 23:47:43 +0900

    SampleCode to roman-to-integer

--- src/roman-to-integer.ts
+++ src/roman-to-integer.ts
@@ -23,3 +23,32 @@ function romanToInt(s: string): number {
   }
   return result;
 }
+
+// Sample Code
+// function romanToInt(s: string): number {
+
+
+//     enum RomanValueEnum {
+//         "I" = 1,
+//         "V" = 5,
+//         "X" = 10,
+//         "L" = 50,
+//         "C" = 100,
+//         "D" = 500,
+//         "M" = 1000
+//     }
+
+//     let intergerNumber = 0
+//     for (let i = 0; i < s.length; i++) {
+//         if (RomanValueEnum[s[i]] < RomanValueEnum[s[i + 1]]) {
+//             intergerNumber -= RomanValueEnum[s[i]]
+//         }
+//         else {
+//             intergerNumber += RomanValueEnum[s[i]];
+//         }
+//     }
+
+
+//     return intergerNumber
+
+// };
