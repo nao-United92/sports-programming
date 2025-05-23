8dac5cdd544a4c5a49f6534a659a9abcdbbe0ce0
nao-United92 <naoyauver@icloud.com>
2025-05-24 00:00:50 +0900

    fix valid-parentheses 5

--- src/valid-parentheses.ts
+++ src/valid-parentheses.ts
@@ -79,7 +79,7 @@ function isValid(s: string): boolean {
   const mergeValue = [...leftValue, ...rightValue];
   for (let j = 0; j < mergeValue.length; j++) {
     if (
-      leftBrackets.indexOf(leftValue[j]) ===
+      leftBrackets.indexOf(leftValue[j]) !==
       rightBrackets.indexOf(rightValue[j])
     )
       return false;
