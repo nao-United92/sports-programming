9a170dca0bc010ad7ec16192fd1561cd55ddad15
nao-United92 <naoyauver@icloud.com>
2025-06-29 01:32:02 +0900

    verify Date

---
+++ src/other/date.js
@@ -0,0 +1,10 @@
+let date = new Date();
+let dateMs = new Date(ミリ秒);
+let dateYmd = new Date("YYYY[-MM-DDTHH:mm:ss.sssTZD]");
+let dateJst = new Date(年, 月[,日, 時, 分, 秒, ミリ秒])
+let now = new Date();
+console.log(date);
+console.log(dateMs);
+console.log(dateYmd);
+console.log(dateJst);
+console.log(now);
