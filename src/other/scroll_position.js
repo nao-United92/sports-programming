8b0c4521c6d02cda4a77f3f2d193cc9554fbbb6f
nao-United92 <naoyauver@icloud.com>
2025-07-01 01:47:17 +0900

    write comment

--- src/other/scroll_position.js
+++ src/other/scroll_position.js
@@ -1,13 +1,15 @@
-let position = localStorage.getItem('position');
-position = JSON.parse(position);
+let position = localStorage.getItem('position'); // localStorageからpositionを取得
+position = JSON.parse(position); // 取得値をオブジェクトへ変換
 
 if (position === null) {
+  // positionがnullの場合、初期値をセット
   position = { x: 0, y: 0 };
 } else {
-  window.scroll({ top: position.y, left: position.x, behavior: 'smooth' });
+  window.scroll({ top: position.y, left: position.x, behavior: 'smooth' }); // 取得値までスクロール
 }
 
-const intevalId = setInterval(() => {
+const intervalId = setInterval(() => {
+  // スクロール位置を1秒ごとに監視・localStorageに保存
   position.x = window.scrollX;
   position.y = window.scrollY;
   localStorage.setItem('position', JSON.stringify(position));
