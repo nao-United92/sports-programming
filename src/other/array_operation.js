91a42d057b53659bea29c97602845400df01ec50
nao-United92 <naoyauver@icloud.com>
2025-07-01 23:57:25 +0900

    add array operation

--- src/other/array_operation.js
+++ src/other/array_operation.js
@@ -9,3 +9,18 @@ fruits.pop();
 console.log(fruits); // ['cherry', 'banana', 'orange']
 fruits.splice(0, 2);
 console.log(fruits); // ['orange']
+const newFruits = fruits.concat(['kiwi', 'melon']);
+console.log(newFruits); // ['orange', 'kiwi', 'melon']
+const slice = fruits.slice(0);
+console.log(slice); // ['orange']
+
+// shallow copy
+const ori = ['1'];
+const copied = ori;
+copied[0] = '2';
+console.log(ori); // ['2']
+// deep copy
+const original = ['1'];
+const deepCopied = original.concat();
+deepCopied[0] = '3';
+console.log(original); // ['1']
