cd2bca3053434eba6ce43c63ea4fec6d7045682c
nao-United92 <naoyauver@icloud.com>
2025-07-04 00:10:28 +0900

    fix spread

--- src/other/spread.js
+++ src/other/spread.js
@@ -2,9 +2,8 @@ function reducer(totalValue, currentValue) {
   return totalValue + currentValue;
 }
 
-function totalPrice(zeiritsu, productsPrice) {
-  return zeiritsu * Math.floor(productsPrice.reduce(reducer));
+function totalPrice(zeiritsu, ...productsPrice) {
+  return Math.floor(((1 * zeiritsu) / 100) * productsPrice.reduce(reducer));
 }
-const productsPrice = [100, 200, 300, 500, 600];
-const result = totalPrice(0.8, ...productsPrice);
+const result = totalPrice(8, 100, 200, 300, 500, 600);
 console.log(result);
