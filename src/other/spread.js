18f1b8595ff8f4545f5e31eceab14feeaf02f097
nao-United92 <naoyauver@icloud.com>
2025-07-04 00:13:36 +0900

    add practice spread

--- src/other/spread.js
+++ src/other/spread.js
@@ -7,3 +7,9 @@ function totalPrice(zeiritsu, ...productsPrice) {
 }
 const result = totalPrice(8, 100, 200, 300, 500, 600);
 console.log(result);
+
+const chuka = ['a', 'b', 'c'];
+const desert = ['1', '2', '3'];
+console.log([...chuka]);
+console.log([...chuka, ...desert]);
+console.log([...chuka, 'Yeah', ...desert]);
