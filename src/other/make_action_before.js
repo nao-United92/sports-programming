a6844a01ddf54168b099ad24087a6a10fa36906e
nao-United92 <naoyauver@icloud.com>
2025-07-06 02:13:31 +0900

    change async and await in makeAction

--- src/other/make_action_before.js
+++ src/other/make_action_before.js
@@ -1,4 +1,4 @@
-function action(actionName, duration) {
+async function action(actionName, duration) {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log(`${actionName}`);
@@ -7,12 +7,12 @@ function action(actionName, duration) {
   });
 }
 
-function makeAction() {
-  action('散歩', 500)
-    .then(() => action('朝食', 200))
-    .then(() => Promise.all([action('昼食', 500), action('おしゃべり', 100)]))
-    .then(() => action('夕食', 600))
-    .then(() => action('趣味', 400));
+async function makeAction() {
+  await action('散歩', 500);
+  await action('朝食', 200);
+  await Promise.all([action('昼食', 500), action('おしゃべり', 100)]);
+  await action('夕食', 600);
+  await action('趣味', 400);
 }
 
 makeAction();
