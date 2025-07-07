404bcb1180dbdc4ebf77f8fe771582851da8a2b6
nao-United92 <naoyauver@icloud.com>
2025-07-08 08:37:32 +0900

    verify getElementBuID・auerySelector

---
+++ src/other/dom_selector.js
@@ -0,0 +1,7 @@
+// getElementByIdのサンプル
+const mainTitle = document.getElementById('main-title');
+console.log('getElementById:', mainTitle.textContent);
+
+// querySelectorのサンプル
+const firstItem = document.querySelector('.item');
+console.log('querySelector:', firstItem.textContent);
