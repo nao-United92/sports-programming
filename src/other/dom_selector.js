858c0f6da03fe2f843839b69441a1ed8376468ae
nao-United92 <naoyauver@icloud.com>
2025-07-08 08:38:38 +0900

    verify getElementsByClassName・querySelectorAll

--- src/other/dom_selector.js
+++ src/other/dom_selector.js
@@ -5,3 +5,15 @@ console.log('getElementById:', mainTitle.textContent);
 // querySelectorのサンプル
 const firstItem = document.querySelector('.item');
 console.log('querySelector:', firstItem.textContent);
+
+// getElementsByClassNameのサンプル
+const items = document.getElementsByClassName('item');
+for (let i = 0; i < items.length; i++) {
+  console.log(`getElementsByClassName[${i}]:`, items[i].textContent);
+}
+
+// querySelectorAllのサンプル
+const allItems = document.querySelectorAll('.item');
+allItems.forEach((item, index) => {
+  console.log(`querySelectorAll[${index}]:`, item.textContent);
+});
