9ddeac955409503db807da97074e8ec057ee54fd
nao-United92 <naoyauver@icloud.com>
2025-08-09 00:10:50 +0900

    feat(other): add flatten utility and tests

--- src/other/flatten-utils.js
+++ src/other/flatten-utils.js
@@ -1,1 +1,6 @@
-export const flatten = (array) => array.flat(Infinity);
+export const flatten = (array) => {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+  return array.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val), []);
+};
--- src/other/flatten-utils.test.js
+++ src/other/flatten-utils.test.js
@@ -2,20 +2,35 @@ import { flatten } from './flatten-utils';
 
 describe('flatten', () => {
   test('should flatten a nested array', () => {
-    const array = [1, [2, [3, [4]], 5]];
-    const flattened = flatten(array);
-    expect(flattened).toEqual([1, 2, 3, 4, 5]);
+    const arr = [1, [2, 3], [4, [5, 6]]];
+    expect(flatten(arr)).toEqual([1, 2, 3, 4, 5, 6]);
   });
 
-  test('should return an empty array if the input array is empty', () => {
-    const array = [];
-    const flattened = flatten(array);
-    expect(flattened).toEqual([]);
+  test('should handle an empty array', () => {
+    const arr = [];
+    expect(flatten(arr)).toEqual([]);
   });
 
-  test('should not modify an already flat array', () => {
-    const array = [1, 2, 3, 4, 5];
-    const flattened = flatten(array);
-    expect(flattened).toEqual([1, 2, 3, 4, 5]);
+  test('should handle a flat array', () => {
+    const arr = [1, 2, 3];
+    expect(flatten(arr)).toEqual([1, 2, 3]);
+  });
+
+  test('should handle an array with empty nested arrays', () => {
+    const arr = [1, [], [2, []]];
+    expect(flatten(arr)).toEqual([1, 2]);
+  });
+
+  test('should handle an array with mixed data types', () => {
+    const arr = [1, 'a', [2, true], { key: 'value' }];
+    expect(flatten(arr)).toEqual([1, 'a', 2, true, { key: 'value' }]);
+  });
+
+  test('should return an empty array if input is not an array', () => {
+    expect(flatten(null)).toEqual([]);
+    expect(flatten(undefined)).toEqual([]);
+    expect(flatten('string')).toEqual([]);
+    expect(flatten(123)).toEqual([]);
+    expect(flatten({})).toEqual([]);
   });
 });
