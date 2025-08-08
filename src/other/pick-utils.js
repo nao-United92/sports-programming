e7bc38aacac424628f8be6bd2d13b3e9e9a699a7
nao-United92 <naoyauver@icloud.com>
2025-08-09 00:01:04 +0900

    feat(other): add pick utility

--- src/other/pick-utils.js
+++ src/other/pick-utils.js
@@ -1,11 +1,9 @@
 export const pick = (obj, keys) => {
-  if (obj === null || typeof obj !== 'object') {
-    return {};
-  }
-  return keys.reduce((acc, key) => {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      acc[key] = obj[key];
+  const result = {};
+  for (const key of keys) {
+    if (key in obj) {
+      result[key] = obj[key];
     }
-    return acc;
-  }, {});
+  }
+  return result;
 };
--- src/other/pick-utils.test.js
+++ src/other/pick-utils.test.js
@@ -1,31 +1,27 @@
-import { pick } from './pick-utils.js';
+import { pick } from './pick-utils';
 
 describe('pick', () => {
   test('should pick specified keys from an object', () => {
     const obj = { a: 1, b: 2, c: 3 };
-    expect(pick(obj, ['a', 'c'])).toEqual({ a: 1, c: 3 });
+    const picked = pick(obj, ['a', 'c']);
+    expect(picked).toEqual({ a: 1, c: 3 });
   });
 
-  test('should return a new object without modifying the original', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    pick(obj, ['a']);
-    expect(obj).toEqual({ a: 1, b: 2, c: 3 });
-  });
-
-  test('should handle non-existent keys gracefully', () => {
+  test('should not include keys that are not in the object', () => {
     const obj = { a: 1, b: 2 };
-    expect(pick(obj, ['c'])).toEqual({});
-  });
-
-  test('should return an empty object if input is null or not an object', () => {
-    expect(pick(null, ['a'])).toEqual({});
-    expect(pick(undefined, ['a'])).toEqual({});
-    expect(pick('string', ['a'])).toEqual({});
-    expect(pick(123, ['a'])).toEqual({});
+    const picked = pick(obj, ['a', 'd']);
+    expect(picked).toEqual({ a: 1 });
   });
 
   test('should return an empty object if no keys are specified', () => {
     const obj = { a: 1, b: 2 };
-    expect(pick(obj, [])).toEqual({});
+    const picked = pick(obj, []);
+    expect(picked).toEqual({});
+  });
+
+  test('should return an empty object if the input object is empty', () => {
+    const obj = {};
+    const picked = pick(obj, ['a', 'b']);
+    expect(picked).toEqual({});
   });
 });
