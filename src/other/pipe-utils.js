78311c4a3738b5baefc5d5330338e90c037e788d
nao-United92 <naoyauver@icloud.com>
2025-08-08 10:26:24 +0900

    feat(pipe): add pipe utility function and its tests
    This commit introduces a new `pipe` utility function that composes multiple functions from left to right. It allows chaining function calls where the output of one function becomes the input of the next.
    Accompanying tests are also added to verify its functionality, including piping multiple functions, handling no functions or a single function, and passing multiple arguments to the leftmost function.

--- src/other/pipe-utils.js
+++ src/other/pipe-utils.js
@@ -1,1 +1,19 @@
-export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+/**
+ * Composes functions from left to right.
+ * The leftmost function can take multiple arguments as it will be the first to be invoked.
+ * The rest of the functions must be unary (accept a single argument).
+ *
+ * @param {...Function} funcs The functions to pipe.
+ * @returns {Function} Returns the new composite function.
+ */
+export function pipe(...funcs) {
+  if (funcs.length === 0) {
+    return (...args) => args.length > 1 ? [...args] : args[0];
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (...args) => b(a(...args)));
+}
--- src/other/pipe-utils.test.js
+++ src/other/pipe-utils.test.js
@@ -1,24 +1,40 @@
-import { pipe } from './pipe-utils.js';
+import { pipe } from './pipe-utils';
 
 describe('pipe', () => {
-  test('should compose functions from left to right', () => {
-    const add1 = (x) => x + 1;
-    const multiply2 = (x) => x * 2;
-    const subtract3 = (x) => x - 3;
+  test('should pipe two functions', () => {
+    const addOne = (x) => x + 1;
+    const multiplyTwo = (x) => x * 2;
+    const addOneThenMultiplyTwo = pipe(addOne, multiplyTwo);
 
-    const pipedFunction = pipe(add1, multiply2, subtract3);
+    expect(addOneThenMultiplyTwo(5)).toBe(12); // (5 + 1) * 2 = 12
+  });
 
-    expect(pipedFunction(5)).toBe((5 + 1) * 2 - 3); // (6 * 2) - 3 = 12 - 3 = 9
+  test('should pipe multiple functions', () => {
+    const addOne = (x) => x + 1;
+    const multiplyTwo = (x) => x * 2;
+    const subtractThree = (x) => x - 3;
+    const pipedFunc = pipe(addOne, multiplyTwo, subtractThree);
+
+    expect(pipedFunc(5)).toBe(9); // ((5 + 1) * 2) - 3 = 9
   });
 
   test('should handle no functions', () => {
-    const pipedFunction = pipe();
-    expect(pipedFunction(10)).toBe(10);
+    const pipedFunc = pipe();
+    expect(pipedFunc(5)).toBe(5);
+    expect(pipedFunc(1, 2, 3)).toEqual([1, 2, 3]);
   });
 
-  test('should handle a single function', () => {
-    const add5 = (x) => x + 5;
-    const pipedFunction = pipe(add5);
-    expect(pipedFunction(10)).toBe(15);
+  test('should handle one function', () => {
+    const addOne = (x) => x + 1;
+    const pipedFunc = pipe(addOne);
+    expect(pipedFunc(5)).toBe(6);
+  });
+
+  test('should pass multiple arguments to the leftmost function', () => {
+    const sum = (a, b) => a + b;
+    const multiplyTwo = (x) => x * 2;
+    const pipedFunc = pipe(sum, multiplyTwo);
+
+    expect(pipedFunc(2, 3)).toBe(10); // (2 + 3) * 2 = 10
   });
 });
