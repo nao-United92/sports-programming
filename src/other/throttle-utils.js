c1022bd3a0b486d2614762b0492fbb1b27318913
nao-United92 <naoyauver@icloud.com>
2025-08-10 23:57:22 +0900

    feat(other): add throttle utility and tests

--- src/other/throttle-utils.js
+++ src/other/throttle-utils.js
@@ -1,11 +1,22 @@
 export const throttle = (func, limit) => {
-  let inThrottle = false;
+  let inThrottle;
+  let lastResult;
+  let lastRan;
+
   return function(...args) {
     const context = this;
     if (!inThrottle) {
       func.apply(context, args);
+      lastRan = Date.now();
       inThrottle = true;
-      setTimeout(() => (inThrottle = false), limit);
+    } else {
+      clearTimeout(inThrottle);
+      inThrottle = setTimeout(function() {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, Math.max(limit - (Date.now() - lastRan), 0));
     }
   };
 };
