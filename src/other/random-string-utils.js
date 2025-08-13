a11a7b166a53a883acc3798a1e9e6f1675c38643
nao-United92 <naoyauver@icloud.com>
2025-08-13 23:50:20 +0900

    feat(utility): Add generateRandomString utility and tests

--- src/other/random-string-utils.js
+++ src/other/random-string-utils.js
@@ -1,9 +1,19 @@
-export const generateRandomString = (length) => {
+/**
+ * Generates a random string of a specified length.
+ *
+ * @param {number} length The length of the string to generate.
+ * @param {string} [characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'] The characters to use for generating the string.
+ * @returns {string} The generated random string.
+ */
+function generateRandomString(length, characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') {
   let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
+}
+
+module.exports = {
+  generateRandomString
 };
--- src/other/random-string-utils.test.js
+++ src/other/random-string-utils.test.js
@@ -1,14 +1,36 @@
-import { generateRandomString } from './random-string-utils.js';
+const { generateRandomString } = require('./random-string-utils');
 
 describe('generateRandomString', () => {
-  test('should generate a random string of the specified length', () => {
-    const randomString = generateRandomString(10);
-    expect(randomString).toHaveLength(10);
+  test('should generate a string of the specified length', () => {
+    const length = 10;
+    const randomString = generateRandomString(length);
+    expect(randomString.length).toBe(length);
   });
 
-  test('should generate a different string each time', () => {
-    const randomString1 = generateRandomString(10);
-    const randomString2 = generateRandomString(10);
-    expect(randomString1).not.toBe(randomString2);
+  test('should generate a string containing only specified characters', () => {
+    const characters = 'abc';
+    const randomString = generateRandomString(5, characters);
+    for (const char of randomString) {
+      expect(characters).toContain(char);
+    }
+  });
+
+  test('should generate an empty string if length is 0', () => {
+    const randomString = generateRandomString(0);
+    expect(randomString).toBe('');
+  });
+
+  test('should generate different strings on multiple calls', () => {
+    const string1 = generateRandomString(10);
+    const string2 = generateRandomString(10);
+    expect(string1).not.toBe(string2);
+  });
+
+  test('should use default characters if none are provided', () => {
+    const randomString = generateRandomString(5);
+    const defaultCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (const char of randomString) {
+      expect(defaultCharacters).toContain(char);
+    }
   });
 });
