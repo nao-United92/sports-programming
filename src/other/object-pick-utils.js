da4a07bae8a245350f1c3cddde295b6312f3288a
nao-United92 <naoyauver@icloud.com>
2025-08-19 00:05:57 +0900

    feat: Add object pick utility

---
+++ src/other/object-pick-utils.js
@@ -0,0 +1,18 @@
+/**
+ * Creates an object composed of the picked object properties.
+ *
+ * @param {Object} object The source object.
+ * @param {string[]} keys The property paths to pick.
+ * @returns {Object} Returns the new object.
+ */
+export const pick = (object, keys) => {
+  if (object === null || object === undefined) {
+    return {};
+  }
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
+};
