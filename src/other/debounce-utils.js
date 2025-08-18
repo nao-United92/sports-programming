89c34a712c8ae7d458a2f109e13cf2440b6478b3
nao-United92 <naoyauver@icloud.com>
2025-08-19 00:50:24 +0900

    feat: Add throttle utility and tests

--- src/other/debounce-utils.js
+++ src/other/debounce-utils.js
@@ -7,3 +7,28 @@ export const debounce = (func, delay) => {
     timeout = setTimeout(() => func.apply(context, args), delay);
   };
 };
+
+export const throttle = (func, limit) => {
+  let inThrottle;
+  let lastResult;
+  let lastArgs;
+  let lastThis;
+
+  return function(...args) {
+    const context = this;
+    lastArgs = args;
+    lastThis = context;
+
+    if (!inThrottle) {
+      inThrottle = true;
+      lastResult = func.apply(context, args);
+      setTimeout(() => {
+        inThrottle = false;
+        if (lastArgs !== args || lastThis !== context) {
+          lastResult = func.apply(lastThis, lastArgs);
+        }
+      }, limit);
+    }
+    return lastResult;
+  };
+};
