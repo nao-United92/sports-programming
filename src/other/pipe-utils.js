dcb10c40beae52f63b7d79cb29923da97ecabc94
nao-United92 <naoyauver@icloud.com>
2025-08-21 15:58:19 +0900

    feat(pipe): add pipe utility function

--- src/other/pipe-utils.js
+++ src/other/pipe-utils.js
@@ -1,1 +1,18 @@
-export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+/**
+ * Composes single-argument functions from left to right.
+ * The first function can take multiple arguments; the remaining functions must be unary.
+ *
+ * @param {...Function} funcs The functions to pipe.
+ * @returns {Function} A function obtained by composing the argument functions from left to right.
+ */
+export const pipe = (...funcs) => {
+  if (funcs.length === 0) {
+    return (arg) => arg;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (...args) => b(a(...args)));
+};
--- src/other/pipe-utils.test.js
+++ src/other/pipe-utils.test.js
@@ -1,25 +1,37 @@
-import { pipe } from './pipe-utils';
+import { pipe } from './pipe-utils.js';
 
 describe('pipe', () => {
+  const add = (a, b) => a + b;
+  const square = (n) => n * n;
+  const double = (n) => n * 2;
+
+  test('should return a function', () => {
+    expect(typeof pipe()).toBe('function');
+  });
+
   test('should pipe functions from left to right', () => {
-    const add5 = (x) => x + 5;
-    const multiplyBy2 = (x) => x * 2;
-    const subtract10 = (x) => x - 10;
-
-    const piped = pipe(add5, multiplyBy2, subtract10);
-
-    expect(piped(5)).toBe(10);
-    expect(piped(10)).toBe(20);
+    const piped = pipe(square, double);
+    expect(piped(2)).toBe(8); // (2*2) * 2
   });
 
-  test('should work with a single function', () => {
-    const add5 = (x) => x + 5;
-    const piped = pipe(add5);
-    expect(piped(5)).toBe(10);
+  test('should handle multiple functions', () => {
+    const piped = pipe(square, double, (n) => n + 1);
+    expect(piped(3)).toBe(19); // (3*3) * 2 + 1
   });
 
-  test('should return the initial value if no functions are provided', () => {
-    const piped = pipe();
-    expect(piped(5)).toBe(5);
+  test('should pass initial arguments to the first function', () => {
+    const piped = pipe(add, square, double);
+    expect(piped(2, 3)).toBe(50); // (2+3)^2 * 2
+  });
+
+  test('should return the identity function if no functions are provided', () => {
+    const identity = pipe();
+    expect(identity(5)).toBe(5);
+    expect(identity('hello')).toBe('hello');
+  });
+
+  test('should return the single function if only one is provided', () => {
+    const piped = pipe(square);
+    expect(piped(4)).toBe(16);
   });
 });
