2b7cbec26425bf6ed6028bb8f297b82edf25b6e5
nao-United92 <naoyauver@icloud.com>
2025-08-25 00:32:11 +0900

    feat(compose-utils): Add pipe function for functional composition and update tests

--- src/other/compose-utils.js
+++ src/other/compose-utils.js
@@ -17,3 +17,23 @@ export function compose(...funcs) {
 
   return funcs.reduce((a, b) => (...args) => a(b(...args)));
 }
+
+/**
+ * Composes functions from left to right.
+ * The leftmost function can take multiple arguments as it will be the first to be invoked.
+ * The rest of the functions must be unary (accept a single argument).
+ *
+ * @param {...Function} funcs The functions to pipe.
+ * @returns {Function} Returns the new composite function.
+ */
+export function pipe(...funcs) {
+  if (funcs.length === 0) {
+    return (...args) => args.length > 1 ? [...args] : args[0];
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (...args) => b(a(...args)));
+}
--- src/other/compose-utils.test.js
+++ src/other/compose-utils.test.js
@@ -1,4 +1,4 @@
-import { compose } from './compose-utils';
+import { compose, pipe } from './compose-utils';
 
 describe('compose', () => {
   test('should compose two functions', () => {
@@ -36,5 +36,44 @@ describe('compose', () => {
     const composedFunc = compose(multiplyTwo, sum);
 
     expect(composedFunc(2, 3)).toBe(10); // (2 + 3) * 2 = 10
+  });
+});
+
+describe('pipe', () => {
+  test('should pipe two functions', () => {
+    const addOne = (x) => x + 1;
+    const multiplyTwo = (x) => x * 2;
+    const addOneThenMultiplyTwo = pipe(addOne, multiplyTwo);
+
+    expect(addOneThenMultiplyTwo(5)).toBe(12); // (5 + 1) * 2 = 12
+  });
+
+  test('should pipe multiple functions', () => {
+    const addOne = (x) => x + 1;
+    const multiplyTwo = (x) => x * 2;
+    const subtractThree = (x) => x - 3;
+    const pipedFunc = pipe(addOne, multiplyTwo, subtractThree);
+
+    expect(pipedFunc(5)).toBe(9); // ((5 + 1) * 2) - 3 = 9
+  });
+
+  test('should handle no functions', () => {
+    const pipedFunc = pipe();
+    expect(pipedFunc(5)).toBe(5);
+    expect(pipedFunc(1, 2, 3)).toEqual([1, 2, 3]);
+  });
+
+  test('should handle one function', () => {
+    const addOne = (x) => x + 1;
+    const pipedFunc = pipe(addOne);
+    expect(pipedFunc(5)).toBe(6);
+  });
+
+  test('should pass multiple arguments to the leftmost function', () => {
+    const sum = (a, b) => a + b;
+    const multiplyTwo = (x) => x * 2;
+    const pipedFunc = pipe(sum, multiplyTwo);
+
+    expect(pipedFunc(2, 3)).toBe(10); // (2 + 3) * 2 = 10
   });
 });
