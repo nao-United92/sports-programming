242478606abd1a96328d5a4b17cb3aac94f05714
nao-United92 <naoyauver@icloud.com>
2025-08-25 23:54:47 +0900

    feat(url-utils): Add query string parser and stringifier

--- src/other/query-string-utils.js
+++ src/other/query-string-utils.js
@@ -1,31 +1,61 @@
-export const parse = (queryString) => {
-  const params = new URLSearchParams(queryString);
+/**
+ * Parses a URL query string into an object.
+ * @param {string} str The URL query string to parse (e.g., 'key=value&key2=value2').
+ * @returns {Object} The object representation of the query string.
+ */
+export const parse = (str) => {
   const result = {};
-  for (const [key, value] of params.entries()) {
-    if (result[key]) {
-      if (Array.isArray(result[key])) {
-        result[key].push(value);
-      } else {
-        result[key] = [result[key], value];
-      }
+  if (typeof str !== 'string') {
+    return result;
+  }
+
+  const cleanStr = str.trim().replace(/^[?#&]/, '');
+
+  if (!cleanStr) {
+    return result;
+  }
+
+  for (const param of cleanStr.split('&')) {
+    let [key, value] = param.split('=');
+    if (key === undefined || key === '') continue;
+
+    key = decodeURIComponent(key);
+    const decodedValue = value === undefined ? null : decodeURIComponent(value.replace(/\+/g, ' '));
+
+    if (result[key] === undefined) {
+      result[key] = decodedValue;
     } else {
-      result[key] = value;
+      if (!Array.isArray(result[key])) {
+        result[key] = [result[key]];
+      }
+      result[key].push(decodedValue);
     }
   }
   return result;
 };
 
+/**
+ * Stringifies an object into a URL query string.
+ * @param {Object} obj The object to stringify.
+ * @returns {string} The URL query string.
+ */
 export const stringify = (obj) => {
-  const params = new URLSearchParams();
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      const value = obj[key];
-      if (Array.isArray(value)) {
-        value.forEach(v => params.append(key, v));
-      } else {
-        params.append(key, value);
-      }
-    }
+  if (obj === null || typeof obj !== 'object') {
+    return '';
   }
-  return params.toString();
+
+  return Object.keys(obj).map(key => {
+    const value = obj[key];
+    const encodedKey = encodeURIComponent(key);
+
+    if (value === null || value === undefined) {
+      return encodedKey;
+    }
+
+    if (Array.isArray(value)) {
+      return value.map(v => `${encodedKey}=${encodeURIComponent(v)}`).join('&');
+    }
+
+    return `${encodedKey}=${encodeURIComponent(value)}`;
+  }).filter(Boolean).join('&');
 };
--- src/other/query-string-utils.test.js
+++ src/other/query-string-utils.test.js
@@ -1,31 +1,57 @@
-import { parse, stringify } from './query-string-utils';
+const { parse, stringify } = require('./query-string-utils.js');
 
-describe('queryString utils', () => {
+describe('Query String Utilities', () => {
   describe('parse', () => {
-    test('should parse a simple query string', () => {
-      expect(parse('foo=bar')).toEqual({ foo: 'bar' });
-    });
-
-    test('should parse a query string with multiple values', () => {
+    it('should parse a simple query string', () => {
       expect(parse('foo=bar&baz=qux')).toEqual({ foo: 'bar', baz: 'qux' });
     });
 
-    test('should parse a query string with an array', () => {
-      expect(parse('a=1&a=2')).toEqual({ a: ['1', '2'] });
+    it('should handle leading question marks', () => {
+      expect(parse('?foo=bar&baz=qux')).toEqual({ foo: 'bar', baz: 'qux' });
+    });
+
+    it('should handle URL-encoded characters', () => {
+      expect(parse('a%20b=c%26d')).toEqual({ 'a b': 'c&d' });
+    });
+
+    it('should handle keys without values', () => {
+      expect(parse('foo&bar=baz')).toEqual({ foo: null, bar: 'baz' });
+    });
+
+    it('should handle multiple values for the same key', () => {
+      expect(parse('a=1&a=2&a=3')).toEqual({ a: ['1', '2', '3'] });
+    });
+
+    it('should return an empty object for empty or invalid input', () => {
+      expect(parse('')).toEqual({});
+      expect(parse('?')).toEqual({});
+      expect(parse(null)).toEqual({});
+      expect(parse(123)).toEqual({});
     });
   });
 
   describe('stringify', () => {
-    test('should stringify a simple object', () => {
-      expect(stringify({ foo: 'bar' })).toBe('foo=bar');
-    });
-
-    test('should stringify an object with multiple values', () => {
+    it('should stringify a simple object', () => {
       expect(stringify({ foo: 'bar', baz: 'qux' })).toBe('foo=bar&baz=qux');
     });
 
-    test('should stringify an object with an array', () => {
-      expect(stringify({ a: ['1', '2'] })).toBe('a=1&a=2');
+    it('should URL-encode special characters', () => {
+      expect(stringify({ 'a b': 'c&d' })).toBe('a%20b=c%26d');
+    });
+
+    it('should handle keys with null or undefined values', () => {
+      expect(stringify({ foo: null, bar: 'baz' })).toBe('foo&bar=baz');
+      expect(stringify({ foo: undefined, bar: 'baz' })).toBe('foo&bar=baz');
+    });
+
+    it('should handle array values', () => {
+      expect(stringify({ a: ['1', '2', '3'] })).toBe('a=1&a=2&a=3');
+    });
+
+    it('should return an empty string for empty or invalid input', () => {
+      expect(stringify({})).toBe('');
+      expect(stringify(null)).toBe('');
+      expect(stringify('test')).toBe('');
     });
   });
 });
