516f031911ee69fbf0ce11771cb095a50c058b76
nao-United92 <naoyauver@icloud.com>
2025-08-25 23:50:26 +0900

    refactor(object-pick): Improve pick implementation and tests

--- src/other/object-pick-utils.js
+++ src/other/object-pick-utils.js
@@ -1,18 +1,21 @@
 /**
  * Creates an object composed of the picked object properties.
  *
- * @param {Object} object The source object.
+ * @param {Object} obj The source object.
  * @param {string[]} keys The property paths to pick.
  * @returns {Object} Returns the new object.
  */
-export const pick = (object, keys) => {
-  if (object === null || object === undefined) {
-    return {};
+const pick = (obj, keys) => {
+  const newObj = {};
+  if (obj == null) {
+    return newObj;
   }
-  return keys.reduce((obj, key) => {
-    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
-      obj[key] = object[key];
+  for (const key of keys) {
+    if (key in obj) {
+      newObj[key] = obj[key];
     }
-    return obj;
-  }, {});
+  }
+  return newObj;
 };
+
+module.exports = { pick };
--- src/other/object-pick-utils.test.js
+++ src/other/object-pick-utils.test.js
@@ -1,28 +1,29 @@
-import { pick } from './object-pick-utils';
+const { pick } = require('./object-pick-utils');
 
 describe('pick', () => {
-  it('should return an object with picked properties', () => {
-    const object = { 'a': 1, 'b': '2', 'c': 3 };
-    expect(pick(object, ['a', 'c'])).toEqual({ 'a': 1, 'c': 3 });
+  test('should create an object with picked properties', () => {
+    const obj = { a: 1, b: '2', c: 3 };
+    expect(pick(obj, ['a', 'c'])).toEqual({ a: 1, c: 3 });
   });
 
-  it('should not include properties that are not in the source object', () => {
-    const object = { 'a': 1, 'b': '2' };
-    expect(pick(object, ['a', 'd'])).toEqual({ 'a': 1 });
+  test('should not include properties that are not in the source object', () => {
+    const obj = { a: 1, b: '2' };
+    expect(pick(obj, ['a', 'd'])).toEqual({ a: 1 });
   });
 
-  it('should return an empty object if the source object is null or undefined', () => {
+  test('should return an empty object if no keys are provided', () => {
+    const obj = { a: 1, b: '2' };
+    expect(pick(obj, [])).toEqual({});
+  });
+
+  test('should return an empty object if the source object is null or undefined', () => {
     expect(pick(null, ['a', 'b'])).toEqual({});
     expect(pick(undefined, ['a', 'b'])).toEqual({});
   });
 
-  it('should return an empty object if no keys are provided', () => {
-    const object = { 'a': 1, 'b': '2' };
-    expect(pick(object, [])).toEqual({});
-  });
-
-  it('should handle nested objects (but only picks top-level properties)', () => {
-    const object = { 'a': { 'b': 2 }, 'c': 3 };
-    expect(pick(object, ['a'])).toEqual({ 'a': { 'b': 2 } });
+  test('should not modify the original object', () => {
+    const obj = { a: 1, b: '2' };
+    pick(obj, ['a']);
+    expect(obj).toEqual({ a: 1, b: '2' });
   });
 });
