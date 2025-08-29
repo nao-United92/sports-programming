07f51fb3ac41ef174e9161e25d3b72d0b9186bb1
nao-United92 <naoyauver@icloud.com>
2025-08-29 23:01:26 +0900

    refactor(object): improve pick and omit utilities
    Refactor the 'pick' and 'omit' functions for better performance and robustness.
    
    - Add null/undefined checks for input objects.
    
    - Utilize 'reduce' for a more functional approach in 'pick'.
    
    - Enhance JSDoc comments for clarity.
    
    - Update and expand tests to cover edge cases and ensure immutability.

--- src/other/object-pick-omit-utils.js
+++ src/other/object-pick-omit-utils.js
@@ -1,17 +1,36 @@
-export const pick = (obj, keys) => {
-  const newObj = {};
-  for (const key of keys) {
-    if (key in obj) {
-      newObj[key] = obj[key];
-    }
+/**
+ * Creates an object composed of the picked object properties.
+ *
+ * @param {Object} object The source object.
+ * @param {string[]} keys The property paths to pick.
+ * @returns {Object} Returns the new object.
+ */
+export const pick = (object, keys) => {
+  if (object == null) {
+    return {};
   }
-  return newObj;
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
 };
 
-export const omit = (obj, keys) => {
-  const newObj = { ...obj };
-  for (const key of keys) {
-    delete newObj[key];
+/**
+ * Creates an object composed of the own and inherited enumerable property paths of object that are not omitted.
+ *
+ * @param {Object} object The source object.
+ * @param {string[]} keys The property paths to omit.
+ * @returns {Object} Returns the new object.
+ */
+export const omit = (object, keys) => {
+  if (object == null) {
+    return {};
   }
-  return newObj;
+  const newObject = { ...object };
+  keys.forEach(key => {
+    delete newObject[key];
+  });
+  return newObject;
 };
--- src/other/object-pick-omit-utils.test.js
+++ src/other/object-pick-omit-utils.test.js
@@ -1,36 +1,47 @@
 import { pick, omit } from './object-pick-omit-utils.js';
 
-describe('pick', () => {
-  it('should create an object with picked properties', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    expect(pick(obj, ['a', 'c'])).toEqual({ a: 1, c: 3 });
+describe('object-pick-omit-utils', () => {
+  const data = { a: 1, b: 2, c: 3 };
+
+  describe('pick', () => {
+    it('should create an object with picked properties', () => {
+      expect(pick(data, ['a', 'c'])).toEqual({ a: 1, c: 3 });
+    });
+
+    it('should not modify the original object', () => {
+      const original = { ...data };
+      pick(data, ['a']);
+      expect(data).toEqual(original);
+    });
+
+    it('should return an empty object if keys do not exist', () => {
+      expect(pick(data, ['d', 'e'])).toEqual({});
+    });
+
+    it('should return an empty object for null or undefined input', () => {
+      expect(pick(null, ['a'])).toEqual({});
+      expect(pick(undefined, ['a'])).toEqual({});
+    });
   });
 
-  it('should return an empty object if no keys are picked', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    expect(pick(obj, [])).toEqual({});
-  });
+  describe('omit', () => {
+    it('should create an object without omitted properties', () => {
+      expect(omit(data, ['b'])).toEqual({ a: 1, c: 3 });
+    });
 
-  it('should ignore keys that do not exist in the source object', () => {
-    const obj = { a: 1, b: 2 };
-    expect(pick(obj, ['a', 'd'])).toEqual({ a: 1 });
-  });
-});
+    it('should not modify the original object', () => {
+      const original = { ...data };
+      omit(data, ['b']);
+      expect(data).toEqual(original);
+    });
 
-describe('omit', () => {
-  it('should create an object without omitted properties', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    expect(omit(obj, ['a', 'c'])).toEqual({ b: 2 });
-  });
+    it('should return the same object if keys to omit do not exist', () => {
+      expect(omit(data, ['d', 'e'])).toEqual(data);
+    });
 
-  it('should return an equivalent object if no keys are omitted', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    expect(omit(obj, [])).toEqual({ a: 1, b: 2, c: 3 });
-  });
-
-  it('should not modify the original object', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    omit(obj, ['a']);
-    expect(obj).toEqual({ a: 1, b: 2, c: 3 });
+    it('should return an empty object for null or undefined input', () => {
+      expect(omit(null, ['a'])).toEqual({});
+      expect(omit(undefined, ['a'])).toEqual({});
+    });
   });
 });
