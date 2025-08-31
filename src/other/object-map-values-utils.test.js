948781018caac68128eebf528ae0d406a1edefed
nao-United92 <naoyauver@icloud.com>
2025-09-01 00:46:24 +0900

    feat(object): add mapValues utility

--- src/other/object-map-values-utils.test.js
+++ src/other/object-map-values-utils.test.js
@@ -1,30 +1,39 @@
-import { mapValues } from './object-map-values-utils';
+import { mapValues } from './object-map-values-utils.js';
 
 describe('mapValues', () => {
-  const users = {
-    'fred': { 'user': 'fred', 'age': 40 },
-    'pebbles': { 'user': 'pebbles', 'age': 1 }
-  };
-
-  it('should apply a function to each value in the object', () => {
-    const result = mapValues(users, (user) => user.age);
-    expect(result).toEqual({ 'fred': 40, 'pebbles': 1 });
+  it('should map values of an object', () => {
+    const object = { 'a': 1, 'b': 2 };
+    const result = mapValues(object, (value) => value * 2);
+    expect(result).toEqual({ 'a': 2, 'b': 4 });
   });
 
-  it('should work with a simple object and a simple iteratee', () => {
-    const obj = { a: 1, b: 2 };
-    const result = mapValues(obj, (n) => n * 2);
-    expect(result).toEqual({ a: 2, b: 4 });
+  it('should pass key and object to the iteratee', () => {
+    const object = { 'a': 1, 'b': 2 };
+    const keys = [];
+    mapValues(object, (value, key, obj) => {
+      keys.push(key);
+      expect(obj).toBe(object);
+    });
+    expect(keys).toEqual(['a', 'b']);
   });
 
-  it('should pass value, key, and object to the iteratee', () => {
-    const mockIteratee = jest.fn();
-    const obj = { a: 1 };
-    mapValues(obj, mockIteratee);
-    expect(mockIteratee).toHaveBeenCalledWith(1, 'a', obj);
+  it('should handle an empty object', () => {
+    expect(mapValues({}, (value) => value * 2)).toEqual({});
   });
 
-  it('should return an empty object for an empty object input', () => {
-    expect(mapValues({}, (x) => x)).toEqual({});
+  it('should handle null or undefined input', () => {
+    expect(mapValues(null, (value) => value * 2)).toEqual({});
+    expect(mapValues(undefined, (value) => value * 2)).toEqual({});
+  });
+
+  function Foo() {
+    this.a = 1;
+    this.b = 2;
+  }
+  Foo.prototype.c = 3;
+
+  it('should only include own properties', () => {
+    const result = mapValues(new Foo(), (value) => value * 2);
+    expect(result).toEqual({ 'a': 2, 'b': 4 });
   });
 });
