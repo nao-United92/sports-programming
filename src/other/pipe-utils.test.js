4010a19f454760a5cab68cd98ebb9666f54a5e1b
nao-United92 <naoyauver@icloud.com>
2025-09-03 00:04:29 +0900

    feat(utils): add pipe-utils

--- src/other/pipe-utils.test.js
+++ src/other/pipe-utils.test.js
@@ -1,33 +1,45 @@
 import { pipe } from './pipe-utils.js';
 
 describe('pipe', () => {
-  it('should pipe functions from left to right', () => {
-    const add5 = x => x + 5;
-    const multiplyBy2 = x => x * 2;
-    const subtract10 = x => x - 10;
+  test('should pipe functions from left to right', () => {
+    const add5 = (x) => x + 5;
+    const multiplyBy2 = (x) => x * 2;
+    const subtract10 = (x) => x - 10;
 
     const piped = pipe(add5, multiplyBy2, subtract10);
-    // (5 + 5) * 2 - 10 = 10
-    expect(piped(5)).toBe(10);
+    // (10 + 5) * 2 - 10 = 20
+    expect(piped(10)).toBe(20);
   });
 
-  it('should work with a single function', () => {
-    const add5 = x => x + 5;
-    const piped = pipe(add5);
-    expect(piped(10)).toBe(15);
+  test('should pass initial arguments to the first function', () => {
+    const sum = (a, b) => a + b;
+    const square = (x) => x * x;
+
+    const piped = pipe(sum, square);
+    // (3 + 7)^2 = 100
+    expect(piped(3, 7)).toBe(100);
   });
 
-  it('should return the input if no functions are provided', () => {
-    const piped = pipe();
-    expect(piped(10)).toBe(10);
+  test('should return an identity function if no functions are provided', () => {
+    const identity = pipe();
+    expect(identity(123)).toBe(123);
+    const obj = {};
+    expect(identity(obj)).toBe(obj);
   });
 
-  it('should handle different types of data', () => {
-    const toUpperCase = str => str.toUpperCase();
-    const addExclamation = str => `${str}!`;
-    const repeat = str => str.repeat(3);
+  test('should return the single function if only one is provided', () => {
+    const myFunc = (x) => x * x;
+    const piped = pipe(myFunc);
+    expect(piped).toBe(myFunc);
+  });
 
-    const piped = pipe(toUpperCase, addExclamation, repeat);
-    expect(piped('hello')).toBe('HELLO!HELLO!HELLO!');
+  test('should work with functions that return different types', () => {
+    const getLength = (str) => str.length;
+    const isEven = (num) => num % 2 === 0;
+    const toString = (bool) => bool.toString();
+
+    const piped = pipe(getLength, isEven, toString);
+    expect(piped('hello')).toBe('false'); // length 5 is odd
+    expect(piped('hellow')).toBe('true'); // length 6 is even
   });
 });
