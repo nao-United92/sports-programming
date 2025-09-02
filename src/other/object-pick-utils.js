2b2ebe3b45380e044ddb64bb99ecc5791e6ab13b
nao-United92 <naoyauver@icloud.com>
2025-09-02 23:45:07 +0900

    feat(utils): add object-pick-utils

--- src/other/object-pick-utils.js
+++ src/other/object-pick-utils.js
@@ -1,16 +1,18 @@
 /**
  * Creates an object composed of the picked object properties.
  *
- * @param {Object} object The source object.
- * @param {string|string[]} paths The property paths to pick.
- * @returns {Object} Returns the new object.
+ * @param {object} object The source object.
+ * @param {string[]} keys The property paths to pick.
+ * @returns {object} Returns the new object.
  */
-export const pick = (object, paths) => {
+export const pick = (object, keys) => {
   if (object == null) {
     return {};
   }
-  const props = Array.isArray(paths) ? paths : [paths];
-  return Object.fromEntries(
-    Object.entries(object).filter(([key]) => props.includes(key))
-  );
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
 };
--- src/other/object-pick-utils.test.js
+++ src/other/object-pick-utils.test.js
@@ -1,28 +1,29 @@
 import { pick } from './object-pick-utils.js';
 
 describe('pick', () => {
-  it('should create an object with picked properties', () => {
-    const object = { a: 1, b: '2', c: 3 };
-    expect(pick(object, ['a', 'c'])).toEqual({ a: 1, c: 3 });
+  test('should pick specified keys from an object', () => {
+    const object = { 'a': 1, 'b': '2', 'c': 3 };
+    expect(pick(object, ['a', 'c'])).toEqual({ 'a': 1, 'c': 3 });
   });
 
-  it('should work with a single property', () => {
-    const object = { a: 1, b: '2', c: 3 };
-    expect(pick(object, 'a')).toEqual({ a: 1 });
+  test('should not mutate the original object', () => {
+    const object = { 'a': 1, 'b': '2', 'c': 3 };
+    pick(object, ['a', 'c']);
+    expect(object).toEqual({ 'a': 1, 'b': '2', 'c': 3 });
   });
 
-  it('should return an empty object if no properties are picked', () => {
-    const object = { a: 1, b: '2', c: 3 };
+  test('should return an empty object if keys do not exist', () => {
+    const object = { 'a': 1, 'b': '2', 'c': 3 };
     expect(pick(object, ['d', 'e'])).toEqual({});
   });
 
-  it('should return an empty object if the source object is null or undefined', () => {
-    expect(pick(null, ['a', 'b'])).toEqual({});
-    expect(pick(undefined, ['a', 'b'])).toEqual({});
+  test('should return an empty object if the keys array is empty', () => {
+    const object = { 'a': 1, 'b': '2', 'c': 3 };
+    expect(pick(object, [])).toEqual({});
   });
 
-  it('should return an empty object if paths are empty', () => {
-    const object = { a: 1, b: '2', c: 3 };
-    expect(pick(object, [])).toEqual({});
+  test('should return an empty object for null or undefined input object', () => {
+    expect(pick(null, ['a', 'b'])).toEqual({});
+    expect(pick(undefined, ['a', 'b'])).toEqual({});
   });
 });
