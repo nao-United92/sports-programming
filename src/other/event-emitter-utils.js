75c624dfd7c76ad5c41a34af8ff5f04542fadcc6
nao-United92 <naoyauver@icloud.com>
2025-09-08 00:27:36 +0900

    feat: Add event emitter utility

--- src/other/event-emitter-utils.js
+++ src/other/event-emitter-utils.js
@@ -1,15 +1,12 @@
-/**
- * A simple event emitter class that allows for subscribing to events and emitting them.
- */
-export class EventEmitter {
+class EventEmitter {
   constructor() {
     this.events = {};
   }
 
   /**
-   * Register an event listener.
+   * Registers an event listener.
    * @param {string} eventName The name of the event.
-   * @param {Function} listener The callback function.
+   * @param {Function} listener The callback function to execute when the event is emitted.
    */
   on(eventName, listener) {
     if (!this.events[eventName]) {
@@ -19,7 +16,7 @@ export class EventEmitter {
   }
 
   /**
-   * Remove an event listener.
+   * Removes an event listener.
    * @param {string} eventName The name of the event.
    * @param {Function} listener The callback function to remove.
    */
@@ -28,35 +25,23 @@ export class EventEmitter {
       return;
     }
     this.events[eventName] = this.events[eventName].filter(
-      l => l !== listener
+      (l) => l !== listener
     );
   }
 
   /**
-   * Emit an event, calling all registered listeners.
+   * Emits an event, calling all registered listeners for that event.
    * @param {string} eventName The name of the event to emit.
-   * @param  {...any} args Arguments to pass to the listeners.
+   * @param {...any} args Arguments to pass to the event listeners.
    */
   emit(eventName, ...args) {
     if (!this.events[eventName]) {
       return;
     }
-    // Use a copy of the array in case a listener modifies the original array
-    [...this.events[eventName]].forEach(listener => {
+    this.events[eventName].forEach((listener) => {
       listener(...args);
     });
   }
-
-  /**
-   * Register an event listener that will be called only once.
-   * @param {string} eventName The name of the event.
-   * @param {Function} listener The callback function.
-   */
-  once(eventName, listener) {
-    const onceWrapper = (...args) => {
-      listener(...args);
-      this.off(eventName, onceWrapper);
-    };
-    this.on(eventName, onceWrapper);
-  }
 }
+
+export default EventEmitter;
--- src/other/event-emitter-utils.test.js
+++ src/other/event-emitter-utils.test.js
@@ -1,4 +1,4 @@
-import { EventEmitter } from './event-emitter-utils.js';
+import EventEmitter from './event-emitter-utils.js';
 
 describe('EventEmitter', () => {
   let emitter;
@@ -7,75 +7,49 @@ describe('EventEmitter', () => {
     emitter = new EventEmitter();
   });
 
-  test('should register and call an event listener', () => {
+  test('should register and emit events', () => {
     const listener = jest.fn();
-    emitter.on('test-event', listener);
-    emitter.emit('test-event');
-    expect(listener).toHaveBeenCalledTimes(1);
+    emitter.on('testEvent', listener);
+    emitter.emit('testEvent', 1, 2);
+    expect(listener).toHaveBeenCalledWith(1, 2);
   });
 
-  test('should pass arguments to the listener', () => {
-    const listener = jest.fn();
-    emitter.on('test-event', listener);
-    emitter.emit('test-event', 1, 'hello', { a: 2 });
-    expect(listener).toHaveBeenCalledWith(1, 'hello', { a: 2 });
-  });
-
-  test('should call multiple listeners for the same event', () => {
+  test('should register multiple listeners for the same event', () => {
     const listener1 = jest.fn();
     const listener2 = jest.fn();
-    emitter.on('test-event', listener1);
-    emitter.on('test-event', listener2);
-    emitter.emit('test-event');
+    emitter.on('testEvent', listener1);
+    emitter.on('testEvent', listener2);
+    emitter.emit('testEvent');
     expect(listener1).toHaveBeenCalledTimes(1);
     expect(listener2).toHaveBeenCalledTimes(1);
   });
 
-  test('should remove a specific listener with off()', () => {
+  test('should remove event listeners', () => {
+    const listener = jest.fn();
+    emitter.on('testEvent', listener);
+    emitter.off('testEvent', listener);
+    emitter.emit('testEvent');
+    expect(listener).not.toHaveBeenCalled();
+  });
+
+  test('should not emit if no listeners are registered', () => {
+    const listener = jest.fn();
+    emitter.emit('anotherEvent');
+    expect(listener).not.toHaveBeenCalled();
+  });
+
+  test('should handle events with no listeners registered', () => {
+    expect(() => emitter.emit('nonExistentEvent')).not.toThrow();
+  });
+
+  test('should not remove other listeners when one is removed', () => {
     const listener1 = jest.fn();
     const listener2 = jest.fn();
-    emitter.on('test-event', listener1);
-    emitter.on('test-event', listener2);
-    
-    emitter.off('test-event', listener1);
-    emitter.emit('test-event');
-
+    emitter.on('testEvent', listener1);
+    emitter.on('testEvent', listener2);
+    emitter.off('testEvent', listener1);
+    emitter.emit('testEvent');
     expect(listener1).not.toHaveBeenCalled();
     expect(listener2).toHaveBeenCalledTimes(1);
-  });
-
-  test('should do nothing when emitting an event with no listeners', () => {
-    expect(() => emitter.emit('non-existent-event')).not.toThrow();
-  });
-
-  test('should register and call a once listener only once', () => {
-    const onceListener = jest.fn();
-    emitter.once('once-event', onceListener);
-
-    emitter.emit('once-event');
-    emitter.emit('once-event');
-    emitter.emit('once-event');
-
-    expect(onceListener).toHaveBeenCalledTimes(1);
-  });
-
-  test('a listener should be able to remove itself without affecting others', () => {
-    const listener1 = jest.fn(() => {
-      emitter.off('test-event', listener1);
-    });
-    const listener2 = jest.fn();
-
-    emitter.on('test-event', listener1);
-    emitter.on('test-event', listener2);
-
-    emitter.emit('test-event');
-
-    expect(listener1).toHaveBeenCalledTimes(1);
-    expect(listener2).toHaveBeenCalledTimes(1);
-
-    // Emit again to make sure listener1 was removed
-    emitter.emit('test-event');
-    expect(listener1).toHaveBeenCalledTimes(1);
-    expect(listener2).toHaveBeenCalledTimes(2);
   });
 });
