11d16f5da1856325ca97247475746161a1c4767b
nao-United92 <naoyauver@icloud.com>
2025-09-10 23:18:52 +0900

    feat: Add object pick utility function

---
+++ src/other/practice/object-pick-utils.js
@@ -0,0 +1,20 @@
+/**
+ * Creates an object composed of the picked object properties.
+ *
+ * @param {Object} object The source object.
+ * @param {string[]} keys The property paths to pick.
+ * @returns {Object} Returns the new object.
+ */
+const pick = (object, keys) => {
+  if (object == null) {
+    return {};
+  }
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
+};
+
+export { pick };
---
+++ src/other/practice/object-pick-utils.test.js
@@ -0,0 +1,23 @@
+import { pick } from './object-pick-utils.js';
+
+describe('pick', () => {
+  it('should create an object with picked properties', () => {
+    const object = { 'a': 1, 'b': '2', 'c': 3 };
+    expect(pick(object, ['a', 'c'])).toEqual({ 'a': 1, 'c': 3 });
+  });
+
+  it('should ignore keys that are not in the object', () => {
+    const object = { 'a': 1, 'b': '2' };
+    expect(pick(object, ['a', 'd'])).toEqual({ 'a': 1 });
+  });
+
+  it('should return an empty object if the source object is null or undefined', () => {
+    expect(pick(null, ['a', 'b'])).toEqual({});
+    expect(pick(undefined, ['a', 'b'])).toEqual({});
+  });
+
+  it('should return an empty object if no keys are provided', () => {
+    const object = { 'a': 1, 'b': '2' };
+    expect(pick(object, [])).toEqual({});
+  });
+});
