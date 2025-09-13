b6b6453bfebb621ac3d72e887f04ea618dbf8b90
nao-United92 <naoyauver@icloud.com>
2025-09-14 00:52:50 +0900

    feat: Add groupBy utility function
    Adds a `groupBy` utility function that creates an object composed of keys generated from the results of running each element of a collection thru an iteratee.
    
    This function is useful for grouping a collection of objects by a common property.
    
    Includes a test suite covering grouping by a function and by a property name, as well as handling empty and non-array inputs.

--- src/other/group-by-utils.js
+++ src/other/group-by-utils.js
@@ -1,24 +1,22 @@
 /**
- * Creates an object composed of keys generated from the results of running each element of collection thru iteratee.
- * The order of grouped values is determined by the order they occur in collection.
- * The corresponding value of each key is an array of elements responsible for generating the key.
+ * Creates an object composed of keys generated from the results of running each element of `collection` thru `iteratee`.
+ * The order of grouped values is determined by the order they occur in `collection`. The corresponding value of each key
+ * is an array of elements responsible for generating the key.
  *
  * @param {Array} collection The collection to iterate over.
- * @param {Function|string} iteratee The iteratee to transform keys.
+ * @param {Function} iteratee The iteratee to transform keys.
  * @returns {Object} Returns the composed aggregate object.
  */
-export const groupBy = (collection, iteratee) => {
-  if (!collection) {
+export function groupBy(collection, iteratee) {
+  if (!Array.isArray(collection)) {
     return {};
   }
-  const getIteratee = typeof iteratee === 'function' ? iteratee : (item) => item[iteratee];
-
-  return collection.reduce((acc, item) => {
-    const key = getIteratee(item);
+  return collection.reduce((acc, val) => {
+    const key = typeof iteratee === 'function' ? iteratee(val) : val[iteratee];
     if (!acc[key]) {
       acc[key] = [];
     }
-    acc[key].push(item);
+    acc[key].push(val);
     return acc;
   }, {});
-};
+}
--- src/other/group-by-utils.test.js
+++ src/other/group-by-utils.test.js
@@ -1,44 +1,31 @@
 import { groupBy } from './group-by-utils.js';
 
 describe('groupBy', () => {
-  test('should group a collection by a given property string', () => {
-    const collection = [
+  it('should group by a function', () => {
+    const array = [6.1, 4.2, 6.3];
+    const result = groupBy(array, Math.floor);
+    expect(result).toEqual({ '4': [4.2], '6': [6.1, 6.3] });
+  });
+
+  it('should group by a property name', () => {
+    const array = [
       { 'dir': 'left', 'code': 97 },
-      { 'dir': 'right', 'code': 100 },
-      { 'dir': 'left', 'code': 97 },
+      { 'dir': 'right', 'code': 100 }
     ];
-    const expected = {
-      'left': [{ 'dir': 'left', 'code': 97 }, { 'dir': 'left', 'code': 97 }],
-      'right': [{ 'dir': 'right', 'code': 100 }],
-    };
-    expect(groupBy(collection, 'dir')).toEqual(expected);
+    const result = groupBy(array, 'dir');
+    expect(result).toEqual({ 'left': [{ 'dir': 'left', 'code': 97 }], 'right': [{ 'dir': 'right', 'code': 100 }] });
   });
 
-  test('should group a collection by the result of a function', () => {
-    const collection = [6.1, 4.2, 6.3];
-    const expected = {
-      '6': [6.1, 6.3],
-      '4': [4.2],
-    };
-    expect(groupBy(collection, Math.floor)).toEqual(expected);
+  it('should handle an empty array', () => {
+    const array = [];
+    const result = groupBy(array, 'length');
+    expect(result).toEqual({});
   });
 
-  test('should return an empty object for a null or undefined collection', () => {
+  it('should handle non-array input', () => {
     expect(groupBy(null, 'length')).toEqual({});
     expect(groupBy(undefined, 'length')).toEqual({});
-  });
-
-  test('should return an empty object for an empty collection', () => {
-    expect(groupBy([], 'length')).toEqual({});
-  });
-
-  test('should work with items that do not have the property', () => {
-    const collection = [{ a: 1 }, { b: 2 }, { a: 3 }];
-    const expected = {
-      '1': [{ a: 1 }],
-      'undefined': [{ b: 2 }],
-      '3': [{ a: 3 }],
-    };
-    expect(groupBy(collection, 'a')).toEqual(expected);
+    expect(groupBy({}, 'length')).toEqual({});
+    expect(groupBy(123, 'length')).toEqual({});
   });
 });
