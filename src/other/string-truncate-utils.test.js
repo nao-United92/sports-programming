e61360cee3da18b2e4a31a2f3db6fcb0092c1e97
nao-United92 <naoyauver@icloud.com>
2025-09-16 00:30:24 +0900

    refactor: Update object-pick and string-truncate utilities

--- src/other/string-truncate-utils.test.js
+++ src/other/string-truncate-utils.test.js
@@ -1,44 +1,34 @@
-const { truncate } = require('./string-truncate-utils');
+import { truncate } from './string-truncate-utils.js';
 
 describe('truncate', () => {
-  const str = 'hi-diddly-ho there, neighborino';
-
-  it('should truncate a string to a default length of 30', () => {
-    expect(truncate(str)).toBe('hi-diddly-ho there, neighbo...');
+  test('should truncate a string to the specified length and add ellipsis', () => {
+    expect(truncate('hello world', 7)).toBe('hello w...');
+    expect(truncate('hello world', 5)).toBe('hello...');
   });
 
-  it('should not truncate if string is shorter than length', () => {
-    expect(truncate(str, { length: str.length })).toBe(str);
+  test('should not truncate if string length is less than or equal to maxLength', () => {
+    expect(truncate('hello', 5)).toBe('hello');
+    expect(truncate('hi', 5)).toBe('hi');
   });
 
-  it('should truncate to a specified length', () => {
-    expect(truncate(str, { length: 24 })).toBe('hi-diddly-ho there, n...');
+  test('should return an empty string for empty input', () => {
+    expect(truncate('', 5)).toBe('');
   });
 
-  it('should use a custom omission string', () => {
-    expect(truncate(str, { omission: ' [...]' })).toBe('hi-diddly-ho there, neig [...]');
+  test('should return an empty string for non-string inputs', () => {
+    expect(truncate(null, 5)).toBe('');
+    expect(truncate(undefined, 5)).toBe('');
+    expect(truncate(123, 5)).toBe('');
+    expect(truncate({}, 5)).toBe('');
+    expect(truncate([], 5)).toBe('');
   });
 
-  it('should not truncate on a separator if it does not exist', () => {
-    expect(truncate(str, { length: 20, separator: '_' })).toBe('hi-diddly-ho ther...');
+  test('should handle maxLength of 0', () => {
+    expect(truncate('hello', 0)).toBe('...');
   });
 
-  it('should truncate with a separator pattern', () => {
-    expect(truncate(str, { length: 24, separator: ' ' })).toBe('hi-diddly-ho there,...');
-  });
-
-  it('should truncate with a regex separator', () => {
-    expect(truncate(str, { length: 24, separator: /,? +/ })).toBe('hi-diddly-ho there...');
-  });
-
-  it('should return an empty string for null or undefined input', () => {
-    expect(truncate(null)).toBe('');
-    expect(truncate(undefined)).toBe('');
-  });
-
-  it('should handle omission string being longer than length', () => {
-    expect(truncate(str, { length: 5, omission: '...' })).toBe('hi...');
-    expect(truncate(str, { length: 3, omission: '...' })).toBe('...');
-    expect(truncate(str, { length: 2, omission: '...' })).toBe('...');
+  test('should handle maxLength less than 3', () => {
+    expect(truncate('hello', 1)).toBe('h...');
+    expect(truncate('hello', 2)).toBe('he...');
   });
 });
