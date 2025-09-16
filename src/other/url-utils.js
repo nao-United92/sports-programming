72cf735cba49741e744e13767d1c1c8ea9fd0970
nao-United92 <naoyauver@icloud.com>
2025-09-17 00:05:30 +0900

    feat: Add URL utilities
    This commit introduces two new URL utilities: getURLParameters and isAbsoluteURL.
    
    getURLParameters: Gets the parameters of a URL.
    
    isAbsoluteURL: Checks if a URL is absolute.
    
    Tests for these utility functions have been added in url-utils.test.js.

--- src/other/url-utils.js
+++ src/other/url-utils.js
@@ -1,44 +1,24 @@
 /**
- * Converts a URL's query string into an object.
- * Requires a full URL to be passed.
+ * Gets the parameters of a URL.
  * @param {string} url The URL to parse.
- * @returns {Object} An object representing the query parameters.
+ * @returns {Object} Returns an object with the URL parameters.
  */
-const paramsToObject = (url) => {
-  const params = new URL(url).searchParams;
-  const obj = {};
-  for (const [key, value] of params.entries()) {
-    if (obj[key]) {
-      if (Array.isArray(obj[key])) {
-        obj[key].push(value);
-      } else {
-        obj[key] = [obj[key], value];
-      }
-    } else {
-      obj[key] = value;
-    }
+const getURLParameters = (url) => {
+  const params = {};
+  const searchParams = new URL(url).searchParams;
+  for (const [key, value] of searchParams.entries()) {
+    params[key] = value;
   }
-  return obj;
+  return params;
 };
 
 /**
- * Converts an object to a URL query string.
- * @param {Object} obj The object to convert.
- * @returns {string} The URL query string.
+ * Checks if a URL is absolute.
+ * @param {string} url The URL to check.
+ * @returns {boolean} Returns `true` if the URL is absolute, else `false`.
  */
-const objectToParams = (obj) => {
-  const params = new URLSearchParams();
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      const value = obj[key];
-      if (Array.isArray(value)) {
-        value.forEach(v => params.append(key, v));
-      } else if (value !== undefined && value !== null) {
-        params.append(key, value);
-      }
-    }
-  }
-  return params.toString();
+const isAbsoluteURL = (url) => {
+  return /^[a-z][a-z0-9+.-]*:/.test(url);
 };
 
-module.exports = { paramsToObject, objectToParams };
+module.exports = { getURLParameters, isAbsoluteURL };
--- src/other/url-utils.test.js
+++ src/other/url-utils.test.js
@@ -1,32 +1,27 @@
-const assert = require('assert');
-const { paramsToObject, objectToParams } = require('./url-utils.js');
+const { getURLParameters, isAbsoluteURL } = require('./url-utils.js');
 
-try {
-  // paramsToObject tests
-  const url1 = 'https://example.com?a=1&b=2';
-  assert.deepStrictEqual(paramsToObject(url1), { a: '1', b: '2' }, 'paramsToObject should handle simple cases');
+describe('getURLParameters', () => {
+  it('should get the parameters of a URL', () => {
+    const url = 'https://www.google.com/search?q=javascript&oq=javascript';
+    const params = getURLParameters(url);
+    expect(params).toEqual({ q: 'javascript', oq: 'javascript' });
+  });
 
-  const url2 = 'https://example.com?a=1&a=2&b=hello';
-  assert.deepStrictEqual(paramsToObject(url2), { a: ['1', '2'], b: 'hello' }, 'paramsToObject should handle array values');
+  it('should handle a URL with no parameters', () => {
+    const url = 'https://www.google.com';
+    const params = getURLParameters(url);
+    expect(params).toEqual({});
+  });
+});
 
-  const url3 = 'https://example.com';
-  assert.deepStrictEqual(paramsToObject(url3), {}, 'paramsToObject should handle no params');
+describe('isAbsoluteURL', () => {
+  it('should return true for an absolute URL', () => {
+    expect(isAbsoluteURL('https://www.google.com')).toBe(true);
+    expect(isAbsoluteURL('ftp://ftp.example.com')).toBe(true);
+  });
 
-  // objectToParams tests
-  const obj1 = { a: 1, b: 'hello' };
-  assert.strictEqual(objectToParams(obj1), 'a=1&b=hello', 'objectToParams should handle simple objects');
-
-  const obj2 = { a: [1, 2], b: 'world' };
-  assert.strictEqual(objectToParams(obj2), 'a=1&a=2&b=world', 'objectToParams should handle array values');
-
-  const obj3 = { a: null, b: undefined, c: 'test' };
-  assert.strictEqual(objectToParams(obj3), 'c=test', 'objectToParams should ignore null and undefined');
-
-  const obj4 = {};
-  assert.strictEqual(objectToParams(obj4), '', 'objectToParams should handle empty object');
-
-  console.log('All url-utils tests passed!');
-} catch (error) {
-  console.error('url-utils tests failed:', error.message);
-  process.exit(1);
-}
+  it('should return false for a relative URL', () => {
+    expect(isAbsoluteURL('/path/to/file')).toBe(false);
+    expect(isAbsoluteURL('path/to/file')).toBe(false);
+  });
+});
