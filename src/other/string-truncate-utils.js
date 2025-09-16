03d86f9743bbe5bc47a1713a42c16a5e1cf8fd33
nao-United92 <naoyauver@icloud.com>
2025-09-16 23:55:36 +0900

    feat: Add truncate string utility
    This commit introduces a new string utility: truncate.
    
    Tests for the utility function have been added in string-truncate-utils.test.js.

--- src/other/string-truncate-utils.js
+++ src/other/string-truncate-utils.js
@@ -1,9 +1,16 @@
-export const truncate = (str, maxLength) => {
-  if (typeof str !== 'string' || str.length === 0) {
-    return '';
-  }
-  if (str.length <= maxLength) {
+/**
+ * Truncates a string if it's longer than the given maximum string length.
+ * The last characters of the truncated string are replaced with the omission string which defaults to "...".
+ * @param {string} str The string to truncate.
+ * @param {number} length The maximum string length.
+ * @param {string} [omission='...'] The string to indicate text is omitted.
+ * @returns {string} Returns the truncated string.
+ */
+const truncate = (str, length, omission = '...') => {
+  if (str.length <= length) {
     return str;
   }
-  return str.slice(0, maxLength) + '...';
+  return str.slice(0, length - omission.length) + omission;
 };
+
+module.exports = { truncate };
