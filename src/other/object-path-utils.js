6b237c496c0e31014092475360f9bec31cd2cbb0
nao-United92 <naoyauver@icloud.com>
2025-09-16 23:54:34 +0900

    feat: Add get and set object path utilities
    This commit introduces two new object path utilities: get and set.
    
    get: Gets the value at a path of an object.
    
    set: Sets the value at a path of an object.
    
    Tests for these utility functions have been added in object-path-utils.test.js.

--- src/other/object-path-utils.js
+++ src/other/object-path-utils.js
@@ -1,36 +1,35 @@
-const stringToPath = (path) => {
-  if (typeof path !== 'string') return path;
-  const result = [];
-  path.replace(/[^.[\].]+|\[(?:.|
-)]|\[(.*?)]/g, (match, key) => {
-    result.push(key || match.replace(/\\(.)/g, '$1'));
-  });
-  return result;
+/**
+ * Gets the value at a path of an object. If the resolved value is undefined, the defaultValue is returned in its place.
+ * @param {Object} obj The object to query.
+ * @param {Array|string} path The path of the property to retrieve.
+ * @param {*} [defaultValue] The value returned for undefined resolved values.
+ * @returns {*} Returns the resolved value.
+ */
+const get = (obj, path, defaultValue = undefined) => {
+  const pathArray = Array.isArray(path) ? path : path.split('.').filter(key => key);
+  const result = pathArray.reduce((acc, key) => (acc && acc[key] !== 'undefined' ? acc[key] : undefined), obj);
+  return result === undefined ? defaultValue : result;
 };
 
-export const get = (obj, path, defaultValue) => {
-  const keys = Array.isArray(path) ? path : stringToPath(path);
-  let result = obj;
-  for (const key of keys) {
-    result = result?.[key];
-    if (result === undefined) {
-      return defaultValue;
-    }
-  }
-  return result;
-};
-
-export const has = (obj, path) => {
-  const keys = Array.isArray(path) ? path : stringToPath(path);
+/**
+ * Sets the value at a path of an object. If a portion of the path doesn't exist, it's created.
+ * @param {Object} obj The object to modify.
+ * @param {Array|string} path The path of the property to set.
+ * @param {*} value The value to set.
+ * @returns {Object} Returns the modified object.
+ */
+const set = (obj, path, value) => {
+  const pathArray = Array.isArray(path) ? path : path.split('.').filter(key => key);
   let current = obj;
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    if (current == null || !Object.prototype.hasOwnProperty.call(current, key)) {
-      return false;
+  for (let i = 0; i < pathArray.length - 1; i++) {
+    const key = pathArray[i];
+    if (current[key] === undefined) {
+      current[key] = {};
     }
     current = current[key];
   }
-  // Check if the final key exists, even if its value is undefined
-  const lastKey = keys[keys.length - 1];
-  return keys.length > 0 ? Object.prototype.hasOwnProperty.call(current, lastKey) : true;
+  current[pathArray[pathArray.length - 1]] = value;
+  return obj;
 };
+
+module.exports = { get, set };
