90a9a51d254016e61fc829c8b0be6561ca85f744
nao-United92 <naoyauver@icloud.com>
2025-09-19 23:38:11 +0900

    feat(object): add defaultsDeep utility function    2    3 Adds a `defaultsDeep` utility function that recursively assigns default properties from source objects to a      destination object. This is useful for applying nested default configurations.

--- src/other/object-defaults-deep-utils.js
+++ src/other/object-defaults-deep-utils.js
@@ -1,24 +1,35 @@
+const isObject = (value) => {
+  const type = typeof value;
+  return value != null && (type === 'object' || type === 'function');
+};
+
 /**
  * This method is like `defaults` except that it recursively assigns default properties.
  *
- * @param {Object} object The destination object.
- * @param {...Object} sources The source objects.
- * @returns {Object} Returns `object`.
+ * @param {object} object The destination object.
+ * @param {...object} sources The source objects.
+ * @returns {object} Returns `object`.
  */
-export function defaultsDeep(object, ...sources) {
+const defaultsDeep = (object, ...sources) => {
   for (const source of sources) {
-    if (source) {
-      for (const key in source) {
-        if (Object.prototype.hasOwnProperty.call(source, key)) {
-          const value = object[key];
-          if (value === undefined) {
-            object[key] = source[key];
-          } else if (typeof value === 'object' && value !== null && typeof source[key] === 'object' && source[key] !== null) {
-            defaultsDeep(value, source[key]);
+    if (!source) continue;
+    for (const key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        const value = object[key];
+        if (value === undefined || (isObject(value) && !Object.prototype.hasOwnProperty.call(object, key))) {
+          const sourceValue = source[key];
+          if (isObject(value) && isObject(sourceValue)) {
+            object[key] = defaultsDeep(value, sourceValue);
+          } else {
+            object[key] = sourceValue;
           }
+        } else if (isObject(value) && isObject(source[key])) {
+            defaultsDeep(value, source[key]);
         }
       }
     }
   }
   return object;
-}
+};
+
+export { defaultsDeep };
--- src/other/object-defaults-deep-utils.test.js
+++ src/other/object-defaults-deep-utils.test.js
@@ -1,39 +1,47 @@
 import { defaultsDeep } from './object-defaults-deep-utils.js';
 
 describe('defaultsDeep', () => {
-  it('should recursively assign default properties', () => {
-    const object = { 'a': { 'b': 2 } };
-    const source = { 'a': { 'b': 1, 'c': 3 } };
-    defaultsDeep(object, source);
-    expect(object).toEqual({ 'a': { 'b': 2, 'c': 3 } });
+  test('should recursively assign default properties', () => {
+    const obj = { a: { b: 2 } };
+    const source = { a: { b: 3, c: 3 }, d: 4 };
+    defaultsDeep(obj, source);
+    expect(obj).toEqual({ a: { b: 2, c: 3 }, d: 4 });
   });
 
-  it('should not overwrite existing properties', () => {
-    const object = { 'a': { 'b': 2 } };
-    const source = { 'a': { 'b': 1 } };
-    defaultsDeep(object, source);
-    expect(object).toEqual({ 'a': { 'b': 2 } });
+  test('should not overwrite existing properties', () => {
+    const obj = { a: 1 };
+    const source = { a: 2, b: 2 };
+    defaultsDeep(obj, source);
+    expect(obj).toEqual({ a: 1, b: 2 });
   });
 
-  it('should handle multiple source objects', () => {
-    const object = { 'a': { 'b': 2 } };
-    const source1 = { 'a': { 'c': 3 } };
-    const source2 = { 'a': { 'd': 4 } };
-    defaultsDeep(object, source1, source2);
-    expect(object).toEqual({ 'a': { 'b': 2, 'c': 3, 'd': 4 } });
+  test('should handle multiple source objects', () => {
+    const obj = { a: { b: 2 } };
+    const source1 = { a: { c: 3 } };
+    const source2 = { a: { b: 99, d: 4 }, e: 5 };
+    defaultsDeep(obj, source1, source2);
+    expect(obj).toEqual({ a: { b: 2, c: 3, d: 4 }, e: 5 });
   });
 
-  it('should not modify source objects', () => {
-    const object = {};
-    const source = { 'a': { 'b': 1 } };
-    defaultsDeep(object, source);
-    expect(source).toEqual({ 'a': { 'b': 1 } });
+  test('should not overwrite null properties', () => {
+    const obj = { a: null };
+    const source = { a: 1 };
+    defaultsDeep(obj, source);
+    expect(obj).toEqual({ a: null });
   });
 
-  it('should handle null and undefined values', () => {
-    const object = { 'a': null };
-    const source = { 'a': { 'b': 1 } };
-    defaultsDeep(object, source);
-    expect(object).toEqual({ 'a': null });
+  test('should assign undefined properties', () => {
+    const obj = { a: undefined };
+    const source = { a: 1 };
+    defaultsDeep(obj, source);
+    expect(obj).toEqual({ a: 1 });
+  });
+
+  test('should mutate the destination object', () => {
+    const obj = {};
+    const source = { a: 1 };
+    const result = defaultsDeep(obj, source);
+    expect(result).toBe(obj);
+    expect(obj).toEqual({ a: 1 });
   });
 });
