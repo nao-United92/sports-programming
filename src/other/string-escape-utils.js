d97f453a34c697428e9fdf020cf8ed3ca208d2a6
nao-United92 <naoyauver@icloud.com>
2025-09-22 23:56:58 +0900

    refactor: Improve string utility functions

--- src/other/string-escape-utils.js
+++ src/other/string-escape-utils.js
@@ -1,41 +1,27 @@
-const htmlEscapes = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#39;'
+export const escapeHTML = (str) => {
+  return str.replace(
+    /[&<>"']/g,
+    (match) =>
+      ({
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+      }[match])
+  );
 };
 
-const unescapeMap = Object.fromEntries(Object.entries(htmlEscapes).map(([key, val]) => [val, key]));
-
-const reEscapedHtml = /[&<>"'']/g; // Escaped single quote
-const reUnescapedHtml = /&(?:amp|lt|gt|quot|#39);/g;
-
-/**
- * Converts the characters "&", "<", ">", '"', and "'" in `string` to their
- * corresponding HTML entities.
- *
- * @param {string} [string=''] The string to escape.
- * @returns {string} Returns the escaped string.
- */
-export const escape = (string) => {
-  const str = string == null ? '' : String(string);
-  return (str && reEscapedHtml.test(str))
-    ? str.replace(reEscapedHtml, (char) => htmlEscapes[char])
-    : str;
-};
-
-/**
- * The inverse of `escape`; this method converts the HTML entities
- * `&amp;`, `&lt;`, `&gt;`, `&quot;`, and `&#39;` in `string` to their
- * corresponding characters.
- *
- * @param {string} [string=''] The string to unescape.
- * @returns {string} Returns the unescaped string.
- */
-export const unescape = (string) => {
-  const str = string == null ? '' : String(string);
-  return (str && reUnescapedHtml.test(str))
-    ? str.replace(reUnescapedHtml, (entity) => unescapeMap[entity])
-    : str;
+export const unescapeHTML = (str) => {
+  return str.replace(
+    /&amp;|&lt;|&gt;|&quot;|&#39;/g,
+    (match) =>
+      ({
+        '&amp;': '&',
+        '&lt;': '<',
+        '&gt;': '>',
+        '&quot;': '"',
+        '&#39;': "'",
+      }[match])
+  );
 };
