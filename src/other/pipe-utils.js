dd337a7049073ccaa1a5bba7e82283f99e914166
nao-United92 <naoyauver@icloud.com>
2025-09-24 23:46:30 +0900

    feat(utils): add pipe utility

--- src/other/pipe-utils.js
+++ src/other/pipe-utils.js
@@ -1,18 +1,1 @@
-/**
- * Creates a function that returns the result of invoking the given functions from left to right.
- * The first function is invoked with the arguments of the created function, and each subsequent function is invoked with the result of the previous function.
- *
- * @param {...Function} funcs The functions to pipe.
- * @returns {Function} Returns the new composite function.
- */
-export const pipe = (...funcs) => {
-  if (funcs.length === 0) {
-    return (arg) => arg;
-  }
-
-  if (funcs.length === 1) {
-    return funcs[0];
-  }
-
-  return funcs.reduce((a, b) => (...args) => b(a(...args)));
-};
+export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
