14f799c6fe5b0c3e0bd7afae6076caac03e8088b
nao-United92 <naoyauver@icloud.com>
2025-09-24 23:50:19 +0900

    feat(utils): add omit utility

--- src/other/omit-utils.js
+++ src/other/omit-utils.js
@@ -1,25 +1,7 @@
-/**
- * Creates an object composed of the own and inherited enumerable property paths of object that are not omitted.
- *
- * @param {Object} object The source object.
- * @param {string[]} keys The property paths to omit.
- * @returns {Object} Returns the new object.
- * @example
- *
- * const object = { 'a': 1, 'b': '2', 'c': 3 };
- *
- * omit(object, ['a', 'c']);
- * // => { 'b': '2' }
- */
-function omit(object, keys) {
-  if (object == null) {
-    return {};
+export const omit = (obj, keys) => {
+  const result = { ...obj };
+  for (const key of keys) {
+    delete result[key];
   }
-  const newObj = { ...object };
-  keys.forEach(key => {
-    delete newObj[key];
-  });
-  return newObj;
-}
-
-export default omit;
+  return result;
+};
