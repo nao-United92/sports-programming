10135bc44bd13350aa675b1514b801e2eaf2635b
nao-United92 <naoyauver@icloud.com>
2025-09-26 00:07:11 +0900

    feat(sample): Add sample utility

--- src/other/sample-utils.test.js
+++ src/other/sample-utils.test.js
@@ -1,26 +1,20 @@
-import { sample } from './sample-utils.js';
+const { sample } = require('./sample-utils.js');
 
 describe('sample', () => {
-  it('should return a random element from the array', () => {
+  test('should return a random element from an array', () => {
     const array = [1, 2, 3, 4, 5];
     const result = sample(array);
     expect(array).toContain(result);
   });
 
-  it('should return undefined for an empty array', () => {
+  test('should return undefined for an empty array', () => {
     const array = [];
-    expect(sample(array)).toBeUndefined();
+    const result = sample(array);
+    expect(result).toBeUndefined();
   });
 
-  it('should return undefined for non-array input', () => {
+  test('should return undefined for a null or undefined array', () => {
     expect(sample(null)).toBeUndefined();
     expect(sample(undefined)).toBeUndefined();
-    expect(sample({})).toBeUndefined();
-    expect(sample(123)).toBeUndefined();
-  });
-
-  it('should work with an array of one element', () => {
-    const array = [1];
-    expect(sample(array)).toBe(1);
   });
 });
