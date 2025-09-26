2c360fa535e8506f583a4e6d29b523020307937c
nao-United92 <naoyauver@icloud.com>
2025-09-26 22:15:19 +0900

    feat(string-kebab-case): Add kebabCase utility

--- src/other/string-kebab-case-utils.js
+++ src/other/string-kebab-case-utils.js
@@ -1,6 +1,18 @@
-export const kebabCase = (str) => {
-  return str
-    .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
-    .toLowerCase()
-    .replace(/^-/, '');
-};
+/**
+ * Converts `string` to kebab case.
+ *
+ * @param {string} [string=''] The string to convert.
+ * @returns {string} Returns the kebab cased string.
+ */
+function kebabCase(string) {
+  if (typeof string !== 'string' || string.length === 0) {
+    return '';
+  }
+  return string
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/[
+_]+/g, '-')
+    .toLowerCase();
+}
+
+module.exports = { kebabCase };
--- src/other/string-kebab-case-utils.test.js
+++ src/other/string-kebab-case-utils.test.js
@@ -1,30 +1,25 @@
-import { kebabCase } from './string-kebab-case-utils.js';
+const { kebabCase } = require('./string-kebab-case-utils.js');
 
 describe('kebabCase', () => {
   test('should convert a string to kebab case', () => {
-    expect(kebabCase('foo bar')).toBe('foo-bar');
-    expect(kebabCase('foo-bar')).toBe('foo-bar');
-    expect(kebabCase('foo_bar')).toBe('foo-bar');
     expect(kebabCase('Foo Bar')).toBe('foo-bar');
-    expect(kebabCase('FooBar')).toBe('foo-bar');
     expect(kebabCase('fooBar')).toBe('foo-bar');
-    expect(kebabCase('foo-bar-baz')).toBe('foo-bar-baz');
-    expect(kebabCase('foo_bar_baz')).toBe('foo-bar-baz');
-    expect(kebabCase('Foo Bar Baz')).toBe('foo-bar-baz');
-    expect(kebabCase('FooBarBaz')).toBe('foo-bar-baz');
-    expect(kebabCase('fooBarBaz')).toBe('foo-bar-baz');
+    expect(kebabCase('__FOO_BAR__')).toBe('foo-bar');
+    expect(kebabCase('foo-bar')).toBe('foo-bar');
   });
 
-  test('should handle numbers', () => {
-    expect(kebabCase('foo-bar-123')).toBe('foo-bar-123');
-    expect(kebabCase('fooBar123')).toBe('foo-bar-123');
-  });
-
-  test('should handle empty strings', () => {
+  test('should handle empty string', () => {
     expect(kebabCase('')).toBe('');
   });
 
-  test('should handle leading/trailing spaces', () => {
-    expect(kebabCase(' foo bar ')).toBe('foo-bar');
+  test('should handle single word string', () => {
+    expect(kebabCase('foo')).toBe('foo');
+    expect(kebabCase('Foo')).toBe('foo');
+  });
+
+  test('should handle non-string input', () => {
+    expect(kebabCase(null)).toBe('');
+    expect(kebabCase(undefined)).toBe('');
+    expect(kebabCase(123)).toBe('');
   });
 });
