385b84d0eef76a307b826c2cafd8e037f45bf220
nao-United92 <naoyauver@icloud.com>
2025-09-26 22:19:05 +0900

    feat(string-truncate): Add truncate utility

--- src/other/string-truncate-utils.js
+++ src/other/string-truncate-utils.js
@@ -1,6 +1,22 @@
-export const truncate = (str, num) => {
-  if (str.length <= num) {
-    return str;
+/**
+ * Truncates `string` if it's longer than the given maximum string length.
+ * The last characters of the truncated string are replaced with the `omission` string
+ * which defaults to "...".
+ *
+ * @param {string} [string=''] The string to truncate.
+ * @param {Object} [options={}] The options object.
+ * @param {number} [options.length=30] The maximum string length.
+ * @param {string} [options.omission='...'] The string to indicate text is omitted.
+ * @returns {string} Returns the truncated string.
+ */
+function truncate(string, options = {}) {
+  const { length = 30, omission = '...' } = options;
+
+  if (typeof string !== 'string' || string.length <= length) {
+    return string;
   }
-  return str.slice(0, num) + '...';
-};
+
+  return string.slice(0, length - omission.length) + omission;
+}
+
+module.exports = { truncate };
