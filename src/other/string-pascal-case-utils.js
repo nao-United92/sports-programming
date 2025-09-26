9758a7a69f2f19b07f78bc82a96b10c04a772542
nao-United92 <naoyauver@icloud.com>
2025-09-26 22:17:13 +0900

    feat(string-pascal-case): Add pascalCase utility

--- src/other/string-pascal-case-utils.js
+++ src/other/string-pascal-case-utils.js
@@ -1,16 +1,16 @@
 /**
- * Converts a string to PascalCase.
+ * Converts `string` to PascalCase.
  *
- * @param {string} str The string to convert.
- * @returns {string} Returns the PascalCased string.
+ * @param {string} [string=''] The string to convert.
+ * @returns {string} Returns the Pascal cased string.
  */
-const pascalCase = (str) => {
-  if (typeof str !== 'string') {
+function pascalCase(string) {
+  if (typeof string !== 'string' || string.length === 0) {
     return '';
   }
-  return str
-    .replace(/[-_\s]+(.)?/g, (match, chr) => (chr ? chr.toUpperCase() : ''))
-    .replace(/^(.)/, (match) => match.toUpperCase());
-};
+  return string
+    .replace(/[^a-zA-Z0-9]+(.)?/g, (match, chr) => chr ? chr.toUpperCase() : '')
+    .replace(/^./, (match) => match.toUpperCase());
+}
 
-export { pascalCase };
+module.exports = { pascalCase };
