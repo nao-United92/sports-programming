ba2da4e65326c7d1430a9eb7412d17af0b86983b
nao-United92 <naoyauver@icloud.com>
2025-09-26 22:14:42 +0900

    feat(string-capitalize): Add capitalize utility

---
+++ src/other/string-capitalize-utils.js
@@ -0,0 +1,15 @@
+/**
+ * Converts the first character of `string` to upper case and the remaining
+ * to lower case.
+ *
+ * @param {string} [string=''] The string to capitalize.
+ * @returns {string} Returns the capitalized string.
+ */
+function capitalize(string) {
+  if (typeof string !== 'string' || string.length === 0) {
+    return '';
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+module.exports = { capitalize };
