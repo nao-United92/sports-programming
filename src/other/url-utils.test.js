320206d5dc0df9cac7e4cdafbd0ec661fce16cb0
nao-United92 <naoyauver@icloud.com>
2025-09-27 21:48:12 +0900

    feat: Add url query parser utility

--- src/other/url-utils.test.js
+++ src/other/url-utils.test.js
@@ -1,49 +1,28 @@
-const { getURLParameters, isAbsoluteURL, combineURLs } = require('./url-utils.js');
+import { parseQuery } from './url-utils';
 
-describe('getURLParameters', () => {
-  it('should get the parameters of a URL', () => {
-    const url = 'https://www.google.com/search?q=javascript&oq=javascript';
-    const params = getURLParameters(url);
-    expect(params).toEqual({ q: 'javascript', oq: 'javascript' });
+describe('parseQuery', () => {
+  test('should parse a full URL', () => {
+    const url = 'https://example.com?foo=bar&baz=qux';
+    expect(parseQuery(url)).toEqual({ foo: 'bar', baz: 'qux' });
   });
 
-  it('should handle a URL with no parameters', () => {
-    const url = 'https://www.google.com';
-    const params = getURLParameters(url);
-    expect(params).toEqual({});
-  });
-});
-
-describe('isAbsoluteURL', () => {
-  it('should return true for an absolute URL', () => {
-    expect(isAbsoluteURL('https://www.google.com')).toBe(true);
-    expect(isAbsoluteURL('ftp://ftp.example.com')).toBe(true);
+  test('should parse just a query string', () => {
+    const queryString = 'foo=bar&baz=qux';
+    expect(parseQuery(queryString)).toEqual({ foo: 'bar', baz: 'qux' });
   });
 
-  it('should return false for a relative URL', () => {
-    expect(isAbsoluteURL('/path/to/file')).toBe(false);
-    expect(isAbsoluteURL('path/to/file')).toBe(false);
-  });
-});
-
-describe('combineURLs', () => {
-  it('should combine a base URL and a relative URL', () => {
-    expect(combineURLs('https://www.google.com', '/search')).toBe('https://www.google.com/search');
+  test('should handle URI encoded components', () => {
+    const url = 'https://example.com?name=%E3%83%86%E3%82%B9%E3%83%88&value=a%26b';
+    expect(parseQuery(url)).toEqual({ name: 'テスト', value: 'a&b' });
   });
 
-  it('should handle trailing slashes in the base URL', () => {
-    expect(combineURLs('https://www.google.com/', '/search')).toBe('https://www.google.com/search');
+  test('should handle params without values', () => {
+    const url = 'https://example.com?foo&bar=baz';
+    expect(parseQuery(url)).toEqual({ foo: true, bar: 'baz' });
   });
 
-  it('should handle leading slashes in the relative URL', () => {
-    expect(combineURLs('https://www.google.com', 'search')).toBe('https://www.google.com/search');
-  });
-
-  it('should handle both trailing and leading slashes', () => {
-    expect(combineURLs('https://www.google.com/', 'search')).toBe('https://www.google.com/search');
-  });
-
-  it('should return the base URL if the relative URL is not provided', () => {
-    expect(combineURLs('https://www.google.com')).toBe('https://www.google.com');
+  test('should handle empty query string', () => {
+    const url = 'https://example.com';
+    expect(parseQuery(url)).toEqual({});
   });
 });
