32d59dd130d67b60de16c7800edfef54aae00cdd
nao-United92 <naoyauver@icloud.com>
2025-10-03 22:52:14 +0900

    feat: Add once utility    2    3 Adds a  utility to create a function that can only be called one time.    4 Includes unit tests.

--- src/other/function-once-utils.js
+++ src/other/function-once-utils.js
@@ -1,21 +1,14 @@
-/**
- * Creates a function that is restricted to invoking func once. Repeat calls to the function return the value of the first invocation.
- * The func is invoked with the this binding and arguments of the created function.
- *
- * @param {Function} func The function to restrict.
- * @returns {Function} Returns the new restricted function.
- */
-const once = (func) => {
+function once(fn) {
   let hasBeenCalled = false;
   let result;
 
   return function(...args) {
     if (!hasBeenCalled) {
       hasBeenCalled = true;
-      result = func.apply(this, args);
+      result = fn.apply(this, args);
     }
     return result;
   };
-};
+}
 
-export { once };
+module.exports = { once };
