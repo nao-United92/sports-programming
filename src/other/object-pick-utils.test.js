c34ee4492954756b33bf169e596927299cf4e111
nao-United92 <naoyauver@icloud.com>
2025-10-04 22:58:17 +0900

    feat: Improve pick utility and add tests
    - Update null/undefined check in `pick` function to be more strict.
    - Add test cases for when no keys are picked and when the keys array is empty.
    - Improve test descriptions for clarity.

--- src/other/object-pick-utils.test.js
+++ src/other/object-pick-utils.test.js
@@ -6,13 +6,23 @@ describe('pick', () => {
     expect(pick(object, ['a', 'c'])).toEqual({ 'a': 1, 'c': 3 });
   });
 
-  it('should ignore keys that do not exist', () => {
+  it('should ignore keys that are not in the object', () => {
     const object = { 'a': 1, 'b': '2' };
     expect(pick(object, ['a', 'd'])).toEqual({ 'a': 1 });
   });
 
-  it('should return an empty object if the source object is null or undefined', () => {
+  it('should return an empty object if no keys are picked', () => {
+    const object = { 'a': 1, 'b': '2' };
+    expect(pick(object, [])).toEqual({});
+  });
+
+  it('should return an empty object for null or undefined input object', () => {
     expect(pick(null, ['a', 'b'])).toEqual({});
     expect(pick(undefined, ['a', 'b'])).toEqual({});
+  });
+
+  it('should work with an empty keys array', () => {
+    const object = { 'a': 1, 'b': '2' };
+    expect(pick(object, [])).toEqual({});
   });
 });
