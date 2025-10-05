e0786c68534ef9372baf923eba24d83b1423e2a6
nao-United92 <naoyauver@icloud.com>
2025-10-05 23:54:00 +0900

    feat(object-utils): Add pick and omit utility functions

--- src/other/object-utils.js
+++ src/other/object-utils.js
@@ -1,60 +1,34 @@
 /**
- * Performs a deep merge of two objects.
- *
- * @param {object} target The target object to merge into.
- * @param {object} source The source object to merge from.
- * @returns {object} The merged object.
+ * Creates an object composed of the picked object properties.
+ * @param {Object} obj The source object.
+ * @param {string[]} keys The properties to pick.
+ * @returns {Object} Returns the new object.
  */
-export const deepMerge = (target, source) => {
-  const output = { ...target };
-
-  if (isObject(target) && isObject(source)) {
-    Object.keys(source).forEach(key => {
-      if (isObject(source[key])) {
-        if (!(key in target)) {
-          Object.assign(output, { [key]: source[key] });
-        } else {
-          output[key] = deepMerge(target[key], source[key]);
-        }
-      } else {
-        Object.assign(output, { [key]: source[key] });
-      }
-    });
+export const pick = (obj, keys) => {
+  if (obj === null || obj === undefined) {
+    return {};
   }
-
-  return output;
-};
-
-const isObject = (item) => {
-  return (item && typeof item === 'object' && !Array.isArray(item));
-};
-
-/**
- * Creates an object composed of the own enumerable string keyed properties of `object` that are not `keys`.
- *
- * @param {object} object The source object.
- * @param {Array<string>} keys The property keys to omit.
- * @returns {object} Returns the new object.
- */
-export const omit = (object, keys) => {
-  const newObject = { ...object };
-  keys.forEach(key => delete newObject[key]);
-  return newObject;
-};
-
-/**
- * Creates an object composed of the picked `object` properties.
- *
- * @param {object} object The source object.
- * @param {Array<string>} keys The property keys to pick.
- * @returns {object} Returns the new object.
- */
-export const pick = (object, keys) => {
-  const newObject = {};
-  keys.forEach(key => {
-    if (Object.prototype.hasOwnProperty.call(object, key)) {
-      newObject[key] = object[key];
+  return keys.reduce((acc, key) => {
+    if (obj.hasOwnProperty(key)) {
+      acc[key] = obj[key];
     }
+    return acc;
+  }, {});
+};
+
+/**
+ * Creates an object composed of the own and inherited enumerable property paths of object that are not omitted.
+ * @param {Object} obj The source object.
+ * @param {string[]} keys The properties to omit.
+ * @returns {Object} Returns the new object.
+ */
+export const omit = (obj, keys) => {
+  if (obj === null || obj === undefined) {
+    return {};
+  }
+  const newObj = { ...obj };
+  keys.forEach(key => {
+    delete newObj[key];
   });
-  return newObject;
+  return newObj;
 };
