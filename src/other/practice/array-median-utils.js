6718e056ce4a9ded73b679c6f74cdb8dbba193f7
nao-United92 <naoyauver@icloud.com>
2025-10-07 23:59:26 +0900

    feat: Add median utility function

---
+++ src/other/practice/array-median-utils.js
@@ -0,0 +1,5 @@
+export const median = (arr) => {
+  const sorted = [...arr].sort((a, b) => a - b);
+  const mid = Math.floor(sorted.length / 2);
+  return sorted.length % 2 !== 0 ? sorted[mid] : (sorted[mid - 1] + sorted[mid]) / 2;
+};
