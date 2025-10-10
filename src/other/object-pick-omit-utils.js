2aad69eac6e1d8fe9de5b75c471a7660ea7c7506
nao-United92 <naoyauver@icloud.com>
2025-10-10 18:57:19 +0900

    feat: Add pick and omit utility functions

--- src/other/object-pick-omit-utils.js
+++ src/other/object-pick-omit-utils.js
@@ -2,36 +2,43 @@
  * Creates an object composed of the picked object properties.
  *
  * @param {Object} obj The source object.
- * @param {string[]} keys The property paths to pick.
+ * @param {string[]} keys The property keys to pick.
  * @returns {Object} Returns the new object.
  */
-export const pick = (obj, keys) => {
-  if (obj === null || obj === undefined) {
-    return {};
-  }
+function pick(obj, keys) {
   const result = {};
-  for (const key of keys) {
+  if (obj == null) {
+    return result;
+  }
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
     if (key in obj) {
       result[key] = obj[key];
     }
   }
   return result;
-};
+}
 
 /**
- * Creates an object composed of the own and inherited enumerable property paths of object that are not omitted.
+ * Creates an object composed of the own and inherited enumerable property paths
+ * of `object` that are not omitted.
  *
  * @param {Object} obj The source object.
- * @param {string[]} keys The property paths to omit.
+ * @param {string[]} keys The property keys to omit.
  * @returns {Object} Returns the new object.
  */
-export const omit = (obj, keys) => {
-  if (obj === null || obj === undefined) {
+function omit(obj, keys) {
+  if (obj == null) {
     return {};
   }
-  const result = { ...obj };
-  for (const key of keys) {
-    delete result[key];
+  const keySet = new Set(keys);
+  const result = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key) && !keySet.has(key)) {
+      result[key] = obj[key];
+    }
   }
   return result;
-};
+}
+
+module.exports = { pick, omit };
