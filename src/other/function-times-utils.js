940ec28dd8906ab7ccb7f65546b5542b5894a36d
nao-United92 <naoyauver@icloud.com>
2025-10-10 19:14:41 +0900

    feat: Add times and noop utility functions

--- src/other/function-times-utils.js
+++ src/other/function-times-utils.js
@@ -1,19 +1,20 @@
 /**
- * Invokes the iteratee n times, returning an array of the results of each invocation.
- * The iteratee is invoked with one argument: (index).
+ * Invokes the iteratee `n` times, returning an array of the results of
+ * each invocation. The iteratee is invoked with one argument: `index`.
  *
- * @param {number} n The number of times to invoke iteratee.
- * @param {Function} iteratee The function to invoke.
+ * @param {number} n The number of times to invoke `iteratee`.
+ * @param {Function} iteratee The function invoked per iteration.
  * @returns {Array} Returns the array of results.
  */
-const times = (n, iteratee) => {
+function times(n, iteratee) {
+  n = Math.floor(n);
+  const result = Array(n < 0 ? 0 : n);
   let index = -1;
-  const result = new Array(n > 0 ? n : 0);
 
   while (++index < n) {
     result[index] = iteratee(index);
   }
   return result;
-};
+}
 
-export { times };
+module.exports = { times };
--- src/other/function-times-utils.test.js
+++ src/other/function-times-utils.test.js
@@ -1,34 +1,45 @@
-import { times } from './function-times-utils.js';
+const { times } = require('./function-times-utils.js');
 
 describe('times', () => {
-  test('should invoke the iteratee n times', () => {
-    const iteratee = jest.fn();
-    times(5, iteratee);
-    expect(iteratee).toHaveBeenCalledTimes(5);
-  });
+  test('should invoke the iteratee n times and return an array of results', () => {
+    const iteratee = jest.fn(index => index * 2);
+    const result = times(3, iteratee);
 
-  test('should return an array of the results of each invocation', () => {
-    const result = times(5, String);
-    expect(result).toEqual(['0', '1', '2', '3', '4']);
-  });
-
-  test('should pass the index to the iteratee', () => {
-    const iteratee = jest.fn();
-    times(3, iteratee);
+    expect(iteratee).toHaveBeenCalledTimes(3);
     expect(iteratee).toHaveBeenCalledWith(0);
     expect(iteratee).toHaveBeenCalledWith(1);
     expect(iteratee).toHaveBeenCalledWith(2);
+    expect(result).toEqual([0, 2, 4]);
   });
 
   test('should return an empty array if n is 0', () => {
     const iteratee = jest.fn();
     const result = times(0, iteratee);
-    expect(result).toEqual([]);
+
     expect(iteratee).not.toHaveBeenCalled();
+    expect(result).toEqual([]);
   });
 
   test('should return an empty array if n is negative', () => {
-    const result = times(-1, String);
+    const iteratee = jest.fn();
+    const result = times(-5, iteratee);
+
+    expect(iteratee).not.toHaveBeenCalled();
     expect(result).toEqual([]);
+  });
+
+  test('should handle non-integer n by flooring it', () => {
+    const iteratee = jest.fn(index => index + 1);
+    const result = times(2.9, iteratee);
+
+    expect(iteratee).toHaveBeenCalledTimes(2);
+    expect(result).toEqual([1, 2]);
+  });
+
+  test('should return an array of undefined if iteratee does not return a value', () => {
+    const iteratee = jest.fn();
+    const result = times(2, iteratee);
+
+    expect(result).toEqual([undefined, undefined]);
   });
 });
