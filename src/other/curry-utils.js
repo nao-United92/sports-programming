5624aae7fb91418bbcc054295103b2328d4ab51d
nao-United92 <naoyauver@icloud.com>
2025-10-16 22:37:20 +0900

    feat: Add curry utility function
    Adds a `curry` higher-order function that transforms a function with multiple arguments into a sequence of nested functions, each taking a single argument. This allows for partial application of the function's arguments.
    
    Includes corresponding tests to verify the currying behavior.

--- src/other/curry-utils.js
+++ src/other/curry-utils.js
@@ -1,33 +1,9 @@
-const curry = (fn, arity = fn.length) => {
-  const placeholder = Symbol ? Symbol.for('_') : '_';
-
-  function curried(...args) {
-    const argsSanitized = args.slice(0, arity);
-    const hasPlaceholder = argsSanitized.includes(placeholder);
-    
-    if (!hasPlaceholder && argsSanitized.length >= arity) {
-      return fn.apply(this, argsSanitized);
+export const curry = (fn) => {
+  const curried = (...args) => {
+    if (args.length >= fn.length) {
+      return fn(...args);
     }
-
-    return function(...nextArgs) {
-      const newArgs = [...argsSanitized];
-      for (let i = 0; i < newArgs.length; i++) {
-        if (newArgs[i] === placeholder && nextArgs.length) {
-          newArgs[i] = nextArgs.shift();
-        }
-      }
-      return curried.apply(this, [...newArgs, ...nextArgs]);
-    };
-  }
-  
-  Object.defineProperty(curried, 'placeholder', {
-      value: placeholder,
-      writable: false,
-      enumerable: false,
-      configurable: false
-  });
-
+    return (...nextArgs) => curried(...args, ...nextArgs);
+  };
   return curried;
 };
-
-module.exports = { curry };
