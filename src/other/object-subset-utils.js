c4b53ff9c4c64cc19c1ca74e2479a05d421d4518
nao-United92 <naoyauver@icloud.com>
2025-10-19 23:57:23 +0900

    feat(object): add pick and omit utilities

--- src/other/object-subset-utils.js
+++ src/other/object-subset-utils.js
@@ -1,15 +1,33 @@
+/**
+ * Creates an object composed of the picked object properties.
+ * @param {Object} obj The source object.
+ * @param {string[]} keys The property keys to pick.
+ * @returns {Object} Returns the new object.
+ */
 const pick = (obj, keys) => {
   const result = {};
+  if (obj == null) {
+    return result;
+  }
   for (const key of keys) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+    if (key in obj) {
       result[key] = obj[key];
     }
   }
   return result;
 };
 
+/**
+ * Creates an object composed of the own and inherited enumerable property paths of object that are not omitted.
+ * @param {Object} obj The source object.
+ * @param {string[]} keys The property keys to omit.
+ * @returns {Object} Returns the new object.
+ */
 const omit = (obj, keys) => {
   const result = { ...obj };
+  if (obj == null) {
+    return {};
+  }
   for (const key of keys) {
     delete result[key];
   }
