4943dafaed7f71eb753e4bf0e79796bf620ee33c
nao-United92 <naoyauver@icloud.com>
2025-10-22 00:22:57 +0900

    feat: Add get utility for nested objects with tests

--- src/other/object-get-utils.js
+++ src/other/object-get-utils.js
@@ -1,28 +1,11 @@
-/**
- * Gets the value at `path` of `object`. If the resolved value is
- * `undefined`, the `defaultValue` is returned in its place.
- *
- * @param {object} object The object to query.
- * @param {Array|string} path The path of the property to retrieve.
- * @param {*} [defaultValue] The value returned for `undefined` resolved values.
- * @returns {*} Returns the resolved value.
- */
-function get(object, path, defaultValue) {
-  // Convert path to an array of keys, handling bracket notation
-  const pathArray = Array.isArray(path)
-    ? path
-    : path.replace(/\[(.*?)\]/g, '.$1').split('.');
+const get = (obj, path, defaultValue = undefined) => {
+  const pathArray = Array.isArray(path) ? path : path.split('.').filter(i => i.length);
 
-  const result = pathArray.reduce((obj, key) => {
-    if (obj === null || obj === undefined) {
-      return undefined;
-    }
-    // Trim brackets and quotes from keys, if any, to handle array indices
-    const cleanKey = key.replace(/^['"`]?|['"`]?$/g, '');
-    return obj[cleanKey];
-  }, object);
+  const result = pathArray.reduce((acc, key) => {
+    return (acc && acc[key] !== 'undefined') ? acc[key] : undefined;
+  }, obj);
 
   return result === undefined ? defaultValue : result;
-}
+};
 
 module.exports = { get };
