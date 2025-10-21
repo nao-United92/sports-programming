fccf9fbcee8d97895af9d8017edbcf6874fc3eaa
nao-United92 <naoyauver@icloud.com>
2025-10-21 23:51:44 +0900

    feat: Add kebabCase utility and remove unused string utils

--- src/other/string-utils.js
+++ src/other/string-utils.js
@@ -1,29 +1,6 @@
-/**
- * Truncates a string to a specified length, appending a suffix if needed.
- * @param {string} str The string to truncate.
- * @param {number} maxLength The maximum length of the string.
- * @param {string} [suffix='...'] The suffix to append if the string is truncated.
- * @returns {string} The truncated string.
- */
-export const truncate = (str, maxLength, suffix = '...') => {
-  if (str.length <= maxLength) {
-    return str;
-  }
-  if (maxLength <= suffix.length) {
-    return suffix;
-  }
-  return str.slice(0, maxLength - suffix.length) + suffix;
-};
+const kebabCase = str => str
+  .replace(/([a-z])([A-Z])/g, '$1-$2')
+  .replace(/[\s_]+/g, '-')
+  .toLowerCase();
 
-/**
- * Converts a string to camelCase.
- * @param {string} str The string to convert.
- * @returns {string} The camelCased string.
- */
-export const toCamelCase = (str) => {
-  return str.replace(/([-_][a-z])/ig, ($1) => {
-    return $1.toUpperCase()
-      .replace('-', '')
-      .replace('_', '');
-  });
-};
+export { kebabCase };
--- src/other/string-utils.test.js
+++ src/other/string-utils.test.js
@@ -1,46 +1,27 @@
-import { truncate, toCamelCase } from './string-utils';
+import { kebabCase } from './string-utils.js';
 
-describe('String Utilities', () => {
-  describe('truncate', () => {
-    it('should not truncate a string shorter than the max length', () => {
-      expect(truncate('hello', 10)).toBe('hello');
-    });
-
-    it('should truncate a string longer than the max length', () => {
-      expect(truncate('hello world', 8)).toBe('hello...');
-    });
-
-    it('should use a custom suffix', () => {
-      expect(truncate('hello world', 8, '!')).toBe('hello w!');
-    });
-
-    it('should return the suffix if maxLength is less than or equal to suffix length', () => {
-      expect(truncate('hello world', 3)).toBe('...');
-      expect(truncate('hello world', 2)).toBe('...');
-    });
+describe('kebabCase', () => {
+  test('should convert camelCase to kebab-case', () => {
+    expect(kebabCase('camelCase')).toBe('camel-case');
   });
 
-  describe('toCamelCase', () => {
-    it('should convert a kebab-case string to camelCase', () => {
-      expect(toCamelCase('hello-world')).toBe('helloWorld');
-    });
+  test('should convert strings with spaces to kebab-case', () => {
+    expect(kebabCase('some text')).toBe('some-text');
+  });
 
-    it('should convert a snake_case string to camelCase', () => {
-      expect(toCamelCase('hello_world')).toBe('helloWorld');
-    });
+  test('should convert strings with underscores to kebab-case', () => {
+    expect(kebabCase('some_text')).toBe('some-text');
+  });
 
-    it('should handle already camelCased strings', () => {
-      expect(toCamelCase('helloWorld')).toBe('helloWorld');
-    });
+  test('should handle already kebab-cased strings', () => {
+    expect(kebabCase('kebab-case')).toBe('kebab-case');
+  });
 
-    it('should handle strings with leading dashes or underscores', () => {
-      expect(toCamelCase('--hello-world')).toBe('--helloWorld');
-      expect(toCamelCase('__hello_world')).toBe('__helloWorld');
-    });
+  test('should handle strings with multiple spaces or underscores', () => {
+    expect(kebabCase('some  __text')).toBe('some-text');
+  });
 
-    it('should handle strings with mixed cases', () => {
-      expect(toCamelCase('foo-bar-baz')).toBe('fooBarBaz');
-      expect(toCamelCase('FOO_BAR_BAZ')).toBe('FOOBarBaz');
-    });
+  test('should convert PascalCase to kebab-case', () => {
+    expect(kebabCase('PascalCase')).toBe('pascal-case');
   });
 });
