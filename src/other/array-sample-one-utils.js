addc18672c21d7eca930f1bbca8a19a6385d2d95
nao-United92 <naoyauver@icloud.com>
2025-10-23 22:59:50 +0900

    feat: Add array sample one utility

---
+++ src/other/array-sample-one-utils.js
@@ -0,0 +1,7 @@
+export const sample = (array) => {
+  if (!Array.isArray(array) || array.length === 0) {
+    return undefined;
+  }
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
