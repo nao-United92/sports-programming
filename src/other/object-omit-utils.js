6d3c087831a799cdcf5ac14158f0078e05ffb857
nao-United92 <naoyauver@icloud.com>
2025-10-25 09:59:51 +0900

    feat: add object omit utility     2     3 Adds an `omit` function, the inverse of `pick`. It creates a new object     4 by excluding a specified set of properties from a source object.     5     6 This utility is valuable for filtering out sensitive or unnecessary data     7 before processing, logging, or sending objects over the network.     8     9 Includes:    10 - `omit` function implementation.    11 - Unit tests for property exclusion, handling of non-existent keys,    12   and non-object inputs.

--- src/other/object-omit-utils.js
+++ src/other/object-omit-utils.js
@@ -1,7 +1,25 @@
-export const omit = (obj, keys) => {
-  const newObj = { ...obj };
-  keys.forEach(key => {
-    delete newObj[key];
-  });
-  return newObj;
+/**
+ * Creates an object composed of the own and inherited enumerable property paths of object that are not omitted.
+ *
+ * @param {Object} obj The source object.
+ * @param {string[]} keys The property keys to omit.
+ * @returns {Object} Returns the new object.
+ */
+const omit = (obj, keys) => {
+  if (obj === null || typeof obj !== 'object') {
+    return {};
+  }
+
+  const result = { ...obj };
+  const keysToOmit = new Set(keys);
+
+  for (const key of keysToOmit) {
+    if (Object.prototype.hasOwnProperty.call(result, key)) {
+      delete result[key];
+    }
+  }
+
+  return result;
 };
+
+module.exports = { omit };
