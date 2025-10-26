67860a1f701cc737dddac2646d25f6ef661fab84
nao-United92 <naoyauver@icloud.com>
2025-10-26 23:56:55 +0900

    feat(utils): Add debounce utility function

--- src/other/debounce-utils.js
+++ src/other/debounce-utils.js
@@ -1,23 +1,24 @@
 /**
- * Creates a debounced function that delays invoking `func` until after `wait` milliseconds have elapsed since the last time the debounced function was invoked.
+ * Creates a debounced function that delays invoking `func` until after `wait`
+ * milliseconds have elapsed since the last time the debounced function was
+ * invoked.
  *
  * @param {Function} func The function to debounce.
  * @param {number} wait The number of milliseconds to delay.
- * @param {boolean} immediate If `true`, trigger the function on the leading edge, instead of the trailing.
  * @returns {Function} Returns the new debounced function.
  */
-export function debounce(func, wait, immediate) {
+export const debounce = (func, wait) => {
   let timeout;
-  return function() {
+
+  return function executedFunction(...args) {
     const context = this;
-    const args = arguments;
+
     const later = function() {
       timeout = null;
-      if (!immediate) func.apply(context, args);
+      func.apply(context, args);
     };
-    const callNow = immediate && !timeout;
+
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
   };
-}
+};
