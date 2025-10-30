cfd26f17b7a844420b24f1da2fafd53b5448ba79
nao-United92 <naoyauver@icloud.com>
2025-10-30 21:35:13 +0900

    feat: Add isPlainObject utility
    Add an isPlainObject function to check if a value is a plain JavaScript object.
    
    Includes comprehensive unit tests for various data types and edge cases.

--- src/other/type-is-plain-object-utils.js
+++ src/other/type-is-plain-object-utils.js
@@ -1,4 +1,11 @@
-const isPlainObject = (value) => {
+/**
+ * Checks if `value` is a plain object, that is, an object created by the
+ * `Object` constructor or one with a `[[Prototype]]` of `null`.
+ *
+ * @param {any} value The value to check.
+ * @returns {boolean} Returns `true` if `value` is a plain object, else `false`.
+ */
+function isPlainObject(value) {
   if (typeof value !== 'object' || value === null) {
     return false;
   }
@@ -10,6 +17,6 @@ const isPlainObject = (value) => {
 
   const Ctor = Object.prototype.hasOwnProperty.call(proto, 'constructor') && proto.constructor;
   return typeof Ctor === 'function' && Ctor instanceof Ctor && Function.prototype.toString.call(Ctor) === Function.prototype.toString.call(Object);
-};
+}
 
 module.exports = { isPlainObject };
