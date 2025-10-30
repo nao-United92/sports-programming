49d1844f28e93f45bef1d2c32294a309c4ed6036
nao-United92 <naoyauver@icloud.com>
2025-10-30 21:34:25 +0900

    feat: Add throttle utility
    Add a throttle function to limit the rate at which a function is called.
    
    Includes leading/trailing options and a cancel method, with comprehensive unit tests.

--- src/other/function-throttle-utils.js
+++ src/other/function-throttle-utils.js
@@ -2,55 +2,36 @@
  * Creates a throttled function that only invokes `func` at most once per
  * every `wait` milliseconds. The throttled function comes with a `cancel`
  * method to cancel delayed `func` invocations and a `flush` method to
- * immediately invoke them. Provide `options` to indicate whether `func` should
- * be invoked on the leading and/or trailing edge of the `wait` timeout.
- * The `func` is invoked with the last arguments provided to the throttled
- * function. Subsequent calls to the throttled function return the result of
- * the last `func` invocation.
+ * immediately invoke them. Provide `options` to indicate whether `func`
+ * should be invoked on the leading and/or trailing edge of the `wait`
+ * timeout. The `func` is invoked with the last arguments provided to the
+ * throttled function. Subsequent calls to the throttled function return
+ * the result of the last `func` invocation.
  *
- * **Note:** If `leading` and `trailing` options are `true`, `func` is invoked
- * on the trailing edge of the timeout only if the throttled function is
- * invoked more than once during the `wait` timeout.
+ * **Note:** If `leading` and `trailing` options are `true`, `func` is
+ * invoked on the trailing edge of the timeout only if the throttled function
+ * is invoked more than once during the `wait` timeout.
  *
  * @param {Function} func The function to throttle.
- * @param {number} [wait=0] The number of milliseconds to throttle invocations to.
+ * @param {number} wait The number of milliseconds to throttle invocations to.
  * @param {Object} [options={}] The options object.
  * @param {boolean} [options.leading=true] Specify invoking on the leading edge of the timeout.
  * @param {boolean} [options.trailing=true] Specify invoking on the trailing edge of the timeout.
  * @returns {Function} Returns the new throttled function.
  */
-function throttle(func, wait = 0, options = {}) {
-  let leading = true,
-    trailing = true;
-
-  if (typeof func !== 'function') {
-    throw new TypeError('Expected a function');
-  }
-  if (typeof options === 'object') {
-    leading = 'leading' in options ? !!options.leading : leading;
-    trailing = 'trailing' in options ? !!options.trailing : trailing;
-  }
-
-  let timerId;
+function throttle(func, wait, options = {}) {
+  let timeoutId;
   let lastArgs;
   let lastThis;
   let lastResult;
   let lastCallTime = 0;
 
+  const leading = options.leading === undefined ? true : !!options.leading;
+  const trailing = options.trailing === undefined ? true : !!options.trailing;
+
   const invokeFunc = (time) => {
     lastResult = func.apply(lastThis, lastArgs);
     lastCallTime = time;
-  };
-
-  const trailingEdge = (time) => {
-    timerId = undefined;
-    // Only invoke if we have `lastArgs` (i.e. throttle has been called)
-    // and `trailing` is set.
-    if (trailing && lastArgs) {
-      return invokeFunc(time);
-    }
-    lastArgs = lastThis = undefined;
-    return lastResult;
   };
 
   const throttled = function(...args) {
@@ -65,33 +46,30 @@ function throttle(func, wait = 0, options = {}) {
     const remaining = wait - (now - lastCallTime);
 
     if (remaining <= 0 || remaining > wait) {
-      if (timerId) {
-        clearTimeout(timerId);
-        timerId = undefined;
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+        timeoutId = null;
       }
       lastCallTime = now;
       invokeFunc(now);
-    } else if (!timerId && trailing) {
-      timerId = setTimeout(() => trailingEdge(Date.now()), remaining);
+    } else if (!timeoutId && trailing) {
+      timeoutId = setTimeout(() => {
+        lastCallTime = Date.now();
+        timeoutId = null;
+        invokeFunc(lastCallTime);
+      }, remaining);
     }
+
     return lastResult;
   };
 
   throttled.cancel = () => {
-    clearTimeout(timerId);
-    timerId = undefined;
+    clearTimeout(timeoutId);
+    timeoutId = null;
     lastCallTime = 0;
-    lastArgs = lastThis = undefined;
-  };
-
-  throttled.flush = () => {
-    if (timerId) {
-      return trailingEdge(Date.now());
-    }
-    return lastResult;
   };
 
   return throttled;
 }
 
-export { throttle };
+module.exports = { throttle };
