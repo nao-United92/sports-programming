8b1395f584ea7441d5ccbe3c10265166f88e3c46
nao-United92 <naoyauver@icloud.com>
2025-11-01 10:49:03 +0900

    feat: Add compose utility function

--- src/other/compose-utils.js
+++ src/other/compose-utils.js
@@ -1,1 +1,9 @@
-export const compose = (...fns) => (x) => fns.reduceRight((v, f) => f(v), x);
+/**
+ * Performs right-to-left function composition. The rightmost function may have
+ * any arity; the remaining functions must be unary.
+ *
+ * @param {...Function} fns The functions to compose.
+ * @returns {Function} Returns the new composite function.
+ */
+export const compose = (...fns) => (initialValue) =>
+  fns.reduceRight((acc, fn) => fn(acc), initialValue);
