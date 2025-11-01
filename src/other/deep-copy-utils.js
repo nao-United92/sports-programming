ec16cfa512b2ecdd9f06b1b92aa4ce212e4d2565
nao-United92 <naoyauver@icloud.com>
2025-11-01 22:43:24 +0900

    feat: Add deepCopy utility and tests

---
+++ src/other/deep-copy-utils.js
@@ -0,0 +1,25 @@
+const deepCopy = (obj) => {
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  if (obj instanceof Date) {
+    return new Date(obj.getTime());
+  }
+
+  if (obj instanceof RegExp) {
+    return new RegExp(obj);
+  }
+
+  const copiedObj = Array.isArray(obj) ? [] : {};
+
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      copiedObj[key] = deepCopy(obj[key]);
+    }
+  }
+
+  return copiedObj;
+};
+
+export { deepCopy };
