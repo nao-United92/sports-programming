cc612ff026b96b0cef35fd433ed5f12366d97500
nao-United92 <naoyauver@icloud.com>
2025-11-05 00:22:38 +0900

    feat(utils): Add function composition and object has utilities

--- src/other/function-composition-utils.js
+++ src/other/function-composition-utils.js
@@ -1,19 +1,26 @@
+/**
+ * Creates a function that returns the result of invoking the given functions
+ * from left to right. Each successive invocation is supplied the return value
+ * of the previous.
+ *
+ * @param {...Function} funcs The functions to invoke.
+ * @returns {Function} Returns the new composite function.
+ */
 export const flow = (...funcs) => {
   return (...args) => {
-    let result = funcs[0](...args);
-    for (let i = 1; i < funcs.length; i++) {
-      result = funcs[i](result);
-    }
-    return result;
+    const [first, ...rest] = funcs.flat();
+    if (!first) return undefined;
+    return rest.reduce((result, func) => func(result), first(...args));
   };
 };
 
+/**
+ * This method is like `flow` except that it creates a function that
+ * invokes the given functions from right to left.
+ *
+ * @param {...Function} funcs The functions to invoke.
+ * @returns {Function} Returns the new composite function.
+ */
 export const flowRight = (...funcs) => {
-  return (...args) => {
-    let result = funcs[funcs.length - 1](...args);
-    for (let i = funcs.length - 2; i >= 0; i--) {
-      result = funcs[i](result);
-    }
-    return result;
-  };
+  return flow(...funcs.flat().reverse());
 };
--- src/other/object-has-utils.js
+++ src/other/object-has-utils.js
@@ -1,34 +1,10 @@
-// This is a simplified version. A robust implementation would handle brackets and quotes.
-const stringToPath = (string) => {
-  return string.replace(/\[(.*?)\]/g, '.$1').split('.');
-}
-
 /**
- * Checks if `path` is a direct property of `object`.
+ * Checks if `key` is a direct property of `object`.
  *
  * @param {object} object The object to query.
- * @param {Array|string} path The path to check.
- * @returns {boolean} Returns `true` if `path` exists, else `false`.
+ * @param {string} key The key to check.
+ * @returns {boolean} Returns `true` if `key` exists, else `false`.
  */
-const has = (object, path) => {
-  const pathArray = Array.isArray(path) ? path : stringToPath(path);
-  if (!pathArray.length) {
-    return false;
-  }
-
-  let current = object;
-  // Traverse up to the second-to-last key
-  for (let i = 0; i < pathArray.length - 1; i++) {
-    const key = pathArray[i];
-    if (current == null || !Object.prototype.hasOwnProperty.call(current, key)) {
-      return false;
-    }
-    current = current[key];
-  }
-
-  // Check the final key on the final object
-  const lastKey = pathArray[pathArray.length - 1];
-  return current != null && Object.prototype.hasOwnProperty.call(current, lastKey);
+export const has = (object, key) => {
+  return object != null && Object.prototype.hasOwnProperty.call(object, key);
 };
-
-export { has };
