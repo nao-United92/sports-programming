840093ef28ca905df4908e368e06800a18092890
nao-United92 <naoyauver@icloud.com>
2025-11-07 20:37:27 +0900

    feat: Add array move utility    2    3 Add a `move` function to move an element within an array.    4 Includes unit tests.

---
+++ src/other/array-move-utils.js
@@ -0,0 +1,9 @@
+export const move = (array, from, to) => {
+  const newArray = [...array];
+  if (from < 0 || from >= newArray.length || to < 0 || to > newArray.length) {
+    return newArray;
+  }
+  const [item] = newArray.splice(from, 1);
+  newArray.splice(to, 0, item);
+  return newArray;
+};
