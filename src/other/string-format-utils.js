3475d500c6497b1e5e5015c2d2d3b2905b80069f
nao-United92 <naoyauver@icloud.com>
2025-11-14 22:40:24 +0900

    feat(string): add string formatting utility with tests

--- src/other/string-format-utils.js
+++ src/other/string-format-utils.js
@@ -1,58 +1,21 @@
 /**
- * Calculates the byte size of a string using the Blob API.
- * @param {string} str The string to calculate the size of.
- * @returns {number} The byte size of the string.
+ * Formats a string by replacing placeholders with corresponding values from a data object.
+ * Placeholders are in the format `{key}`.
+ * If a key is not found in the data object, its placeholder will be replaced with an empty string.
+ *
+ * @param {string} template The template string with placeholders.
+ * @param {object} data The object containing values for the placeholders.
+ * @returns {string} The formatted string.
  */
-const byteSize = (str) => new Blob([str]).size;
-
-/**
- * Truncates a string to a specified length, appending '...' if truncated.
- * @param {string} str The string to truncate.
- * @param {number} num The maximum length of the resulting string.
- * @returns {string} The truncated string.
- */
-const truncate = (str, num) => {
-  if (typeof str !== 'string') {
-    return str;
+export const formatString = (template, data) => {
+  if (typeof template !== 'string') {
+    return '';
   }
-  if (num <= 3) {
-    return '...';
+  if (typeof data !== 'object' || data === null) {
+    return template; // If data is not an object, no replacements can be made
   }
-  if (str.length <= num) {
-    return str;
-  }
-  return str.slice(0, num - 3) + '...';
-};
 
-/**
- * Escapes special HTML characters in a string.
- * @param {string} str The string to escape.
- * @returns {string} The escaped string.
- */
-const escapeHTML = (str) => {
-    if (typeof str !== 'string') return str;
-    return str.replace(
-        /[&<>"']/g,
-        (m) => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' }[m])
-    );
-};
-
-/**
- * Unescapes escaped HTML characters in a string.
- * @param {string} str The string to unescape.
- * @returns {string} The unescaped string.
- */
-const unescapeHTML = (str) => {
-    if (typeof str !== 'string') return str;
-    return str.replace(
-        /&amp;|&lt;|&gt;|&quot;|&#39;/g,
-        (m) => ({ '&amp;': '&', '&lt;': '<', '&gt;': '>', '&quot;': '"', '&#39;': "'" }[m])
-    );
-};
-
-module.exports = {
-  byteSize,
-  truncate,
-  escapeHTML,
-  unescapeHTML,
+  return template.replace(/\{(\w+)\}/g, (match, key) => {
+    return Object.prototype.hasOwnProperty.call(data, key) ? String(data[key]) : '';
+  });
 };
