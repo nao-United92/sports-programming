18f763697638b9e31364ba6f3e978fb3cf80ed65
nao-United92 <naoyauver@icloud.com>
2025-11-15 10:53:48 +0900

    feat: Add function control utilities (debounce, throttle)

--- src/other/function-control-utils.js
+++ src/other/function-control-utils.js
@@ -1,58 +1,42 @@
 /**
- * Creates a debounced function that delays invoking `func` until after `wait`
- * milliseconds have elapsed since the last time the debounced function was invoked.
+ * Creates a debounced function that delays invoking func until after wait milliseconds
+ * have elapsed since the last time the debounced function was invoked.
  * @param {Function} func The function to debounce.
  * @param {number} wait The number of milliseconds to delay.
+ * @param {boolean} immediate Pass true to invoke func on the leading edge, instead of the trailing.
  * @returns {Function} Returns the new debounced function.
  */
-const debounce = (func, wait) => {
-    let timeout;
-    return function(...args) {
-        const context = this;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(context, args), wait);
+const debounce = (func, wait, immediate = false) => {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 };
 
 /**
- * Creates a throttled function that only invokes `func` at most once per
- * every `limit` milliseconds.
+ * Creates a throttled function that only invokes func at most once per every limit milliseconds.
  * @param {Function} func The function to throttle.
  * @param {number} limit The number of milliseconds to throttle invocations to.
  * @returns {Function} Returns the new throttled function.
  */
 const throttle = (func, limit) => {
-    let inThrottle;
-    return function(...args) {
-        const context = this;
-        if (!inThrottle) {
-            func.apply(context, args);
-            inThrottle = true;
-            setTimeout(() => (inThrottle = false), limit);
-        }
-    };
+  let inThrottle;
+  return function(...args) {
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
 };
 
-/**
- * Creates a function that is restricted to invoking func once. Repeat calls
- * to the function return the value of the first invocation.
- * @param {Function} func The function to restrict.
- * @returns {Function} Returns the new restricted function.
- */
-const once = (func) => {
-    let hasBeenCalled = false;
-    let result;
-    return function(...args) {
-        if (!hasBeenCalled) {
-            hasBeenCalled = true;
-            result = func.apply(this, args);
-        }
-        return result;
-    };
-};
-
-module.exports = {
-    debounce,
-    throttle,
-    once,
-};
+module.exports = { debounce, throttle };
