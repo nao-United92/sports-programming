6fc3f00fd04c7f46db583ee2e56cb5bd74d96701
nao-United92 <naoyauver@icloud.com>
2025-11-20 00:13:44 +0900

    feat: Add chunkArrayInGroups function to array chunk utilities

--- src/other/array-chunk-utils.test.js
+++ src/other/array-chunk-utils.test.js
@@ -1,4 +1,4 @@
-const { chunk } = require('./array-chunk-utils.js');
+const { chunk, chunkArrayInGroups } = require('./array-chunk-utils.js');
 
 describe('chunk', () => {
   it('should create chunks of a specified size', () => {
@@ -26,5 +26,34 @@ describe('chunk', () => {
     const array = ['a', 'b', 'c'];
     expect(chunk(array, 0)).toEqual([]);
     expect(chunk(array, -1)).toEqual([]);
+  });
+});
+
+describe('chunkArrayInGroups', () => {
+  it('should chunk an array into smaller arrays of a specified size', () => {
+    expect(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2)).toEqual([['a', 'b'], ['c', 'd']]);
+  });
+
+  it('should handle uneven chunks correctly', () => {
+    expect(chunkArrayInGroups(['a', 'b', 'c', 'd', 'e'], 2)).toEqual([['a', 'b'], ['c', 'd'], ['e']]);
+  });
+
+  it('should return an empty array if the input array is empty', () => {
+    expect(chunkArrayInGroups([], 3)).toEqual([]);
+  });
+
+  it('should return an empty array if size is zero or negative', () => {
+    expect(chunkArrayInGroups([1, 2, 3], 0)).toEqual([]);
+    expect(chunkArrayInGroups([1, 2, 3], -1)).toEqual([]);
+  });
+
+  it('should return an empty array for non-array inputs', () => {
+    expect(chunkArrayInGroups(null, 2)).toEqual([]);
+    expect(chunkArrayInGroups(undefined, 2)).toEqual([]);
+    expect(chunkArrayInGroups('string', 2)).toEqual([]);
+  });
+
+  it('should create single-element chunks if size is 1', () => {
+    expect(chunkArrayInGroups([1, 2, 3], 1)).toEqual([[1], [2], [3]]);
   });
 });
