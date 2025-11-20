54016aca8c7319a4c8ed5cba76a555e62d37d209
nao-United92 <naoyauver@icloud.com>
2025-11-20 23:09:18 +0900

    feat: Add shuffleArray function to array shuffle utilities

--- src/other/array-shuffle-utils.test.js
+++ src/other/array-shuffle-utils.test.js
@@ -1,4 +1,4 @@
-const { shuffle } = require('./array-shuffle-utils');
+const { shuffle, shuffleArray } = require('./array-shuffle-utils');
 
 describe('shuffle', () => {
   const originalArray = [1, 2, 3, 4, 5];
@@ -35,6 +35,49 @@ describe('shuffle', () => {
     let isShuffled = false;
     for (let i = 0; i < 100; i++) { // Try multiple times to reduce false negatives
       const shuffled = shuffle(originalArray);
+      if (!shuffled.every((val, index) => val === originalArray[index])) {
+        isShuffled = true;
+        break;
+      }
+    }
+    expect(isShuffled).toBe(true);
+  });
+});
+
+describe('shuffleArray', () => {
+  const originalArray = [1, 2, 3, 4, 5];
+
+  it('should return an array of the same length', () => {
+    const shuffled = shuffleArray(originalArray);
+    expect(shuffled.length).toBe(originalArray.length);
+  });
+
+  it('should contain the same elements as the original array', () => {
+    const shuffled = shuffleArray(originalArray);
+    expect(shuffled.sort()).toEqual(originalArray.sort());
+  });
+
+  it('should not mutate the original array', () => {
+    const originalCopy = [...originalArray];
+    shuffleArray(originalArray);
+    expect(originalArray).toEqual(originalCopy);
+  });
+
+  it('should return a new array instance', () => {
+    const shuffled = shuffleArray(originalArray);
+    expect(shuffled).not.toBe(originalArray);
+  });
+
+  it('should return an empty array for an empty input array', () => {
+    expect(shuffleArray([])).toEqual([]);
+  });
+
+  // This test has a very small chance of failing if Math.random produces a sequence
+  // that results in the array being shuffled back to its original order.
+  it('should generally change the order of elements', () => {
+    let isShuffled = false;
+    for (let i = 0; i < 100; i++) { // Try multiple times to reduce false negatives
+      const shuffled = shuffleArray(originalArray);
       if (!shuffled.every((val, index) => val === originalArray[index])) {
         isShuffled = true;
         break;
