f0647d7fe945a406216be7e561ba69e04e76b992
nao-United92 <naoyauver@icloud.com>
2025-11-22 09:53:59 +0900

    refactor(object-key-converter): snake_case変換に焦点を当て、簡素化
    `keysToCamel`関数を削除し、モジュールの責務をオブジェクトキーの`snake_case`への変換のみに絞りました。
    `keysToSnake`関数は`keysToSnakeCase`に名前が変更され、実装が改善されました。
    また、コードベースの他の部分と一貫性を保つために、ES6の`import`/`export`構文を使用するようにテストファイルを更新しました。

--- src/other/object-key-converter.js
+++ src/other/object-key-converter.js
@@ -1,38 +1,17 @@
-const toCamelCase = (str) => {
-  if (typeof str !== 'string') return str;
-  return str.replace(/([-_][a-z])/gi, ($1) => {
-    return $1.toUpperCase().replace('-', '').replace('_', '');
-  });
-};
+const toSnakeCase = (str) =>
+  str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
 
-const toSnakeCase = (str) => {
-  if (typeof str !== 'string') return str;
-  // Ensure not to add a leading underscore if the first letter is uppercase
-  return str.replace(/[A-Z]/g, (letter, index) => {
-    return index === 0 ? letter.toLowerCase() : `_${letter.toLowerCase()}`;
-  });
-};
+const isObject = (obj) => obj === Object(obj) && !Array.isArray(obj);
 
-const convertKeys = (obj, converter) => {
-  if (obj === null || typeof obj !== 'object') {
-    return obj;
+export const keysToSnakeCase = (obj) => {
+  if (isObject(obj)) {
+    const newObj = {};
+    Object.keys(obj).forEach((key) => {
+      newObj[toSnakeCase(key)] = keysToSnakeCase(obj[key]);
+    });
+    return newObj;
+  } else if (Array.isArray(obj)) {
+    return obj.map((i) => keysToSnakeCase(i));
   }
-
-  if (Array.isArray(obj)) {
-    return obj.map(v => convertKeys(v, converter));
-  }
-
-  return Object.keys(obj).reduce((acc, key) => {
-    const newKey = converter(key);
-    acc[newKey] = convertKeys(obj[key], converter);
-    return acc;
-  }, {});
-};
-
-const keysToCamel = (obj) => convertKeys(obj, toCamelCase);
-const keysToSnake = (obj) => convertKeys(obj, toSnakeCase);
-
-module.exports = {
-  keysToCamel,
-  keysToSnake,
+  return obj;
 };
