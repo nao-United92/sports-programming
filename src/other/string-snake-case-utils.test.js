bbd90da0d7442c4323db06e1f4f07d654e98656f
nao-United92 <naoyauver@icloud.com>
2025-11-24 09:50:27 +0900

    feat(string): Add snakeCase utility function and tests

--- src/other/string-snake-case-utils.test.js
+++ src/other/string-snake-case-utils.test.js
@@ -1,9 +1,50 @@
-import { snakeCase } from './string-snake-case-utils.js';
+import { snakeCase } from './string-snake-case-utils';
 
 describe('snakeCase', () => {
-  test('should convert a string to snake case', () => {
-    expect(snakeCase('Foo Bar')).toBe('foo_bar');
-    expect(snakeCase('--foo-bar--')).toBe('foo_bar');
-    expect(snakeCase('__FOO_BAR__')).toBe('foo_bar');
+  test('should convert a single word to snake_case', () => {
+    expect(snakeCase('hello')).toBe('hello');
+  });
+
+  test('should convert a space-separated string to snake_case', () => {
+    expect(snakeCase('hello world')).toBe('hello_world');
+  });
+
+  test('should convert a camelCase string to snake_case', () => {
+    expect(snakeCase('helloWorld')).toBe('hello_world');
+  });
+
+  test('should convert a PascalCase string to snake_case', () => {
+    expect(snakeCase('HelloWorld')).toBe('hello_world');
+  });
+
+  test('should handle mixed separators', () => {
+    expect(snakeCase('hello-world_again')).toBe('hello_world_again');
+  });
+
+  test('should handle leading/trailing spaces', () => {
+    expect(snakeCase('  hello world  ')).toBe('hello_world');
+  });
+
+  test('should handle multiple spaces between words', () => {
+    expect(snakeCase('hello   world')).toBe('hello_world');
+  });
+
+  test('should handle numbers', () => {
+    expect(snakeCase('version 1 0')).toBe('version_1_0');
+  });
+
+  test('should return an empty string for an empty input', () => {
+    expect(snakeCase('')).toBe('');
+  });
+
+  test('should return an empty string for non-string inputs', () => {
+    expect(snakeCase(null)).toBe('');
+    expect(snakeCase(undefined)).toBe('');
+    expect(snakeCase(123)).toBe('');
+    expect(snakeCase({})).toBe('');
+  });
+
+  test('should handle already snake_cased strings', () => {
+    expect(snakeCase('already_snake_cased')).toBe('already_snake_cased');
   });
 });
