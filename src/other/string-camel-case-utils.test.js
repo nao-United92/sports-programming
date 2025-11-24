b571efd6f133250e211a8bc6ba33b5fb101f965f
nao-United92 <naoyauver@icloud.com>
2025-11-24 09:48:50 +0900

    feat(string): Add camelCase utility function and tests

--- src/other/string-camel-case-utils.test.js
+++ src/other/string-camel-case-utils.test.js
@@ -1,30 +1,50 @@
 import { camelCase } from './string-camel-case-utils';
 
 describe('camelCase', () => {
-  it('should convert a space-separated string to camel case', () => {
-    expect(camelCase('Foo Bar')).toBe('fooBar');
+  test('should convert a single word to camelCase', () => {
+    expect(camelCase('hello')).toBe('hello');
   });
 
-  it('should convert a kebab-cased string to camel case', () => {
-    expect(camelCase('--foo-bar--')).toBe('fooBar');
+  test('should convert a space-separated string to camelCase', () => {
+    expect(camelCase('hello world')).toBe('helloWorld');
   });
 
-  it('should convert a snake_cased string to camel case', () => {
-    expect(camelCase('__FOO_BAR__')).toBe('fooBar');
+  test('should convert a hyphen-separated string to camelCase', () => {
+    expect(camelCase('hello-world')).toBe('helloWorld');
   });
 
-  it('should handle a single word', () => {
-    expect(camelCase('foo')).toBe('foo');
-    expect(camelCase('FOO')).toBe('foo');
+  test('should convert an underscore-separated string to camelCase', () => {
+    expect(camelCase('hello_world')).toBe('helloWorld');
   });
 
-  it('should handle an empty string', () => {
+  test('should handle mixed separators', () => {
+    expect(camelCase('hello-world_again')).toBe('helloWorldAgain');
+  });
+
+  test('should handle leading/trailing spaces', () => {
+    expect(camelCase('  hello world  ')).toBe('helloWorld');
+  });
+
+  test('should handle multiple spaces between words', () => {
+    expect(camelCase('hello   world')).toBe('helloWorld');
+  });
+
+  test('should handle numbers', () => {
+    expect(camelCase('version 1 0')).toBe('version10');
+  });
+
+  test('should return an empty string for an empty input', () => {
     expect(camelCase('')).toBe('');
   });
 
-  it('should handle non-string inputs', () => {
+  test('should return an empty string for non-string inputs', () => {
     expect(camelCase(null)).toBe('');
     expect(camelCase(undefined)).toBe('');
     expect(camelCase(123)).toBe('');
+    expect(camelCase({})).toBe('');
+  });
+
+  test('should handle already camelCased strings', () => {
+    expect(camelCase('alreadyCamelCased')).toBe('alreadyCamelCased');
   });
 });
