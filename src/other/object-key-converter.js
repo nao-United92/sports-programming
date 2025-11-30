fc407d368de216ea1d1aae77bfe5253c5aab4e85
nao-United92 <naoyauver@icloud.com>
2025-12-01 00:24:24 +0900

    feat: Add object key case converter utility
    Adds functions to recursively convert object keys to camelCase or snake_case. This provides a convenient way to handle API data with different naming conventions.
    
    Includes comprehensive unit tests for both conversion directions, handling nested objects and arrays.

--- src/other/object-key-converter.js
+++ src/other/object-key-converter.js
@@ -1,17 +1,33 @@
-const toSnakeCase = (str) =>
-  str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+const toCamelCase = (str) => {
+  if (typeof str !== 'string') return str;
+  return str.replace(/([-_][a-z])/g, (group) =>
+    group.toUpperCase().replace('-', '').replace('_', '')
+  );
+};
 
-const isObject = (obj) => obj === Object(obj) && !Array.isArray(obj);
+const toSnakeCase = (str) => {
+  if (typeof str !== 'string') return str;
+  const result = str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+  return result.startsWith('_') ? result.substring(1) : result;
+};
 
-export const keysToSnakeCase = (obj) => {
-  if (isObject(obj)) {
-    const newObj = {};
-    Object.keys(obj).forEach((key) => {
-      newObj[toSnakeCase(key)] = keysToSnakeCase(obj[key]);
-    });
-    return newObj;
-  } else if (Array.isArray(obj)) {
-    return obj.map((i) => keysToSnakeCase(i));
+const convertKeys = (obj, converter) => {
+  if (Array.isArray(obj)) {
+    return obj.map(v => convertKeys(v, converter));
+  } else if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce((acc, key) => {
+      const newKey = converter(key);
+      acc[newKey] = convertKeys(obj[key], converter);
+      return acc;
+    }, {});
   }
   return obj;
+};
+
+const keysToCamelCase = (obj) => convertKeys(obj, toCamelCase);
+const keysToSnakeCase = (obj) => convertKeys(obj, toSnakeCase);
+
+module.exports = {
+  keysToCamelCase,
+  keysToSnakeCase,
 };
