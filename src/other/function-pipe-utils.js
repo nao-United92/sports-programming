9a58ee710d3b485885690b96ffc99172c544933c
nao-United92 <naoyauver@icloud.com>
2025-12-09 00:47:15 +0900

    feat: add pipe utility for function composition
    Implements a `pipe` function to perform left-to-right function composition.
    This allows for creating elegant, readable data-processing pipelines by chaining together simple, reusable functions.
    This pattern is particularly useful for agent frameworks with hooks or middleware.

--- src/other/function-pipe-utils.js
+++ src/other/function-pipe-utils.js
@@ -1,19 +1,4 @@
-/**
- * Creates a function that returns the result of invoking the given functions from left to right.
- * The first function is invoked with the arguments of the created function,
- * the second function is invoked with the result of the first function, and so on.
- *
- * @param {Function[]} fns The functions to pipe.
- * @returns {Function} Returns the new composite function.
- */
-export const pipe = (...fns) => {
-  if (fns.length === 0) {
-    return (...args) => args.length > 1 ? [...args] : args[0];
-  }
+// Passes the result of each function to the next one, from left to right.
+const pipe = (...fns) => (initialValue) => fns.reduce((acc, fn) => fn(acc), initialValue);
 
-  if (fns.length === 1) {
-    return fns[0];
-  }
-
-  return fns.reduce((a, b) => (...args) => b(a(...args)));
-};
+module.exports = pipe;
