94854fef237d62b828af23f7b041f41eac605404
nao-United92 <naoyauver@icloud.com>
2025-12-09 00:23:45 +0900

    feat: add object omit utility
    Implements an `omit` utility, the counterpart to `pick`, to create an object excluding specified keys.
    Includes tests to ensure immutability and correct behavior.

--- src/other/object-omit-utils.test.js
+++ src/other/object-omit-utils.test.js
@@ -1,22 +1,40 @@
-import { omit } from './object-omit-utils';
+const omit = require('./object-omit-utils');
 
 describe('omit', () => {
-  test('should create an object with omitted properties', () => {
-    const obj = { a: 1, b: '2', c: 3 };
-    expect(omit(obj, ['a', 'c'])).toEqual({ b: '2' });
+  const obj = { 'a': 1, 'b': '2', 'c': 3 };
+
+  test('should return an empty object if the source object is null or undefined', () => {
+    expect(omit(null, ['a', 'b'])).toEqual({});
+    expect(omit(undefined, 'a')).toEqual({});
   });
 
-  test('should return the same object if no keys are omitted', () => {
-    const obj = { a: 1, b: '2', c: 3 };
-    expect(omit(obj, [])).toEqual({ a: 1, b: '2', c: 3 });
+  test('should create a new object omitting the specified properties', () => {
+    const result = omit(obj, ['a', 'c']);
+    expect(result).toEqual({ 'b': '2' });
   });
 
-  test('should handle keys that do not exist in the object', () => {
-    const obj = { a: 1, b: '2' };
-    expect(omit(obj, ['c', 'd'])).toEqual({ a: 1, b: '2' });
+  test('should not modify the original object', () => {
+    omit(obj, 'b');
+    expect(obj).toEqual({ 'a': 1, 'b': '2', 'c': 3 });
   });
 
-  test('should return an empty object for an empty source object', () => {
-    expect(omit({}, ['a', 'b'])).toEqual({});
+  test('should work with a single key provided as a string', () => {
+    expect(omit(obj, 'b')).toEqual({ 'a': 1, 'c': 3 });
+  });
+
+  test('should ignore keys that do not exist in the source object', () => {
+    expect(omit(obj, ['a', 'f'])).toEqual({ 'b': '2', 'c': 3 });
+  });
+
+  test('should return a copy of the object if no keys are provided', () => {
+    const result = omit(obj, []);
+    expect(result).toEqual(obj);
+    expect(result).not.toBe(obj);
+  });
+  
+  test('should return a copy of the object if no matching keys are found', () => {
+    const result = omit(obj, ['x', 'y']);
+    expect(result).toEqual(obj);
+    expect(result).not.toBe(obj);
   });
 });
