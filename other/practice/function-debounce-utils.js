f8f0996c4c16290b8496434184d71930ac40f5c2
nao-United92 <naoyauver@icloud.com>
2025-12-11 10:08:52 +0900

    feat(practice): add function debounce utility

---
+++ other/practice/function-debounce-utils.js
@@ -0,0 +1,15 @@
+const debounce = (func, wait) => {
+  let timeout;
+
+  return function(...args) {
+    const context = this;
+    const later = () => {
+      timeout = null;
+      func.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
+
+module.exports = debounce;
