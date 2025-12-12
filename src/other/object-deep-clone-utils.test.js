6490ff02e6b4bd0aa97f9bbf873635d4da61f9f7
nao-United92 <naoyauver@icloud.com>
2025-12-12 15:54:19 +0900

    feat: Add deepClone utility function

--- src/other/object-deep-clone-utils.test.js
+++ src/other/object-deep-clone-utils.test.js
@@ -1,49 +1,61 @@
-const { deepClone } = require('./object-deep-clone-utils.js');
+const { deepClone } = require('./object-deep-clone-utils');
 
 describe('deepClone', () => {
-  it('should deep clone a simple object', () => {
-    const obj = { a: 1, b: { c: 2 } };
-    const cloned = deepClone(obj);
-    expect(cloned).toEqual(obj);
-    expect(cloned).not.toBe(obj);
-    expect(cloned.b).not.toBe(obj.b);
+  test('should deep clone a simple object', () => {
+    const obj = { a: 1, b: 'hello' };
+    const clonedObj = deepClone(obj);
+    expect(clonedObj).toEqual(obj);
+    expect(clonedObj).not.toBe(obj);
   });
 
-  it('should deep clone an array', () => {
-    const arr = [1, [2, 3], { a: 4 }];
-    const cloned = deepClone(arr);
-    expect(cloned).toEqual(arr);
-    expect(cloned).not.toBe(arr);
-    expect(cloned[1]).not.toBe(arr[1]);
-    expect(cloned[2]).not.toBe(arr[2]);
+  test('should deep clone nested objects', () => {
+    const obj = { a: 1, b: { c: 2, d: { e: 3 } } };
+    const clonedObj = deepClone(obj);
+    expect(clonedObj).toEqual(obj);
+    expect(clonedObj).not.toBe(obj);
+    expect(clonedObj.b).not.toBe(obj.b);
+    expect(clonedObj.b.d).not.toBe(obj.b.d);
   });
 
-  it('should handle null and primitive values', () => {
+  test('should deep clone arrays', () => {
+    const arr = [1, { a: 2 }, [3, 4]];
+    const clonedArr = deepClone(arr);
+    expect(clonedArr).toEqual(arr);
+    expect(clonedArr).not.toBe(arr);
+    expect(clonedArr[1]).not.toBe(arr[1]);
+    expect(clonedArr[2]).not.toBe(arr[2]);
+  });
+
+  test('should deep clone arrays containing objects', () => {
+    const arr = [{ a: 1 }, { b: 2 }];
+    const clonedArr = deepClone(arr);
+    expect(clonedArr).toEqual(arr);
+    expect(clonedArr).not.toBe(arr);
+    expect(clonedArr[0]).not.toBe(arr[0]);
+  });
+
+  test('should handle null and undefined', () => {
     expect(deepClone(null)).toBeNull();
+    expect(deepClone(undefined)).toBeUndefined();
+  });
+
+  test('should handle primitive values', () => {
     expect(deepClone(123)).toBe(123);
-    expect(deepClone('hello')).toBe('hello');
+    expect(deepClone('string')).toBe('string');
     expect(deepClone(true)).toBe(true);
   });
 
-  it('should handle dates', () => {
+  test('should handle Date objects', () => {
     const date = new Date();
-    const cloned = deepClone(date);
-    expect(cloned.getTime()).toEqual(date.getTime());
-    expect(cloned).not.toBe(date);
+    const clonedDate = deepClone(date);
+    expect(clonedDate).toEqual(date);
+    expect(clonedDate).not.toBe(date);
   });
 
-  it('should handle complex nested objects', () => {
-    const obj = {
-      a: { b: { c: [1, 2, { d: 'hello' }] } },
-      e: new Date(),
-      f: null,
-    };
-    const cloned = deepClone(obj);
-    expect(cloned).toEqual(obj);
-    expect(cloned).not.toBe(obj);
-    expect(cloned.a.b).not.toBe(obj.a.b);
-    expect(cloned.a.b.c).not.toBe(obj.a.b.c);
-    expect(cloned.a.b.c[2]).not.toBe(obj.a.b.c[2]);
-    expect(cloned.e).not.toBe(obj.e);
+  test('should handle RegExp objects', () => {
+    const regex = /abc/g;
+    const clonedRegex = deepClone(regex);
+    expect(clonedRegex).toEqual(regex);
+    expect(clonedRegex).not.toBe(regex);
   });
 });
