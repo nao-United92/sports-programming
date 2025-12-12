8fda5a2f6efd96b0b407a2032bef542c3572dce6
nao-United92 <naoyauver@icloud.com>
2025-12-12 15:53:40 +0900

    feat: Add union utility function

--- src/other/array-union-utils.test.js
+++ src/other/array-union-utils.test.js
@@ -1,38 +1,27 @@
-const { union } = require('./array-union-utils.js');
+const { union } = require('./array-union-utils');
 
 describe('union', () => {
-  test('should return the union of two arrays', () => {
-    const result = union([2], [1, 2]);
-    expect(result).toEqual([2, 1]);
-  });
-
-  test('should handle multiple arrays', () => {
-    const result = union([1, 2], [2, 3], [3, 4]);
-    expect(result).toEqual([1, 2, 3, 4]);
-  });
-
-  test('should remove duplicate values', () => {
-    const result = union([1, 1, 2, 2], [2, 3, 3, 3]);
-    expect(result).toEqual([1, 2, 3]);
-  });
-
-  test('should handle arrays with different data types', () => {
-    const result = union([1, 'a', null], ['a', false, null]);
-    expect(result).toEqual([1, 'a', null, false]);
+  test('should return unique values from all given arrays', () => {
+    expect(union([1, 2], [2, 3], [1, 4])).toEqual([1, 2, 3, 4]);
   });
 
   test('should handle empty arrays', () => {
-    const result = union([], [1, 2], []);
-    expect(result).toEqual([1, 2]);
+    expect(union([], [], [])).toEqual([]);
   });
 
-  test('should return an empty array if all input arrays are empty', () => {
-    const result = union([], []);
-    expect(result).toEqual([]);
+  test('should handle single array input', () => {
+    expect(union([1, 2, 1, 3])).toEqual([1, 2, 3]);
   });
 
-  test('should handle a single array', () => {
-    const result = union([1, 2, 2, 3]);
-    expect(result).toEqual([1, 2, 3]);
+  test('should handle arrays with different data types', () => {
+    expect(union([1, 'a'], [2, 'a', null], [null, 3])).toEqual([1, 'a', 2, null, 3]);
+  });
+
+  test('should not modify the original arrays', () => {
+    const arr1 = [1, 2];
+    const arr2 = [2, 3];
+    union(arr1, arr2);
+    expect(arr1).toEqual([1, 2]);
+    expect(arr2).toEqual([2, 3]);
   });
 });
