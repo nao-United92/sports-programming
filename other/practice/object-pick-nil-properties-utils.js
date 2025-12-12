68bfc4c1a84aee11c9ddb99bd21959a595505262
nao-United92 <naoyauver@icloud.com>
2025-12-13 08:46:40 +0900

    feat: Add object pick nil properties utility function

---
+++ other/practice/object-pick-nil-properties-utils.js
@@ -0,0 +1,31 @@
+/**
+ * Creates an object composed of the properties of `object` that are `null` or `undefined`.
+ *
+ * @param {Object} object The source object.
+ * @returns {Object} Returns the new object with only nil properties.
+ * @example
+ *
+ * const object = { 'a': 1, 'b': null, 'c': 'hello', 'd': undefined, 'e': false };
+ *
+ * pickNilProperties(object);
+ * // => { 'b': null, 'd': undefined }
+ */
+function pickNilProperties(object) {
+  const result = {};
+
+  if (object == null) {
+    return result;
+  }
+
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      if (object[key] === null || object[key] === undefined) {
+        result[key] = object[key];
+      }
+    }
+  }
+
+  return result;
+}
+
+export default pickNilProperties;
