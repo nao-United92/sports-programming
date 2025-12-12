09f23d23040ed2c084a9cbd718d313c16b4994e2
nao-United92 <naoyauver@icloud.com>
2025-12-12 15:47:20 +0900

    feat: Add flatten utility function

--- other/practice/array-flatten-extended-utils.js
+++ other/practice/array-flatten-extended-utils.js
@@ -1,18 +1,8 @@
-const flatten = (array, depth = 1) => {
-  if (!Array.isArray(array) || depth < 0) {
-    return array; // Return original array if not an array or invalid depth
+const flatten = (arr) => {
+  if (!Array.isArray(arr)) {
+    return arr;
   }
-
-  let result = [];
-  for (let i = 0; i < array.length; i++) {
-    const item = array[i];
-    if (Array.isArray(item) && depth > 0) {
-      result.push(...flatten(item, depth - 1));
-    } else {
-      result.push(item);
-    }
-  }
-  return result;
+  return arr.reduce((acc, val) => acc.concat(val), []);
 };
 
-module.exports = flatten;
+module.exports = { flatten };
