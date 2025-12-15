f1f3717bc2a852317b42a8bbcee496abdd61146d
nao-United92 <naoyauver@icloud.com>
2025-12-15 19:40:00 +0900

    feat: Add sortedUniq utility function

---
+++ other/practice/array-sorted-uniq-utils.js
@@ -0,0 +1,16 @@
+const sortedUniq = (arr) => {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const result = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1]) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+};
+
+module.exports = sortedUniq;
