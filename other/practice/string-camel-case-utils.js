c6c631e154690011d6cd5fb8dc34976bc004e125
nao-United92 <naoyauver@icloud.com>
2025-12-21 09:56:56 +0900

    fix: correct string camelCase method

--- other/practice/string-camel-case-utils.js
+++ other/practice/string-camel-case-utils.js
@@ -1,18 +1,5 @@
-const camelCase = (str) => {
-  if (typeof str !== 'string' || !str) {
-    return '';
-  }
-
-  return str
-    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-    .map(x => x.toLowerCase())
-    .map((word, index) => {
-      if (index === 0) {
-        return word;
-      }
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join('');
+const camelCase = str => {
+  return str.replace(/[^a-zA-Z0-9]+(.)?/g, (match, chr) => chr ? chr.toUpperCase() : '').replace(/^./, match => match.toLowerCase());
 };
 
-module.exports = camelCase;
+export default camelCase;
