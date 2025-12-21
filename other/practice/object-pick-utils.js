763f681a2333c1be030ed1d6bd4dc7f7501ab34e
nao-United92 <naoyauver@icloud.com>
2025-12-21 11:15:11 +0900

    feat: add object-pick utility

--- other/practice/object-pick-utils.js
+++ other/practice/object-pick-utils.js
@@ -1,34 +1,18 @@
-/**
- * Creates an object composed of the picked `object` properties.
- *
- * @param {Object} object The source object.
- * @param {...(string|string[])} paths The property keys to pick.
- * @returns {Object} Returns the new object.
- * @example
- *
- * const object = { 'a': 1, 'b': '2', 'c': 3 };
- *
- * pick(object, ['a', 'c']);
- * // => { 'a': 1, 'c': 3 }
- *
- * pick(object, 'a', 'b');
- * // => { 'a': 1, 'b': '2' }
- */
-function pick(object, ...paths) {
-  const result = {};
-  const flatPaths = paths.flat(); // Handle array of paths or multiple arguments
-
-  if (object == null) {
-    return result;
+const pick = (obj, keys) => {
+  if (typeof obj !== 'object' || obj === null) {
+    return {};
   }
 
-  flatPaths.forEach((key) => {
-    if (Object.prototype.hasOwnProperty.call(object, key)) {
-      result[key] = object[key];
+  const result = {};
+  const keysToPick = Array.isArray(keys) ? keys : [keys];
+
+  for (const key of keysToPick) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      result[key] = obj[key];
     }
-  });
+  }
 
   return result;
-}
+};
 
-export default pick;
+module.exports = pick;
