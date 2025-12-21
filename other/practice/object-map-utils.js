036d5d362203b81618f25af0f65a6e16724cb2e7
nao-United92 <naoyauver@icloud.com>
2025-12-21 11:12:19 +0900

    feat: add object-map utility

---
+++ other/practice/object-map-utils.js
@@ -0,0 +1,16 @@
+const map = (obj, iteratee) => {
+  if (typeof obj !== 'object' || obj === null) {
+    return [];
+  }
+
+  const result = [];
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      result.push(iteratee(obj[key], key, obj));
+    }
+  }
+
+  return result;
+};
+
+module.exports = map;
