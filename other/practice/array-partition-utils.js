b34451e4ab14feb49ec3a09b2969f5273e8d8f85
nao-United92 <naoyauver@icloud.com>
2025-12-21 09:40:27 +0900

    feat: add array partition method

---
+++ other/practice/array-partition-utils.js
@@ -0,0 +1,10 @@
+const partition = (arr, fn) =>
+  arr.reduce(
+    (acc, val, i, arr) => {
+      acc[fn(val, i, arr) ? 0 : 1].push(val);
+      return acc;
+    },
+    [[], []]
+  );
+
+export default partition;
