ab0b742dc19dc492e2fb275bd6d1d85d22f39456
nao-United92 <naoyauver@icloud.com>
2025-12-23 00:14:25 +0900

    feat: Add object omit utility

--- other/practice/object-omit-utils.test.js
+++ other/practice/object-omit-utils.test.js
@@ -1,36 +1,23 @@
-const omit = require('./object-omit-utils');
+import { omit } from './object-omit-utils';
 
 describe('omit', () => {
-  test('should omit a single property', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    expect(omit(obj, 'a')).toEqual({ b: 2, c: 3 });
+  it('should omit specified properties from an object', () => {
+    const obj = { a: 1, b: '2', c: 3 };
+    expect(omit(obj, ['a', 'c'])).toEqual({ b: '2' });
   });
 
-  test('should omit multiple properties', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    expect(omit(obj, ['a', 'c'])).toEqual({ b: 2 });
-  });
-
-  test('should not modify the original object', () => {
-    const obj = { a: 1, b: 2 };
-    omit(obj, 'a');
-    expect(obj).toEqual({ a: 1, b: 2 });
-  });
-
-  test('should return the object if no keys are provided', () => {
+  it('should return the original object if no properties are omitted', () => {
     const obj = { a: 1, b: 2 };
     expect(omit(obj, [])).toEqual({ a: 1, b: 2 });
   });
 
-  test('should return an empty object for null or non-object inputs', () => {
-    expect(omit(null, 'a')).toEqual({});
-    expect(omit(undefined, 'a')).toEqual({});
-    expect(omit(123, 'a')).toEqual({});
+  it('should handle an empty object', () => {
+    expect(omit({}, ['a'])).toEqual({});
   });
 
-  test('should handle keys that do not exist on the object', () => {
+  it('should not mutate the original object', () => {
     const obj = { a: 1, b: 2 };
-    expect(omit(obj, 'c')).toEqual({ a: 1, b: 2 });
-    expect(omit(obj, ['a', 'd'])).toEqual({ b: 2 });
+    omit(obj, ['a']);
+    expect(obj).toEqual({ a: 1, b: 2 });
   });
 });
