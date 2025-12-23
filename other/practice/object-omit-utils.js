623d7dd005a230bd502b317e2e497ad00ce610f1
nao-United92 <naoyauver@icloud.com>
2025-12-24 00:11:42 +0900

    feat: Add object omit utility

--- other/practice/object-omit-utils.js
+++ other/practice/object-omit-utils.js
@@ -1,27 +1,7 @@
-/**
- * Creates an object composed of the `object` properties not `omitted`.
- *
- * @param {Object} obj The source object.
- * @param {Array<string>} keys The property keys to omit.
- * @returns {Object} Returns the new object.
- */
-function omit(obj, keys) {
-  if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
-    return {};
-  }
-  if (!Array.isArray(keys)) {
-    keys = [];
-  }
-
-  const result = {};
-  const omitSet = new Set(keys);
-
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key) && !omitSet.has(key)) {
-      result[key] = obj[key];
-    }
-  }
-  return result;
-}
+const omit = (obj, keys) => {
+  const newObj = { ...obj };
+  keys.forEach(key => delete newObj[key]);
+  return newObj;
+};
 
 module.exports = omit;
