6e6ea2ac9dae46c3fd71d6a84bbc11cf3285829b
nao-United92 <naoyauver@icloud.com>
2025-12-24 00:19:12 +0900

    feat: Add string padStart utility

--- other/practice/string-pad-start-utils.js
+++ other/practice/string-pad-start-utils.js
@@ -1,13 +1,5 @@
-const padStart = (str, length, chars = ' ') => {
-  const strLength = str.length;
-  if (strLength >= length) {
-    return str;
-  }
-
-  const padLength = length - strLength;
-  const padding = chars.repeat(padLength);
-  
-  return padding.slice(0, padLength) + str;
+const padStart = (str, length, char = ' ') => {
+  return str.padStart(length, char);
 };
 
 module.exports = padStart;
