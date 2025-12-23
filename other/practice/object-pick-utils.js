d9f677d7b2e2bf3f3e0b7ae3ce4b38593f73092f
nao-United92 <naoyauver@icloud.com>
2025-12-24 00:11:20 +0900

    feat: Add object pick utility

--- other/practice/object-pick-utils.js
+++ other/practice/object-pick-utils.js
@@ -1,27 +1,4 @@
-/**
- * Creates an object composed of the picked `object` properties.
- *
- * @param {Object} obj The source object.
- * @param {Array<string>} keys The property keys to pick.
- * @returns {Object} Returns the new object.
- */
-function pick(obj, keys) {
-  if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
-    return {};
-  }
-  if (!Array.isArray(keys)) {
-    keys = [];
-  }
-
-  const result = {};
-  const pickSet = new Set(keys);
-
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key) && pickSet.has(key)) {
-      result[key] = obj[key];
-    }
-  }
-  return result;
-}
+const pick = (obj, keys) =>
+  Object.fromEntries(keys.map(key => [key, obj[key]]));
 
 module.exports = pick;
