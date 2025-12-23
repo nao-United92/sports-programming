ed9b13a9dee3f95a72b56488be1b76bd6c27ba45
nao-United92 <naoyauver@icloud.com>
2025-12-23 19:40:04 +0900

    feat: Add utility to convert string to kebab case

--- other/practice/string-kebab-case-utils.js
+++ other/practice/string-kebab-case-utils.js
@@ -1,9 +1,18 @@
-export const kebabCase = (str) => {
-  if (str === null || str === undefined) {
+/**
+ * Converts `string` to kebab case.
+ *
+ * @param {string} str The string to convert.
+ * @returns {string} Returns the kebab cased string.
+ */
+function kebabCase(str) {
+  if (typeof str !== 'string') {
     return '';
   }
   return str
-    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
-    .replace(/[\s_]+/g, '-')
-    .toLowerCase();
-};
+    .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2') // Add hyphen between lower/upper case
+    .replace(/[^a-zA-Z0-9]+/g, '-') // Replace non-alphanumeric with hyphen
+    .toLowerCase() // Convert to lower case
+    .replace(/^-+|-+$/g, ''); // Trim leading/trailing hyphens
+}
+
+module.exports = kebabCase;
--- other/practice/string-kebab-case-utils.test.js
+++ other/practice/string-kebab-case-utils.test.js
@@ -1,32 +1,50 @@
-import { kebabCase } from './string-kebab-case-utils';
+const kebabCase = require('./string-kebab-case-utils');
 
 describe('kebabCase', () => {
-  it('should convert camelCase to kebab-case', () => {
-    expect(kebabCase('camelCase')).toBe('camel-case');
+  test('should convert a camelCase string to kebab-case', () => {
+    expect(kebabCase('fooBar')).toBe('foo-bar');
   });
 
-  it('should convert PascalCase to kebab-case', () => {
-    expect(kebabCase('PascalCase')).toBe('pascal-case');
+  test('should convert a PascalCase string to kebab-case', () => {
+    expect(kebabCase('FooBar')).toBe('foo-bar');
   });
 
-  it('should convert snake_case to kebab-case', () => {
-    expect(kebabCase('snake_case')).toBe('snake-case');
+  test('should convert a space-separated string to kebab-case', () => {
+    expect(kebabCase('foo bar')).toBe('foo-bar');
   });
 
-  it('should convert sentences to kebab-case', () => {
-    expect(kebabCase('A new post')).toBe('a-new-post');
+  test('should convert an underscore-separated string to kebab-case', () => {
+    expect(kebabCase('foo_bar')).toBe('foo-bar');
   });
 
-  it('should handle empty strings', () => {
+  test('should handle mixed separators', () => {
+    expect(kebabCase('Foo Bar-baz_qux')).toBe('foo-bar-baz-qux');
+  });
+
+  test('should handle empty string input', () => {
     expect(kebabCase('')).toBe('');
   });
 
-  it('should handle null and undefined', () => {
-    expect(kebabCase(null)).toBe('');
-    expect(kebabCase(undefined)).toBe('');
+  test('should handle single word strings', () => {
+    expect(kebabCase('foo')).toBe('foo');
   });
 
-  it('should handle strings with numbers', () => {
-    expect(kebabCase('myFunction2')).toBe('my-function2');
+  test('should handle strings with leading/trailing separators', () => {
+    expect(kebabCase('-foo-bar-')).toBe('foo-bar');
+    expect(kebabCase('_foo_bar_')).toBe('foo-bar');
+    expect(kebabCase(' foo bar ')).toBe('foo-bar');
+  });
+
+  test('should handle non-string input', () => {
+    expect(kebabCase(null)).toBe('');
+    expect(kebabCase(undefined)).toBe('');
+    expect(kebabCase(123)).toBe('');
+    expect(kebabCase({})).toBe('');
+  });
+
+  test('should handle string with multiple consecutive non-alphanumeric characters', () => {
+    expect(kebabCase('foo--bar')).toBe('foo-bar');
+    expect(kebabCase('foo__bar')).toBe('foo-bar');
+    expect(kebabCase('foo  bar')).toBe('foo-bar');
   });
 });
