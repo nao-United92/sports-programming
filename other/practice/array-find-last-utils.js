0e660ca6c0ead6aeee958e4e9230d59e31f4475c
nao-United92 <naoyauver@icloud.com>
2025-12-23 19:34:51 +0900

    feat: Add utility to find the last element in an array satisfying a predicate

--- other/practice/array-find-last-utils.js
+++ other/practice/array-find-last-utils.js
@@ -1,11 +1,23 @@
-export const findLast = (arr, func) => {
-  if (!Array.isArray(arr)) {
+/**
+ * Iterates over elements of `arr` from right to left, returning the first element
+ * `predicate` returns truthy for. The predicate is invoked with three arguments:
+ * (value, index, array).
+ *
+ * @param {Array<any>} arr The array to inspect.
+ * @param {Function} predicate The function invoked per iteration.
+ * @returns {any|undefined} Returns the matched element, else `undefined`.
+ */
+function findLast(arr, predicate) {
+  if (!Array.isArray(arr) || typeof predicate !== 'function') {
     return undefined;
   }
+
   for (let i = arr.length - 1; i >= 0; i--) {
-    if (func(arr[i])) {
+    if (predicate(arr[i], i, arr)) {
       return arr[i];
     }
   }
   return undefined;
-};
+}
+
+module.exports = findLast;
