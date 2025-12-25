aa2d0c6b1c76b027e44861b467f548ba95de854e
nao-United92 <naoyauver@icloud.com>
2025-12-25 10:51:38 +0900

    feat: Add pipe utility

--- other/practice/function-pipe-utils.js
+++ other/practice/function-pipe-utils.js
@@ -1,23 +1,4 @@
-const pipe = (...funcs) => {
-  return function(...args) {
-    if (funcs.length === 0) {
-      return args.length > 0 ? args[0] : undefined;
-    }
+const pipe = (...fns) =>
+  fns.reduce((f, g) => (...args) => g(f(...args)), arg => arg);
 
-    // Apply functions from right to left (opposite of flow)
-    // The first function (rightmost in the pipe definition) takes the initial arguments.
-    // Subsequent functions (to the left) take the result of the previous one.
-    // This is often implemented by reversing the array and then using flow's logic.
-
-    // Let's implement directly for clarity
-    const reversedFuncs = [...funcs].reverse();
-
-    let result = reversedFuncs[0].apply(this, args);
-    for (let i = 1; i < reversedFuncs.length; i++) {
-      result = reversedFuncs[i].call(this, result);
-    }
-    return result;
-  };
-};
-
-module.exports = pipe;
+module.exports = { pipe };
--- other/practice/function-pipe-utils.test.js
+++ other/practice/function-pipe-utils.test.js
@@ -1,61 +1,30 @@
-const pipe = require('./function-pipe-utils');
+const { pipe } = require('./function-pipe-utils.js');
 
 describe('pipe', () => {
-  test('should compose functions from right to left', () => {
-    const add = (a, b) => a + b; // This function takes two arguments
-    const square = (n) => n * n; // This function takes one argument
-    const toString = (n) => String(n); // This function takes one argument
+  const add1 = (x) => x + 1;
+  const multiply2 = (x) => x * 2;
+  const subtract3 = (x) => x - 3;
 
-    // pipe(toString, square, add)
-    // The initial arguments (2, 3) go to the rightmost function `add`.
-    // add(2, 3) -> 5
-    // square(5) -> 25
-    // toString(25) -> '25'
-    const addThenSquareThenToString = pipe(toString, square, add);
-
-    expect(addThenSquareThenToString(2, 3)).toBe('25');
+  it('should pipe functions from left to right', () => {
+    const pipedFn = pipe(add1, multiply2, subtract3);
+    // (5 + 1) * 2 - 3 = 6 * 2 - 3 = 12 - 3 = 9
+    expect(pipedFn(5)).toBe(9);
   });
 
-  test('should handle single function in the pipe', () => {
-    const add = (a, b) => a + b;
-    const singleFuncPipe = pipe(add);
-    expect(singleFuncPipe(5, 5)).toBe(10);
+  it('should work with a single function', () => {
+    const pipedFn = pipe(add1);
+    expect(pipedFn(5)).toBe(6);
   });
 
-  test('should handle empty pipe and return the first argument or undefined', () => {
-    const emptyPipe = pipe();
-    expect(emptyPipe(1, 2, 3)).toBe(1);
-    expect(emptyPipe()).toBeUndefined();
+  it('should return the initial value if no functions are provided', () => {
+    const pipedFn = pipe();
+    expect(pipedFn(5)).toBe(5);
   });
 
-  test('should pass arguments to the rightmost function', () => {
-    const fn1 = jest.fn((a, b) => a + b); // rightmost
-    const fn2 = jest.fn((n) => n * 2); // middle
-    const fn3 = jest.fn((n) => n + '!'); // leftmost
-    const composed = pipe(fn3, fn2, fn1);
-
-    composed(10, 20);
-
-    expect(fn1).toHaveBeenCalledWith(10, 20); // receives initial args
-    expect(fn2).toHaveBeenCalledWith(30); // receives result of fn1
-    expect(fn3).toHaveBeenCalledWith(60); // receives result of fn2
-  });
-
-  test('should maintain `this` context for all functions', () => {
-    const obj = {
-      value: 1,
-      add: function(a, b) {
-        return this.value + a + b;
-      },
-      multiply: function(n) {
-        return this.value * n;
-      },
-    };
-
-    // pipe(obj.multiply, obj.add)
-    // obj.add(2,3) -> 1+2+3 = 6
-    // obj.multiply(6) -> 1*6 = 6
-    const composedMethod = pipe(obj.multiply, obj.add);
-    expect(composedMethod.call(obj, 2, 3)).toBe(6);
+  it('should handle functions with multiple arguments correctly for the first function', () => {
+    const sum = (a, b) => a + b;
+    const square = (x) => x * x;
+    const pipedFn = pipe(sum, square); // square(sum(a, b))
+    expect(pipedFn(2, 3)).toBe(25); // (2 + 3) * (2 + 3) = 5 * 5 = 25
   });
 });
