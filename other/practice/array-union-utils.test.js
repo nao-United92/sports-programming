9a2f6d6fca07db155483db736131aa8d2308d23f
nao-United92 <naoyauver@icloud.com>
2025-12-26 14:11:21 +0900

    feat: Add union function for arrays

--- other/practice/array-union-utils.test.js
+++ other/practice/array-union-utils.test.js
@@ -1,20 +1,21 @@
-import union from './array-union-utils';
+const {
+  union
+} = require('./array-union-utils');
 
 describe('union', () => {
   test('should return the union of two arrays', () => {
     expect(union([1, 2, 3], [2, 3, 4])).toEqual([1, 2, 3, 4]);
   });
 
-  test('should handle multiple arrays', () => {
+  test('should return the union of multiple arrays', () => {
     expect(union([1, 2], [2, 3], [3, 4])).toEqual([1, 2, 3, 4]);
   });
 
-  test('should handle arrays with no common elements', () => {
-    expect(union([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
+  test('should return an empty array if no arrays are provided', () => {
+    expect(union()).toEqual([]);
   });
 
-  test('should handle empty arrays', () => {
-    expect(union([], [1, 2])).toEqual([1, 2]);
-    expect(union([1, 2], [])).toEqual([1, 2]);
+  test('should return the same array if only one array is provided', () => {
+    expect(union([1, 2, 3])).toEqual([1, 2, 3]);
   });
 });
