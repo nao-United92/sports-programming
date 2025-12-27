6df2be624cece8bbfe52ce7b5a0d52690e15a399
nao-United92 <naoyauver@icloud.com>
2025-12-28 08:51:24 +0900

    feat: Add memoize utility method

--- other/practice/function-memoize-utils.js
+++ other/practice/function-memoize-utils.js
@@ -1,29 +1,15 @@
-/**
- * Creates a function that memoizes the result of `func`. If `resolver` is provided,
- * it determines the cache key for storing the result based on the arguments provided to the memoized function.
- * By default, the first argument to the memoized function is used as the map key.
- *
- * @param {Function} func The function to memoize.
- * @param {Function} [resolver] The function to resolve the cache key.
- * @returns {Function} Returns the new memoized function.
- */
-function memoize(func, resolver) {
-  if (typeof func !== 'function') {
-    throw new TypeError('Expected a function');
-  }
+const memoize = (fn) => {
+  const cache = new Map();
 
-  const memoized = function(...args) {
-    const key = resolver ? resolver.apply(this, args) : args[0];
-    if (memoized.cache.has(key)) {
-      return memoized.cache.get(key);
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
     }
-    const result = func.apply(this, args);
-    memoized.cache.set(key, result);
+    const result = fn(...args);
+    cache.set(key, result);
     return result;
   };
+};
 
-  memoized.cache = new Map();
-  return memoized;
-}
-
-module.exports = memoize;
+module.exports = { memoize };
