4ffd90a22c2d76d76eab572a3f3bc616bd4b2f87
nao-United92 <naoyauver@icloud.com>
2025-12-28 08:45:50 +0900

    feat: Add union utility method

--- other/practice/array-union-utils.test.js
+++ other/practice/array-union-utils.test.js
@@ -1,27 +1,23 @@
-import union from './array-union-utils';
+const { union } = require('./array-union-utils');
 
 describe('union', () => {
-  it('should return an array with unique values from all given arrays', () => {
-    expect(union([1, 2, 3], [2, 3, 4], [3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  it('should return the union of multiple arrays', () => {
+    expect(union([1, 2], [2, 3], [3, 4])).toEqual([1, 2, 3, 4]);
+  });
+
+  it('should handle arrays with different data types', () => {
+    expect(union([1, 'a'], [2, 'b', 'a'])).toEqual([1, 'a', 2, 'b']);
+  });
+
+  it('should return a single array unchanged if only one is provided', () => {
+    expect(union([1, 2, 3])).toEqual([1, 2, 3]);
+  });
+
+  it('should return an empty array if no arrays are provided', () => {
+    expect(union()).toEqual([]);
   });
 
   it('should handle empty arrays', () => {
     expect(union([], [1, 2], [])).toEqual([1, 2]);
-  });
-
-  it('should handle arrays with different types of elements', () => {
-    expect(union([1, 'a'], ['a', 2], [3])).toEqual([1, 'a', 2, 3]);
-  });
-
-  it('should return an empty array if all input arrays are empty', () => {
-    expect(union([], [], [])).toEqual([]);
-  });
-
-  it('should work with a single array', () => {
-    expect(union([1, 2, 3])).toEqual([1, 2, 3]);
-  });
-
-  it('should handle duplicate values within a single array', () => {
-    expect(union([1, 1, 2, 2, 3])).toEqual([1, 2, 3]);
   });
 });
