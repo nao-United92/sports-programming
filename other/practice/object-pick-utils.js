87d24dc1a4e5711435889470836296741a3319e8
nao-United92 <naoyauver@icloud.com>
2025-12-28 08:45:18 +0900

    feat: Add pick utility method

--- other/practice/object-pick-utils.js
+++ other/practice/object-pick-utils.js
@@ -1,15 +1,13 @@
 const pick = (obj, keys) => {
-  if (obj == null) {
+  if (obj === null || obj === undefined) {
     return {};
   }
   return keys.reduce((acc, key) => {
-    if (obj.hasOwnProperty(key)) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
       acc[key] = obj[key];
     }
     return acc;
   }, {});
 };
 
-module.exports = {
-  pick
-};
+module.exports = { pick };
--- other/practice/object-pick-utils.test.js
+++ other/practice/object-pick-utils.test.js
@@ -1,33 +1,26 @@
-const {
-  pick
-} = require('./object-pick-utils');
+const { pick } = require('./object-pick-utils');
 
 describe('pick', () => {
-  const obj = {
-    a: 1,
-    b: '2',
-    c: 3
-  };
+  const obj = { a: 1, b: '2', c: 3 };
 
-  test('should create an object with picked properties', () => {
-    expect(pick(obj, ['a', 'c'])).toEqual({
-      a: 1,
-      c: 3
-    });
+  it('should pick specified keys from an object', () => {
+    expect(pick(obj, ['a', 'c'])).toEqual({ a: 1, c: 3 });
   });
 
-  test('should ignore keys that do not exist in the source object', () => {
-    expect(pick(obj, ['a', 'd'])).toEqual({
-      a: 1
-    });
+  it('should not include keys that are not in the object', () => {
+    expect(pick(obj, ['a', 'd'])).toEqual({ a: 1 });
   });
 
-  test('should return an empty object if no keys are provided', () => {
+  it('should return an empty object if no keys are specified', () => {
     expect(pick(obj, [])).toEqual({});
   });
 
-  test('should return an empty object for null or undefined input', () => {
-    expect(pick(null, ['a', 'b'])).toEqual({});
-    expect(pick(undefined, ['a', 'b'])).toEqual({});
+  it('should return an empty object if the input object is null or undefined', () => {
+    expect(pick(null, ['a'])).toEqual({});
+    expect(pick(undefined, ['a'])).toEqual({});
+  });
+
+  it('should handle non-string keys in the keys array gracefully', () => {
+    expect(pick(obj, ['a', 2])).toEqual({ a: 1 });
   });
 });
