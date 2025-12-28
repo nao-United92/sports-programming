f491b64dffa0d19a8b4c5be45a438a077325691b
nao-United92 <naoyauver@icloud.com>
2025-12-28 09:31:52 +0900

    feat: Add tail utility method

--- other/practice/array-tail-utils.js
+++ other/practice/array-tail-utils.js
@@ -1,5 +1,26 @@
-const tail = (arr) => {
-  return arr.slice(1);
-};
+const { tail } = require('./array-tail-utils');
 
-export default tail;
+describe('tail', () => {
+  it('should return all but the first element of an array', () => {
+    expect(tail([1, 2, 3])).toEqual([2, 3]);
+  });
+
+  it('should return an empty array if given an empty array', () => {
+    expect(tail([])).toEqual([]);
+  });
+
+  it('should return an empty array if given an array with one element', () => {
+    expect(tail([1])).toEqual([]);
+  });
+
+  it('should return an empty array if given null or undefined', () => {
+    expect(tail(null)).toEqual([]);
+    expect(tail(undefined)).toEqual([]);
+  });
+
+  it('should not mutate the original array', () => {
+    const original = [1, 2, 3];
+    tail(original);
+    expect(original).toEqual([1, 2, 3]);
+  });
+});
