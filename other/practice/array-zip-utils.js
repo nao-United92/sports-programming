112368d4e60b6dcf3dc266ebbea487d279f39d47
nao-United92 <naoyauver@icloud.com>
2025-12-29 11:10:19 +0900

    feat: Add array zip utility

--- other/practice/array-zip-utils.js
+++ other/practice/array-zip-utils.js
@@ -1,11 +1,14 @@
-const zip = (...arrays) => {
+export const zip = (...arrays) => {
   if (arrays.length === 0) {
     return [];
   }
-  const maxLength = Math.max(...arrays.map(arr => arr.length));
-  return Array.from({ length: maxLength }).map((_, i) => {
-    return arrays.map(arr => arr[i]);
-  });
-};
 
-module.exports = { zip };
+  const maxLength = Math.max(...arrays.map(arr => arr.length));
+  const result = [];
+
+  for (let i = 0; i < maxLength; i++) {
+    result.push(arrays.map(arr => arr[i]));
+  }
+
+  return result;
+};
