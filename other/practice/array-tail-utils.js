3ab4b8e40559891b15eb344da0699f7af5ff840d
nao-United92 <naoyauver@icloud.com>
2025-12-31 09:01:58 +0900

    feat: add tail utility function

--- other/practice/array-tail-utils.js
+++ other/practice/array-tail-utils.js
@@ -1,26 +1,2 @@
-const { tail } = require('./array-tail-utils');
-
-describe('tail', () => {
-  it('should return all but the first element of an array', () => {
-    expect(tail([1, 2, 3])).toEqual([2, 3]);
-  });
-
-  it('should return an empty array if given an empty array', () => {
-    expect(tail([])).toEqual([]);
-  });
-
-  it('should return an empty array if given an array with one element', () => {
-    expect(tail([1])).toEqual([]);
-  });
-
-  it('should return an empty array if given null or undefined', () => {
-    expect(tail(null)).toEqual([]);
-    expect(tail(undefined)).toEqual([]);
-  });
-
-  it('should not mutate the original array', () => {
-    const original = [1, 2, 3];
-    tail(original);
-    expect(original).toEqual([1, 2, 3]);
-  });
-});
+// Returns all but the last element of an array.
+export const tail = (arr) => (arr.length > 1 ? arr.slice(0, arr.length - 1) : []);
--- other/practice/array-tail-utils.test.js
+++ other/practice/array-tail-utils.test.js
@@ -1,15 +1,21 @@
-import tail from './array-tail-utils';
+import { tail } from './array-tail-utils.js';
 
 describe('tail', () => {
-  test('should return all but the first element of an array', () => {
-    expect(tail([1, 2, 3])).toEqual([2, 3]);
+  it('should return all but the last element of an array', () => {
+    expect(tail([1, 2, 3, 4])).toEqual([1, 2, 3]);
   });
 
-  test('should return an empty array for an array with one element', () => {
+  it('should return an empty array if the array has one element', () => {
     expect(tail([1])).toEqual([]);
   });
 
-  test('should return an empty array for an empty array', () => {
+  it('should return an empty array if the array is empty', () => {
     expect(tail([])).toEqual([]);
+  });
+
+  it('should not modify the original array', () => {
+    const arr = [1, 2, 3];
+    tail(arr);
+    expect(arr).toEqual([1, 2, 3]);
   });
 });
