4db84ace8ea7e582ce2921c0eeb6038ad1e14612
nao-United92 <naoyauver@icloud.com>
2025-12-31 09:00:05 +0900

    feat: add symmetricDifference utility function

--- other/practice/array-symmetric-difference-utils.js
+++ other/practice/array-symmetric-difference-utils.js
@@ -1,12 +1,6 @@
-const symmetricDifference = (arr1, arr2) => {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    return [];
-  }
-  const set1 = new Set(arr1);
-  const set2 = new Set(arr2);
-  const diff1 = arr1.filter(x => !set2.has(x));
-  const diff2 = arr2.filter(x => !set1.has(x));
-  return [...diff1, ...diff2];
+// Calculates the symmetric difference between two arrays
+export const symmetricDifference = (a, b) => {
+  const sA = new Set(a);
+  const sB = new Set(b);
+  return [...a.filter(x => !sB.has(x)), ...b.filter(x => !sA.has(x))];
 };
-
-export default symmetricDifference;
