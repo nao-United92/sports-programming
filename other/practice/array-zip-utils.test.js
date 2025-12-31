8cfd76294089c2006c6730fd674ffc23d215f782
nao-United92 <naoyauver@icloud.com>
2025-12-31 09:02:46 +0900

    feat: add zip utility function

--- other/practice/array-zip-utils.test.js
+++ other/practice/array-zip-utils.test.js
@@ -1,14 +1,14 @@
-import { zip } from './array-zip-utils';
+import { zip } from './array-zip-utils.js';
 
 describe('zip', () => {
-  test('should group elements from corresponding positions in arrays', () => {
+  it('should group elements from corresponding positions in multiple arrays', () => {
     expect(zip(['a', 'b'], [1, 2], [true, false])).toEqual([
       ['a', 1, true],
       ['b', 2, false],
     ]);
   });
 
-  test('should handle arrays of different lengths, filling with undefined for missing values', () => {
+  it('should handle arrays of different lengths, filling with undefined for missing elements', () => {
     expect(zip(['a', 'b', 'c'], [1, 2])).toEqual([
       ['a', 1],
       ['b', 2],
@@ -16,20 +16,15 @@ describe('zip', () => {
     ]);
   });
 
-  test('should handle single array input', () => {
-    expect(zip([1, 2, 3])).toEqual([[1], [2], [3]]);
-  });
-
-  test('should handle empty input arrays', () => {
-    expect(zip([], [])).toEqual([]);
-    expect(zip([1, 2], [])).toEqual([[1, undefined], [2, undefined]]);
-  });
-
-  test('should return an empty array if no arrays are provided', () => {
+  it('should return an empty array if no arrays are provided', () => {
     expect(zip()).toEqual([]);
   });
 
-  test('should handle arrays with mixed data types', () => {
-    expect(zip([1, 'a'], [true, null])).toEqual([[1, true], ['a', null]]);
+  it('should return an array of arrays with undefined if input arrays are empty', () => {
+    expect(zip([], [])).toEqual([]);
+  });
+
+  it('should handle a single array input', () => {
+    expect(zip([1, 2, 3])).toEqual([[1], [2], [3]]);
   });
 });
