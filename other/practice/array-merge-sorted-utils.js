859dcec57af133920271d243377879d8924ea4dc
nao-United92 <naoyauver@icloud.com>
2025-12-31 09:07:36 +0900

    feat: add mergeSortedArrays utility function

---
+++ other/practice/array-merge-sorted-utils.js
@@ -0,0 +1,28 @@
+// Merges two sorted arrays into a single sorted array.
+export const mergeSortedArrays = (arr1, arr2) => {
+  const merged = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
+  }
+
+  return merged;
+};
