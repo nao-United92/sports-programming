29181c8f5df744ff935fb7e9f34a9dc696119be0
nao-United92 <naoyauver@icloud.com>
2026-01-02 00:41:01 +0900

    feat: Add times function utility

--- other/practice/function-times-utils.test.js
+++ other/practice/function-times-utils.test.js
@@ -1,30 +1,29 @@
-import times from './function-times-utils';
+const { times } = require('./function-times-utils');
 
 describe('times', () => {
-  test('should invoke a function n times', () => {
-    let count = 0;
-    const increment = () => count++;
-    times(5, increment);
-    expect(count).toBe(5);
-  });
-
-  test('should return an array of the results', () => {
-    const square = (i) => i * i;
-    expect(times(5, square)).toEqual([0, 1, 4, 9, 16]);
+  test('should call the iteratee n times', () => {
+    const iteratee = jest.fn();
+    times(5, iteratee);
+    expect(iteratee).toHaveBeenCalledTimes(5);
   });
 
   test('should pass the index to the iteratee', () => {
-    const identity = (i) => i;
-    expect(times(3, identity)).toEqual([0, 1, 2]);
+    const iteratee = jest.fn();
+    times(3, iteratee);
+    expect(iteratee).toHaveBeenCalledWith(0);
+    expect(iteratee).toHaveBeenCalledWith(1);
+    expect(iteratee).toHaveBeenCalledWith(2);
   });
 
-  test('should return an empty array if n is 0', () => {
-    const spy = jest.fn();
-    expect(times(0, spy)).toEqual([]);
-    expect(spy).not.toHaveBeenCalled();
+  test('should return an array of the results', () => {
+    const result = times(5, String);
+    expect(result).toEqual(['0', '1', '2', '3', '4']);
   });
 
-  test('should return an empty array for negative n', () => {
-    expect(times(-3, () => {})).toEqual([]);
+  test('should handle n=0', () => {
+    const iteratee = jest.fn();
+    const result = times(0, iteratee);
+    expect(iteratee).not.toHaveBeenCalled();
+    expect(result).toEqual([]);
   });
 });
