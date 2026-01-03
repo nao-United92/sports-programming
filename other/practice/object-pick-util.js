e14449313399ca53031e207f791b46045f8d694c
nao-United92 <naoyauver@icloud.com>
2026-01-03 10:31:45 +0900

    feat: Add object pick utility

---
+++ other/practice/object-pick-util.js
@@ -0,0 +1,10 @@
+const pick = (obj, keys) => {
+  return keys.reduce((newObj, key) => {
+    if (key in obj) {
+      newObj[key] = obj[key];
+    }
+    return newObj;
+  }, {});
+};
+
+module.exports = pick;
