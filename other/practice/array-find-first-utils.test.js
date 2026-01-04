c154a5126a7d97af3e02d0bb88342c83a98ee6f5
nao-United92 <naoyauver@icloud.com>
2026-01-04 09:34:28 +0900

    feat: Add array find first utility

---
+++ other/practice/array-find-first-utils.test.js
@@ -0,0 +1,62 @@
+import { findFirst } from './array-find-first-utils';
+
+describe('findFirst', () => {
+  const numbers = [10, 20, 30, 40, 50];
+  const objects = [{
+    id: 1,
+    value: 'a'
+  }, {
+    id: 2,
+    value: 'b'
+  }, {
+    id: 3,
+    value: 'c'
+  }];
+
+  test('should return the first element that satisfies the predicate', () => {
+    expect(findFirst(numbers, num => num > 25)).toBe(30);
+  });
+
+  test('should return undefined if no element satisfies the predicate', () => {
+    expect(findFirst(numbers, num => num > 100)).toBeUndefined();
+  });
+
+  test('should return undefined for an empty array', () => {
+    expect(findFirst([], num => num > 0)).toBeUndefined();
+  });
+
+  test('predicate should receive value, index, and array as arguments', () => {
+    const mockPredicate = jest.fn((value, index, arr) => {
+      expect(arr).toEqual(numbers);
+      return value === 30;
+    });
+    findFirst(numbers, mockPredicate);
+    expect(mockPredicate).toHaveBeenCalledWith(10, 0, numbers);
+    expect(mockPredicate).toHaveBeenCalledWith(20, 1, numbers);
+    expect(mockPredicate).toHaveBeenCalledWith(30, 2, numbers);
+  });
+
+  test('should not modify the original array', () => {
+    const originalArray = [...numbers];
+    findFirst(originalArray, num => num > 25);
+    expect(originalArray).toEqual(numbers);
+  });
+
+  test('should work with objects', () => {
+    expect(findFirst(objects, obj => obj.id === 2)).toEqual({
+      id: 2,
+      value: 'b'
+    });
+  });
+
+  test('should throw an error if array is not an array', () => {
+    expect(() => findFirst(null, () => true)).toThrow('Expected an array');
+    expect(() => findFirst(undefined, () => true)).toThrow('Expected an array');
+    expect(() => findFirst("string", () => true)).toThrow('Expected an array');
+  });
+
+  test('should throw an error if predicate is not a function', () => {
+    expect(() => findFirst(numbers, null)).toThrow('Expected a predicate function');
+    expect(() => findFirst(numbers, "not a function")).toThrow('Expected a predicate function');
+  });
+});
