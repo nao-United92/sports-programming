26948422611298b0e6c3262598b30762f7480d97
nao-United92 <naoyauver@icloud.com>
2026-01-05 00:55:31 +0900

    feat: Add array intersection by utility

--- other/practice/array-intersection-by-utils.js
+++ other/practice/array-intersection-by-utils.js
@@ -1,6 +1,6 @@
 const intersectionBy = (arr1, arr2, iteratee) => {
-  const set = new Set(arr2.map(iteratee));
-  return arr1.filter(item => set.has(iteratee(item)));
+  const set2 = new Set(arr2.map(iteratee));
+  return arr1.filter(item => set2.has(iteratee(item)));
 };
 
-export default intersectionBy;
+module.exports = { intersectionBy };
