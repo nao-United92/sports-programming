9936e0331a0db5a443809832dc9ccc58ba8104eb
nao-United92 <naoyauver@icloud.com>
2026-01-05 00:33:22 +0900

    feat: Add object pick utility

--- other/practice/object-pick-utils.test.js
+++ other/practice/object-pick-utils.test.js
@@ -1,29 +1,34 @@
-import pick from './object-pick-utils';
+import { pick } from './object-pick-utils.js';
 
 describe('pick', () => {
-  test('should create an object with picked properties', () => {
+  it('should pick specified keys from an object', () => {
     const obj = { a: 1, b: '2', c: 3 };
     expect(pick(obj, ['a', 'c'])).toEqual({ a: 1, c: 3 });
   });
 
-  test('should ignore keys that are not in the object', () => {
-    const obj = { a: 1, b: '2' };
-    expect(pick(obj, ['a', 'd'])).toEqual({ a: 1 });
-  });
-
-  test('should return an empty object if no keys are provided', () => {
-    const obj = { a: 1, b: '2' };
+  it('should return an empty object if no keys are specified', () => {
+    const obj = { a: 1, b: '2', c: 3 };
     expect(pick(obj, [])).toEqual({});
   });
 
-  test('should return an empty object for null or non-object inputs', () => {
+  it('should ignore keys that do not exist in the object', () => {
+    const obj = { a: 1, b: 2 };
+    expect(pick(obj, ['a', 'c'])).toEqual({ a: 1 });
+  });
+
+  it('should return an empty object for null or non-object inputs', () => {
     expect(pick(null, ['a'])).toEqual({});
     expect(pick(undefined, ['a'])).toEqual({});
     expect(pick(123, ['a'])).toEqual({});
   });
 
-  test('should work with nested objects', () => {
-    const obj = { a: { nested: 1 }, b: 2 };
-    expect(pick(obj, ['a'])).toEqual({ a: { nested: 1 } });
+  it('should handle an empty source object', () => {
+    expect(pick({}, ['a', 'b'])).toEqual({});
+  });
+
+  it('should handle cases where keys parameter is not an array', () => {
+    const obj = { a: 1, b: 2 };
+    expect(pick(obj, null)).toEqual({});
+    expect(pick(obj, 'a')).toEqual({});
   });
 });
