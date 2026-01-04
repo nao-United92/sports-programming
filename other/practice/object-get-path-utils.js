8b2ca406cf1c3c826e7b2bd71f790d8b058defd7
nao-United92 <naoyauver@icloud.com>
2026-01-04 09:38:21 +0900

    feat: Add object get by path utility

--- other/practice/object-get-path-utils.js
+++ other/practice/object-get-path-utils.js
@@ -1,7 +1,20 @@
-const get = (obj, path, defaultValue = undefined) => {
-  const pathArray = Array.isArray(path) ? path : path.split('.').filter(i => i.length);
-  const result = pathArray.reduce((acc, key) => (acc && acc[key] !== undefined ? acc[key] : undefined), obj);
-  return result === undefined ? defaultValue : result;
-};
+export const getByPath = (obj, path) => {
+  if (typeof obj !== 'object' || obj === null) {
+    return undefined;
+  }
+  if (typeof path !== 'string' || path.trim() === '') {
+    throw new TypeError('Expected path to be a non-empty string');
+  }
 
-module.exports = { get };
+  const pathParts = path.split('.');
+  let current = obj;
+
+  for (let i = 0; i < pathParts.length; i++) {
+    const part = pathParts[i];
+    if (typeof current !== 'object' || current === null || !Object.prototype.hasOwnProperty.call(current, part)) {
+      return undefined;
+    }
+    current = current[part];
+  }
+  return current;
+};
