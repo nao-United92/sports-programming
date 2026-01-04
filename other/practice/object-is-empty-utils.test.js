5df40c70e06a650439cde7a9ba1e5ef14b300730
nao-United92 <naoyauver@icloud.com>
2026-01-04 09:38:52 +0900

    feat: Add object is empty utility

--- other/practice/object-is-empty-utils.test.js
+++ other/practice/object-is-empty-utils.test.js
@@ -1,33 +1,44 @@
-const { isEmptyObject } = require('./object-is-empty-utils');
+import { isEmptyObject } from './object-is-empty-utils';
 
 describe('isEmptyObject', () => {
   test('should return true for an empty object', () => {
     expect(isEmptyObject({})).toBe(true);
   });
 
-  test('should return false for a non-empty object', () => {
-    expect(isEmptyObject({ a: 1 })).toBe(false);
+  test('should return false for an object with properties', () => {
+    expect(isEmptyObject({
+      a: 1
+    })).toBe(false);
   });
 
-  test('should return false for null and undefined', () => {
+  test('should return false for an object with inherited properties', () => {
+    function Parent() {
+      this.a = 1;
+    }
+
+    function Child() {}
+    Child.prototype = new Parent();
+    const child = new Child();
+    expect(isEmptyObject(child)).toBe(true); // Object.keys only checks own properties
+  });
+
+  test('should return false for null', () => {
     expect(isEmptyObject(null)).toBe(false);
+  });
+
+  test('should return false for undefined', () => {
     expect(isEmptyObject(undefined)).toBe(false);
   });
 
-  test('should return false for non-object types', () => {
-    expect(isEmptyObject(123)).toBe(false);
-    expect(isEmptyObject('hello')).toBe(false);
-    expect(isEmptyObject(true)).toBe(false);
-  });
-
-  test('should return false for arrays', () => {
+  test('should return false for an array (even empty)', () => {
     expect(isEmptyObject([])).toBe(false);
     expect(isEmptyObject([1, 2])).toBe(false);
   });
-  
-  test('should return true for empty class instances', () => {
-    function MyObject() {}
-    MyObject.prototype.a = 1;
-    expect(isEmptyObject(new MyObject())).toBe(true);
+
+  test('should return false for other non-object types', () => {
+    expect(isEmptyObject(123)).toBe(false);
+    expect(isEmptyObject('string')).toBe(false);
+    expect(isEmptyObject(true)).toBe(false);
+    expect(isEmptyObject(Symbol('a'))).toBe(false);
   });
 });
