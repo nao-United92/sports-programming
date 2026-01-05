84c706641803550b4b6bb94202b72739a38eff79
nao-United92 <naoyauver@icloud.com>
2026-01-05 11:11:42 +0900

    feat(boolean): Add randomBoolean utility to generate a random boolean value

---
+++ other/practice/boolean-random-utils.js
@@ -0,0 +1,10 @@
+/**
+ * Generates a random boolean value.
+ *
+ * @returns {boolean} Returns `true` or `false` randomly.
+ */
+const randomBoolean = () => {
+  return Math.random() >= 0.5;
+};
+
+export default randomBoolean;
