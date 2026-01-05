1c48ee21256b2c823b54136c3f0ffac234d8d87f
nao-United92 <naoyauver@icloud.com>
2026-01-06 00:23:10 +0900

    feat: Add arrayShuffle utility to randomly shuffle an array

--- other/practice/array-shuffle-utils.js
+++ other/practice/array-shuffle-utils.js
@@ -1,10 +1,26 @@
-const shuffle = (arr) => {
-  let a = [...arr];
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
+const arrayShuffle = (arr, inPlace = false) => {
+  if (!Array.isArray(arr)) {
+    throw new TypeError('Expected an array for the first argument.');
   }
-  return a;
+
+  const arrayToShuffle = inPlace ? arr : [...arr];
+  let currentIndex = arrayToShuffle.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [arrayToShuffle[currentIndex], arrayToShuffle[randomIndex]] = [
+      arrayToShuffle[randomIndex],
+      arrayToShuffle[currentIndex],
+    ];
+  }
+
+  return arrayToShuffle;
 };
 
-module.exports = { shuffle };
+module.exports = arrayShuffle;
