3d88d3d3c40cf4685f4318696cf6f1b1a2f05641
nao-United92 <naoyauver@icloud.com>
2026-01-07 00:16:01 +0900

    feat: Add utility to deep clone objects and arrays

--- other/practice/object-deep-clone-utils.js
+++ other/practice/object-deep-clone-utils.js
@@ -1,28 +1,43 @@
-export const deepClone = (obj) => {
+const deepClone = (obj, hash = new WeakMap()) => {
   if (obj === null || typeof obj !== 'object') {
-    return obj; // Return primitive values or null as is
+    return obj;
   }
 
-  // Handle Date objects
+  // Handle circular references
+  if (hash.has(obj)) {
+    return hash.get(obj);
+  }
+
+  // Handle Date
   if (obj instanceof Date) {
-    return new Date(obj.getTime());
+    return new Date(obj);
+  }
+
+  // Handle RegExp
+  if (obj instanceof RegExp) {
+    return new RegExp(obj);
   }
 
   // Handle Array
   if (Array.isArray(obj)) {
     const arrCopy = [];
-    for (let i = 0; i < obj.length; i++) {
-      arrCopy[i] = deepClone(obj[i]);
-    }
+    hash.set(obj, arrCopy);
+    obj.forEach((item, index) => {
+      arrCopy[index] = deepClone(item, hash);
+    });
     return arrCopy;
   }
 
   // Handle Object
   const objCopy = {};
+  hash.set(obj, objCopy);
   for (const key in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      objCopy[key] = deepClone(obj[key]);
+      objCopy[key] = deepClone(obj[key], hash);
     }
   }
+
   return objCopy;
 };
+
+module.exports = deepClone;
