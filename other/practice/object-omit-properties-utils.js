ffc84e7b9e37f783abf4252a8247ef9280663d58
nao-United92 <naoyauver@icloud.com>
2026-01-07 00:29:53 +0900

    feat: Add utility to omit properties from an object

--- other/practice/object-omit-properties-utils.js
+++ other/practice/object-omit-properties-utils.js
@@ -1,15 +1,24 @@
-const omit = (obj, keysToOmit) => {
-  if (obj === null || typeof obj !== 'object' || Array.isArray(obj)) {
-    return {};
+const omit = (obj, propertiesToOmit) => {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj; // Return original for primitives, null, undefined
+  }
+  // Explicitly check for plain objects only
+  if (Object.prototype.toString.call(obj) !== '[object Object]') {
+    return obj; // Return original for arrays, Dates, RegExps, etc.
+  }
+  if (!Array.isArray(propertiesToOmit)) {
+    throw new Error('propertiesToOmit must be an array of strings.');
   }
 
-  const result = {};
+  const newObj = {};
   for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key) && !keysToOmit.includes(key)) {
-      result[key] = obj[key];
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      if (!propertiesToOmit.includes(key)) {
+        newObj[key] = obj[key];
+      }
     }
   }
-  return result;
+  return newObj;
 };
 
 module.exports = omit;
