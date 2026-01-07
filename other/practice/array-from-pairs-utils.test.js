d34ba703938b1f310fcffc39178f3db6a4eec98c
nao-United92 <naoyauver@icloud.com>
2026-01-08 00:37:08 +0900

    refactor: Improve and standardize array-from-pairs-utils

--- other/practice/array-from-pairs-utils.test.js
+++ other/practice/array-from-pairs-utils.test.js
@@ -1,4 +1,4 @@
-import fromPairs from './array-from-pairs-utils';
+const { fromPairs } = require('./array-from-pairs-utils');
 
 describe('fromPairs', () => {
   test('should create an object from an array of key-value pairs', () => {
@@ -13,5 +13,21 @@ describe('fromPairs', () => {
   test('should handle duplicate keys, keeping the last value', () => {
     const pairs = [['a', 1], ['b', 2], ['a', 3]];
     expect(fromPairs(pairs)).toEqual({ a: 3, b: 2 });
+  });
+  
+  test('should return an empty object for non-array inputs', () => {
+    expect(fromPairs(null)).toEqual({});
+    expect(fromPairs(undefined)).toEqual({});
+    expect(fromPairs('test')).toEqual({});
+  });
+
+  test('should ignore malformed pairs within the input array', () => {
+    const pairs = [['a', 1], ['b'], null, [3, 'c'], ['d', 4]];
+    expect(fromPairs(pairs)).toEqual({ '3': 'c', a: 1, d: 4 });
+  });
+
+  test('should handle various data types as values', () => {
+    const pairs = [['name', 'John'], ['age', 30], ['active', true], ['roles', ['admin', 'user']]];
+    expect(fromPairs(pairs)).toEqual({ name: 'John', age: 30, active: true, roles: ['admin', 'user'] });
   });
 });
