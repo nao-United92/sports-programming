6014532b82a9320a724c04cd8b295202ff07957b
nao-United92 <naoyauver@icloud.com>
2026-01-08 00:53:20 +0900

    refactor: Standardize array-find-first-utils with consistent exports

--- other/practice/array-find-first-utils.js
+++ other/practice/array-find-first-utils.js
@@ -1,9 +1,9 @@
-export const findFirst = (array, predicate) => {
+const findFirst = (array, predicate) => {
   if (!Array.isArray(array)) {
-    throw new TypeError('Expected an array');
+    throw new TypeError('Expected an array for the first argument.');
   }
   if (typeof predicate !== 'function') {
-    throw new TypeError('Expected a predicate function');
+    throw new TypeError('Expected a function for the second argument.');
   }
 
   for (let i = 0; i < array.length; i++) {
@@ -13,3 +13,5 @@ export const findFirst = (array, predicate) => {
   }
   return undefined;
 };
+
+module.exports = { findFirst };
