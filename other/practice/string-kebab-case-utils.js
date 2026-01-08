aa046d682d023b65deec7c9c9130e682b127fa45
nao-United92 <naoyauver@icloud.com>
2026-01-09 00:03:53 +0900

    feat: Add string-kebab-case utility

--- other/practice/string-kebab-case-utils.js
+++ other/practice/string-kebab-case-utils.js
@@ -1,12 +1,23 @@
-const kebabCase = (str) => {
-  if (!str) return '';
-
-  const words = str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g);
-  if (!words) return '';
-  
-  return words
-    .map(x => x.toLowerCase())
-    .join('-');
+const toWords = (str) => {
+  return str
+    .replace(/([a-z])([A-Z])/g, '$1 $2') // camelCase/PascalCase to "camel Case"
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1 $2') // acronyms like "USA Today" to "USA Today"
+    .replace(/([0-9])([a-zA-Z])/g, '$1 $2') // number followed by letter
+    .replace(/([a-zA-Z])([0-9])/g, '$1 $2') // letter followed by number
+    .replace(/[^a-zA-Z0-9]+/g, ' ') // Replace non-alphanumeric with space
+    .toLowerCase()
+    .trim()
+    .split(' ');
 };
 
-module.exports = { kebabCase };
+const kebabCase = (str) => {
+  if (typeof str !== 'string') {
+    throw new TypeError('Expected a string for the argument.');
+  }
+  if (str.length === 0) {
+    return '';
+  }
+  return toWords(str).join('-');
+};
+
+export default kebabCase;
--- other/practice/string-kebab-case-utils.test.js
+++ other/practice/string-kebab-case-utils.test.js
@@ -1,27 +1,48 @@
-const {
-  kebabCase
-} = require('./string-kebab-case-utils');
+import kebabCase from './string-kebab-case-utils';
 
 describe('kebabCase', () => {
-  test('should convert a string to kebab case', () => {
-    expect(kebabCase('camelCase')).toBe('camel-case');
-    expect(kebabCase('some-string')).toBe('some-string');
-    expect(kebabCase('some string')).toBe('some-string');
-    expect(kebabCase('some-string-with-a-dash')).toBe('some-string-with-a-dash');
-    expect(kebabCase('XMLHttpRequest')).toBe('xml-http-request');
+  test('should convert camelCase to kebab-case', () => {
+    expect(kebabCase('helloWorld')).toBe('hello-world');
+    expect(kebabCase('fooBarBaz')).toBe('foo-bar-baz');
   });
 
-  test('should handle an empty string', () => {
+  test('should convert PascalCase to kebab-case', () => {
+    expect(kebabCase('HelloWorld')).toBe('hello-world');
+    expect(kebabCase('FooBarBaz')).toBe('foo-bar-baz');
+  });
+
+  test('should convert snake_case to kebab-case', () => {
+    expect(kebabCase('hello_world')).toBe('hello-world');
+    expect(kebabCase('foo_bar_baz')).toBe('foo-bar-baz');
+  });
+
+  test('should convert space-separated words to kebab-case', () => {
+    expect(kebabCase('Hello World')).toBe('hello-world');
+    expect(kebabCase('foo bar baz')).toBe('foo-bar-baz');
+  });
+
+  test('should handle mixed delimiters', () => {
+    expect(kebabCase('Hello-world_this Is A-test')).toBe('hello-world-this-is-a-test');
+  });
+
+  test('should handle leading/trailing spaces and multiple delimiters', () => {
+    expect(kebabCase('  --Hello World--  ')).toBe('hello-world');
+    expect(kebabCase('__foo   bar__')).toBe('foo-bar');
+  });
+
+  test('should handle strings with numbers', () => {
+    expect(kebabCase('fooBar123')).toBe('foo-bar-123');
+    expect(kebabCase('foo-123-bar')).toBe('foo-123-bar');
+  });
+
+  test('should handle empty string', () => {
     expect(kebabCase('')).toBe('');
   });
 
-  test('should handle non-string inputs', () => {
-    expect(kebabCase(null)).toBe('');
-    expect(kebabCase(undefined)).toBe('');
-    expect(kebabCase(123)).toBe('');
-  });
-
-  test('should handle strings with multiple spaces', () => {
-    expect(kebabCase('  foo   bar  ')).toBe('foo-bar');
+  test('should throw TypeError if argument is not a string', () => {
+    expect(() => kebabCase(null)).toThrow(TypeError);
+    expect(() => kebabCase(undefined)).toThrow(TypeError);
+    expect(() => kebabCase(123)).toThrow(TypeError);
+    expect(() => kebabCase({})).toThrow(TypeError);
   });
 });
