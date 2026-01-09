4100220d326fa747b27bc0310177ccdb4b2721b1
nao-United92 <naoyauver@icloud.com>
2026-01-10 00:20:00 +0900

    feat: Add last utility

--- other/practice/array-last-utils.js
+++ other/practice/array-last-utils.js
@@ -1,5 +1,6 @@
-const last = (arr) => {
-  return arr.length ? arr[arr.length - 1] : undefined;
+export const last = (arr) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return undefined;
+  }
+  return arr[arr.length - 1];
 };
-
-export default last;
