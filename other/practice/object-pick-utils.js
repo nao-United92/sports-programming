ffe68673db573d50c865c9c63babb63b41f3a775
nao-United92 <naoyauver@icloud.com>
2026-01-10 00:18:20 +0900

    feat: Add pick utility

--- other/practice/object-pick-utils.js
+++ other/practice/object-pick-utils.js
@@ -1,18 +1,11 @@
-const pick = (obj, keysToPick) => {
-  if (obj === null || typeof obj !== 'object' || Array.isArray(obj)) {
-    return {}; // Return empty object if input is not a plain object
+export const pick = (obj, ...keys) => {
+  if (typeof obj !== 'object' || obj === null) {
+    return {};
   }
-  if (!Array.isArray(keysToPick)) {
-    throw new TypeError('Expected an array for the keysToPick argument.');
-  }
-
-  const newObj = {};
-  keysToPick.forEach(key => {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      newObj[key] = obj[key];
+  return keys.reduce((acc, key) => {
+    if (key in obj) {
+      acc[key] = obj[key];
     }
-  });
-  return newObj;
+    return acc;
+  }, {});
 };
-
-export default pick;
