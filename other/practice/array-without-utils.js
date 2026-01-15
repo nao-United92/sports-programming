607bc88d042fe8ebc751beb795caa61a17c9443a
nao-United92 <naoyauver@icloud.com>
2026-01-15 23:35:37 +0900

    feat: add 'without' array utility

--- other/practice/array-without-utils.js
+++ other/practice/array-without-utils.js
@@ -1,8 +1,1 @@
-const arrayWithout = (arr, ...values) => {
-  if (!Array.isArray(arr)) {
-    throw new TypeError('Expected an array for the first argument.');
-  }
-  return arr.filter(item => !values.includes(item));
-};
-
-module.exports = arrayWithout;
+export const without = (arr, ...args) => arr.filter(v => !args.includes(v));
--- other/practice/array-without-utils.test.js
+++ other/practice/array-without-utils.test.js
@@ -1,45 +1,21 @@
-const arrayWithout = require('./array-without-utils');
+import { without } from './array-without-utils.js';
 
-describe('arrayWithout', () => {
-  test('should return a new array excluding a single specified value', () => {
-    const arr = [1, 2, 3, 4, 5];
-    expect(arrayWithout(arr, 3)).toEqual([1, 2, 4, 5]);
-  });
-
-  test('should return a new array excluding multiple specified values', () => {
-    const arr = [1, 2, 3, 4, 5];
-    expect(arrayWithout(arr, 2, 4)).toEqual([1, 3, 5]);
-  });
-
-  test('should handle values that are not present in the array', () => {
-    const arr = [1, 2, 3];
-    expect(arrayWithout(arr, 99)).toEqual([1, 2, 3]);
-  });
-
-  test('should handle an empty array', () => {
-    const arr = [];
-    expect(arrayWithout(arr, 1, 2)).toEqual([]);
-  });
-
-  test('should handle an array with duplicate values and remove all occurrences', () => {
-    const arr = [1, 2, 3, 2, 4, 2, 5];
-    expect(arrayWithout(arr, 2)).toEqual([1, 3, 4, 5]);
-  });
-
-  test('should work with mixed types of values', () => {
-    const arr = [1, '2', true, null, undefined, 1, '3'];
-    expect(arrayWithout(arr, 1, '2')).toEqual([true, null, undefined, '3']);
+describe('without', () => {
+  test('should return an array without the specified values', () => {
+    expect(without([2, 1, 2, 3], 1, 2)).toEqual([3]);
   });
 
   test('should not modify the original array', () => {
     const arr = [1, 2, 3];
-    arrayWithout(arr, 2);
+    without(arr, 1);
     expect(arr).toEqual([1, 2, 3]);
   });
 
-  test('should throw TypeError if first argument is not an array', () => {
-    expect(() => arrayWithout(null, 1)).toThrow(TypeError);
-    expect(() => arrayWithout('string', 1)).toThrow(TypeError);
-    expect(() => arrayWithout(123, 1)).toThrow(TypeError);
+  test('should return the array if no values are specified', () => {
+    expect(without([1, 2, 3])).toEqual([1, 2, 3]);
+  });
+
+  test('should return an empty array for an empty array', () => {
+    expect(without([], 1, 2)).toEqual([]);
   });
 });
