c58e4f27cd9da4d0a83accea65c950c3f4452d66
nao-United92 <naoyauver@icloud.com>
2026-01-15 23:22:13 +0900

    feat: add 'partition' array utility

--- other/practice/array-partition-utils.js
+++ other/practice/array-partition-utils.js
@@ -1,23 +1,8 @@
-const partition = (arr, predicate) => {
-  if (!Array.isArray(arr)) {
-    throw new TypeError('Expected an array for the first argument.');
-  }
-  if (typeof predicate !== 'function') {
-    throw new TypeError('Expected a function for the predicate argument.');
-  }
-
-  const pass = [];
-  const fail = [];
-
-  arr.forEach(item => {
-    if (predicate(item)) {
-      pass.push(item);
-    } else {
-      fail.push(item);
-    }
-  });
-
-  return [pass, fail];
-};
-
-export default partition;
+export const partition = (arr, fn) =>
+  arr.reduce(
+    (acc, val, i, arr) => {
+      acc[fn(val, i, arr) ? 0 : 1].push(val);
+      return acc;
+    },
+    [[], []]
+  );
