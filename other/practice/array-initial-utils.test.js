cbb1fc3ccb627bde4cb62daae00a14941b10f661
nao-United92 <naoyauver@icloud.com>
2026-01-15 23:17:36 +0900

    feat: add 'initial' array utility

--- other/practice/array-initial-utils.test.js
+++ other/practice/array-initial-utils.test.js
@@ -1,22 +1,15 @@
-const initial = require('./array-initial-utils');
+import { initial } from './array-initial-utils.js';
 
 describe('initial', () => {
-  it('should return all but the last element of an array', () => {
+  test('should return all but the last element of an array', () => {
     expect(initial([1, 2, 3])).toEqual([1, 2]);
   });
 
-  it('should return an empty array when given a single-element array', () => {
+  test('should return an empty array for a single-element array', () => {
     expect(initial([1])).toEqual([]);
   });
 
-  it('should return an empty array for an empty array', () => {
+  test('should return an empty array for an empty array', () => {
     expect(initial([])).toEqual([]);
-  });
-
-  it('should return an empty array for non-array inputs', () => {
-    expect(initial(null)).toEqual([]);
-    expect(initial(undefined)).toEqual([]);
-    expect(initial({})).toEqual([]);
-    expect(initial('hello')).toEqual([]);
   });
 });
