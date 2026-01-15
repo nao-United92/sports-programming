1adbf59c045d3a6e9f30bfe800269624d7367e86
nao-United92 <naoyauver@icloud.com>
2026-01-15 23:27:44 +0900

    feat: add 'shuffle' array utility

--- other/practice/array-shuffle-utils.test.js
+++ other/practice/array-shuffle-utils.test.js
@@ -1,35 +1,20 @@
 import { shuffle } from './array-shuffle-utils.js';
 
 describe('shuffle', () => {
+  const arr = [1, 2, 3, 4, 5];
+
+  test('should return an array with the same length', () => {
+    expect(shuffle(arr)).toHaveLength(5);
+  });
+
   test('should return an array with the same elements', () => {
-    const arr = [1, 2, 3, 4, 5];
     const shuffled = shuffle(arr);
-    expect(shuffled).toHaveLength(arr.length);
     expect(shuffled.sort()).toEqual(arr.sort());
   });
 
   test('should not modify the original array', () => {
-    const arr = [1, 2, 3];
     const originalArr = [...arr];
     shuffle(arr);
-    expect(arr).toEqual(originalArr);
-  });
-
-  test('should return a new array instance', () => {
-    const arr = [1, 2, 3];
-    const shuffled = shuffle(arr);
-    expect(shuffled).not.toBe(arr);
-  });
-
-  test('should handle an empty array', () => {
-    const arr = [];
-    const shuffled = shuffle(arr);
-    expect(shuffled).toEqual([]);
-  });
-
-  test('should handle non-array input by returning it as is', () => {
-    const notAnArray = { a: 1 };
-    const result = shuffle(notAnArray);
-    expect(result).toBe(notAnArray);
+    expect(originalArr).toEqual(arr);
   });
 });
