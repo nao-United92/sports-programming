b1fc252f7a28126f9244fe20b5e0be51127980ca
nao-United92 <naoyauver@icloud.com>
2026-01-15 23:28:40 +0900

    feat: add 'take' array utility

--- other/practice/array-take-utils.test.js
+++ other/practice/array-take-utils.test.js
@@ -1,23 +1,23 @@
-import take from './array-take-utils';
+import { take } from './array-take-utils.js';
 
 describe('take', () => {
-  test('should take the first n elements from an array', () => {
-    expect(take([1, 2, 3, 4, 5], 3)).toEqual([1, 2, 3]);
+  test('should take the first n elements of an array', () => {
+    expect(take([1, 2, 3], 2)).toEqual([1, 2]);
   });
 
   test('should take the first element by default', () => {
     expect(take([1, 2, 3])).toEqual([1]);
   });
 
+  test('should return all elements if n is greater than length', () => {
+    expect(take([1, 2, 3], 5)).toEqual([1, 2, 3]);
+  });
+
   test('should return an empty array if n is 0', () => {
     expect(take([1, 2, 3], 0)).toEqual([]);
   });
 
-  test('should return the whole array if n is larger than the array length', () => {
-    expect(take([1, 2, 3], 5)).toEqual([1, 2, 3]);
-  });
-
-  test('should return an empty array for non-array inputs', () => {
-    expect(take(null, 2)).toEqual([]);
+  test('should return an empty array for an empty array', () => {
+    expect(take([], 2)).toEqual([]);
   });
 });
