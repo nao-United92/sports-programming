0223c6166217fc6685d12a21c370e159cf3a1696
nao-United92 <naoyauver@icloud.com>
2026-01-15 23:18:48 +0900

    feat: add 'intersection' array utility

--- other/practice/array-intersection-utils.js
+++ other/practice/array-intersection-utils.js
@@ -1,15 +1,6 @@
-export const intersection = (...arrays) => {
-  if (arrays.length === 0) {
-    return [];
-  }
-
-  // Convert the first array to a Set for efficient lookup
-  const firstArraySet = new Set(arrays[0]);
-
-  // Iterate over the rest of the arrays and filter elements
-  // that are present in all previous arrays.
-  return arrays.slice(1).reduce((acc, currentArray) => {
-    const currentArraySet = new Set(currentArray);
-    return acc.filter(item => currentArraySet.has(item));
-  }, [...firstArraySet]); // Start with unique elements from the first array
+export const intersection = (a, ...args) => {
+  const setA = new Set(a);
+  return [...new Set(args.reduce((acc, arr) => {
+    return arr.filter(item => setA.has(item));
+  }, []))];
 };
--- other/practice/array-intersection-utils.test.js
+++ other/practice/array-intersection-utils.test.js
@@ -1,37 +1,19 @@
-import { intersection } from './array-intersection-utils';
+import { intersection } from './array-intersection-utils.js';
 
 describe('intersection', () => {
-  test('should return unique values that are included in all given arrays', () => {
-    expect(intersection([2, 1], [2, 3])).toEqual([2]);
+  test('should return the intersection of two arrays', () => {
+    expect(intersection([1, 2, 3], [2, 3, 4])).toEqual([2, 3]);
   });
 
-  test('should handle multiple arrays', () => {
+  test('should return the intersection of multiple arrays', () => {
     expect(intersection([1, 2, 3], [2, 3, 4], [3, 4, 5])).toEqual([3]);
   });
 
+  test('should return an empty array if there is no intersection', () => {
+    expect(intersection([1, 2, 3], [4, 5, 6])).toEqual([]);
+  });
+
   test('should handle empty arrays', () => {
-    expect(intersection([], [1, 2, 3])).toEqual([]);
-    expect(intersection([1, 2, 3], [])).toEqual([]);
-    expect(intersection([], [])).toEqual([]);
-  });
-
-  test('should handle arrays with no common elements', () => {
-    expect(intersection([1, 2], [3, 4])).toEqual([]);
-  });
-
-  test('should handle arrays with all common elements', () => {
-    expect(intersection([1, 2, 3], [1, 2, 3])).toEqual([1, 2, 3]);
-  });
-
-  test('should maintain order of appearance based on the first array', () => {
-    expect(intersection([1, 5, 2, 3], [2, 3, 5])).toEqual([5, 2, 3]);
-  });
-
-  test('should handle different data types', () => {
-    expect(intersection([1, 'a', 2], [2, 'a', 3])).toEqual(['a', 2]);
-  });
-
-  test('should return empty array if no arrays are provided', () => {
-    expect(intersection()).toEqual([]);
+    expect(intersection([], [1, 2])).toEqual([]);
   });
 });
