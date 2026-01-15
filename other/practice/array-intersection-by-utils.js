eab22f9cfa0b2de82ad70097d8b1443bc76ffba1
nao-United92 <naoyauver@icloud.com>
2026-01-15 23:19:32 +0900

    feat: add 'intersectionBy' array utility

--- other/practice/array-intersection-by-utils.js
+++ other/practice/array-intersection-by-utils.js
@@ -1,23 +1,4 @@
-const intersectionBy = (...arrays) => {
-  const iteratee = arrays.pop();
-  const iterateeFunc = typeof iteratee === 'function'
-    ? iteratee
-    : (item) => item[iteratee];
-
-  if (arrays.length === 0) {
-    return [];
-  }
-  if (arrays.length === 1) {
-    return [...arrays[0]];
-  }
-  
-  const [firstArr, ...restArrs] = arrays;
-  const sets = restArrs.map(arr => new Set(arr.map(iterateeFunc)));
-
-  return firstArr.filter(item => {
-    const mappedItem = iterateeFunc(item);
-    return sets.every(set => set.has(mappedItem));
-  });
+export const intersectionBy = (a, b, fn) => {
+  const s = new Set(b.map(fn));
+  return a.filter(x => s.has(fn(x)));
 };
-
-module.exports = { intersectionBy };
