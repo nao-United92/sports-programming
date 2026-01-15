0200343594fd31aa84d2c8a4f5538896e74efa46
nao-United92 <naoyauver@icloud.com>
2026-01-15 23:24:58 +0900

    feat: add 'sample' array utility

--- other/practice/array-sample-utils.test.js
+++ other/practice/array-sample-utils.test.js
@@ -1,26 +1,17 @@
 import { sample } from './array-sample-utils.js';
 
 describe('sample', () => {
-  test('should return a random element from the array', () => {
+  test('should return one of the elements in the array', () => {
     const arr = [1, 2, 3, 4, 5];
     const result = sample(arr);
     expect(arr).toContain(result);
   });
 
   test('should return undefined for an empty array', () => {
-    const arr = [];
-    const result = sample(arr);
-    expect(result).toBeUndefined();
-  });
-
-  test('should return undefined for a non-array input', () => {
-    const result = sample('not an array');
-    expect(result).toBeUndefined();
+    expect(sample([])).toBeUndefined();
   });
 
   test('should return the only element for a single-element array', () => {
-    const arr = ['a'];
-    const result = sample(arr);
-    expect(result).toBe('a');
+    expect(sample([1])).toBe(1);
   });
 });
