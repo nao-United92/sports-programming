6fb4a220a7030430a8a5e7312d657b40d8f71196
nao-United92 <naoyauver@icloud.com>
2026-01-16 23:01:08 +0900

    feat: Add arrayInitial utility to get all but the last element of an array

--- other/practice/array-initial-utils.test.js
+++ other/practice/array-initial-utils.test.js
@@ -1,15 +1,38 @@
-import { initial } from './array-initial-utils.js';
+const arrayInitial = require('./array-initial-utils');
 
-describe('initial', () => {
+describe('arrayInitial', () => {
   test('should return all but the last element of an array', () => {
-    expect(initial([1, 2, 3])).toEqual([1, 2]);
+    const arr = [1, 2, 3, 4, 5];
+    expect(arrayInitial(arr)).toEqual([1, 2, 3, 4]);
   });
 
-  test('should return an empty array for a single-element array', () => {
-    expect(initial([1])).toEqual([]);
+  test('should return an empty array if the input array has one element', () => {
+    const arr = [1];
+    expect(arrayInitial(arr)).toEqual([]);
   });
 
-  test('should return an empty array for an empty array', () => {
-    expect(initial([])).toEqual([]);
+  test('should return an empty array if the input array is empty', () => {
+    const arr = [];
+    expect(arrayInitial(arr)).toEqual([]);
+  });
+
+  test('should not modify the original array', () => {
+    const arr = [1, 2, 3];
+    const originalArr = [...arr];
+    arrayInitial(arr);
+    expect(arr).toEqual(originalArr);
+  });
+
+  test('should handle arrays with mixed types', () => {
+    const arr = [1, 'a', null, { key: 'value' }, undefined];
+    expect(arrayInitial(arr)).toEqual([1, 'a', null, { key: 'value' }]);
+  });
+
+  test('should throw an error if the input is not an array', () => {
+    expect(() => arrayInitial(null)).toThrow('Expected an array for the first argument.');
+    expect(() => arrayInitial(undefined)).toThrow('Expected an array for the first argument.');
+    expect(() => arrayInitial('string')).toThrow('Expected an array for the first argument.');
+    expect(() => arrayInitial(123)).toThrow('Expected an array for the first argument.');
+    expect(() => arrayInitial({})).toThrow('Expected an array for the first argument.');
   });
 });
