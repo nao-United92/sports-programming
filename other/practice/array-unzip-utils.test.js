21efd95d291e848f8eff77613a78e453d9d01c67
nao-United92 <naoyauver@icloud.com>
2026-01-16 22:56:39 +0900

    feat: Add arrayUnzip utility to inverse arrayZip

--- other/practice/array-unzip-utils.test.js
+++ other/practice/array-unzip-utils.test.js
@@ -1,15 +1,45 @@
-import { unzip } from './array-unzip-utils.js';
+const arrayUnzip = require('./array-unzip-utils');
 
-describe('unzip', () => {
-  test('should unzip an array of arrays', () => {
-    expect(unzip([['a', 1, true], ['b', 2, false]])).toEqual([['a', 'b'], [1, 2], [true, false]]);
+describe('arrayUnzip', () => {
+  test('should unzip an array of grouped elements correctly', () => {
+    const zipped = [[1, 'a'], [2, 'b'], [3, 'c']];
+    expect(arrayUnzip(zipped)).toEqual([[1, 2, 3], ['a', 'b', 'c']]);
   });
 
-  test('should handle arrays with different lengths', () => {
-    expect(unzip([['a', 1], ['b', 2, true]])).toEqual([['a', 'b'], [1, 2], [true]]);
+  test('should handle unzipping multiple arrays', () => {
+    const zipped = [[1, 'a', true], [2, 'b', false]];
+    expect(arrayUnzip(zipped)).toEqual([[1, 2], ['a', 'b'], [true, false]]);
   });
 
-  test('should return an empty array for an empty input array', () => {
-    expect(unzip([])).toEqual([]);
+  test('should handle arrays of different lengths (padding with undefined)', () => {
+    const zipped = [[1, 'a'], [2, 'b'], [3, undefined]];
+    expect(arrayUnzip(zipped)).toEqual([[1, 2, 3], ['a', 'b', undefined]]);
+  });
+
+  test('should handle an empty input array', () => {
+    expect(arrayUnzip([])).toEqual([]);
+  });
+
+  test('should handle an input array containing empty arrays', () => {
+    const zipped = [[], [], []];
+    expect(arrayUnzip(zipped)).toEqual([]);
+  });
+
+  test('should handle a single element in grouped arrays', () => {
+    const zipped = [[1], [2], [3]];
+    expect(arrayUnzip(zipped)).toEqual([[1, 2, 3]]);
+  });
+
+  test('should throw an error if the main argument is not an array', () => {
+    expect(() => arrayUnzip(null)).toThrow('Expected an array for the first argument.');
+    expect(() => arrayUnzip(undefined)).toThrow('Expected an array for the first argument.');
+    expect(() => arrayUnzip('string')).toThrow('Expected an array for the first argument.');
+  });
+
+  test('should throw an error if elements within the zipped array are not arrays', () => {
+    const zipped = [[1, 'a'], 2, [3, 'c']];
+    expect(() => arrayUnzip(zipped)).toThrow('Expected an array of arrays.');
+    const zipped2 = [{}, []];
+    expect(() => arrayUnzip(zipped2)).toThrow('Expected an array of arrays.');
   });
 });
