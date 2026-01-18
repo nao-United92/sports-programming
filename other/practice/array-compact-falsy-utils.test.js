505ec1dca38f662619773e0775f24e823f4f029f
nao-United92 <naoyauver@icloud.com>
2026-01-18 10:22:34 +0900

    feat: Add arrayCompactFalsy utility to remove all falsy values

--- other/practice/array-compact-falsy-utils.test.js
+++ other/practice/array-compact-falsy-utils.test.js
@@ -1,9 +1,11 @@
+// other/practice/array-compact-falsy-utils.test.js
+
 const arrayCompactFalsy = require('./array-compact-falsy-utils');
 
 describe('arrayCompactFalsy', () => {
   test('should remove all falsy values from an array', () => {
-    const arr = [0, 1, false, 2, '', 3, null, 'a', undefined, NaN, 'b'];
-    expect(arrayCompactFalsy(arr)).toEqual([1, 2, 3, 'a', 'b']);
+    const arr = [0, 1, false, 2, '', 3, null, 4, undefined, 5, NaN, 'hello'];
+    expect(arrayCompactFalsy(arr)).toEqual([1, 2, 3, 4, 5, 'hello']);
   });
 
   test('should return an empty array if all values are falsy', () => {
@@ -11,29 +13,26 @@ describe('arrayCompactFalsy', () => {
     expect(arrayCompactFalsy(arr)).toEqual([]);
   });
 
-  test('should return the same array if no falsy values are present', () => {
-    const arr = [1, 'hello', true, {},
-      []
-    ];
-    expect(arrayCompactFalsy(arr)).toEqual([1, 'hello', true, {},
-      []
-    ]);
-  });
-
-  test('should handle an empty array', () => {
-    const arr = [];
-    expect(arrayCompactFalsy(arr)).toEqual([]);
+  test('should return an empty array if the input array is empty', () => {
+    expect(arrayCompactFalsy([])).toEqual([]);
   });
 
   test('should not modify the original array', () => {
-    const arr = [0, 1, false];
-    arrayCompactFalsy(arr);
-    expect(arr).toEqual([0, 1, false]);
+    const originalArr = [0, 1, false];
+    arrayCompactFalsy(originalArr);
+    expect(originalArr).toEqual([0, 1, false]);
   });
 
-  test('should throw TypeError if argument is not an array', () => {
-    expect(() => arrayCompactFalsy(null)).toThrow(TypeError);
-    expect(() => arrayCompactFalsy(123)).toThrow(TypeError);
-    expect(() => arrayCompactFalsy('string')).toThrow(TypeError);
+  test('should handle arrays with no falsy values', () => {
+    const arr = [1, 'hello', true, {}, []];
+    expect(arrayCompactFalsy(arr)).toEqual([1, 'hello', true, {}, []]);
+  });
+
+  test('should handle non-array input by returning an empty array', () => {
+    expect(arrayCompactFalsy(null)).toEqual([]);
+    expect(arrayCompactFalsy(undefined)).toEqual([]);
+    expect(arrayCompactFalsy(123)).toEqual([]);
+    expect(arrayCompactFalsy('string')).toEqual([]);
+    expect(arrayCompactFalsy({})).toEqual([]);
   });
 });
