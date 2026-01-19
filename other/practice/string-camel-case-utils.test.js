662a1dcb5a9af14e86aa03098f10b2008b7904ac
nao-United92 <naoyauver@icloud.com>
2026-01-19 19:44:24 +0900

    feat: Add camelCase utility function

--- other/practice/string-camel-case-utils.test.js
+++ other/practice/string-camel-case-utils.test.js
@@ -1,48 +1,36 @@
-import camelCase from './string-camel-case-utils';
+const { camelCase } = require('./string-camel-case-utils');
 
 describe('camelCase', () => {
-  test('should convert kebab-case to camelCase', () => {
-    expect(camelCase('hello-world')).toBe('helloWorld');
-    expect(camelCase('foo-bar-baz')).toBe('fooBarBaz');
-  });
-
-  test('should convert snake_case to camelCase', () => {
+  it('should convert a snake_case string to camelCase', () => {
     expect(camelCase('hello_world')).toBe('helloWorld');
-    expect(camelCase('foo_bar_baz')).toBe('fooBarBaz');
   });
 
-  test('should convert space-separated words to camelCase', () => {
-    expect(camelCase('Hello World')).toBe('helloWorld');
-    expect(camelCase('foo bar baz')).toBe('fooBarBaz');
+  it('should convert a kebab-case string to camelCase', () => {
+    expect(camelCase('hello-world')).toBe('helloWorld');
   });
 
-  test('should handle mixed delimiters', () => {
-    expect(camelCase('Hello-world_this is a-test')).toBe('helloWorldThisIsATest');
+  it('should convert a space-separated string to camelCase', () => {
+    expect(camelCase('hello world')).toBe('helloWorld');
   });
 
-  test('should handle leading/trailing spaces and multiple delimiters', () => {
-    expect(camelCase('  --Hello World--  ')).toBe('helloWorld');
-    expect(camelCase('__foo   bar__')).toBe('fooBar');
+  it('should convert a mixed case string to camelCase', () => {
+    expect(camelCase('Foo Bar')).toBe('fooBar');
   });
 
-  test('should handle strings with existing camelCase by converting them fully', () => {
-    expect(camelCase('existingCamelCase')).toBe('existingcamelcase');
-    expect(camelCase('AnotherExistingCamelCase')).toBe('anotherexistingcamelcase');
+  it('should handle strings with multiple separators', () => {
+    expect(camelCase('foo--bar__baz')).toBe('fooBarBaz');
   });
 
-  test('should handle numbers within the string', () => {
-    expect(camelCase('foo-bar-123')).toBe('fooBar123');
-    expect(camelCase('foo-123-bar')).toBe('foo123Bar');
+  it('should handle leading/trailing separators', () => {
+    expect(camelCase('-foo-bar')).toBe('fooBar');
+    expect(camelCase('foo-bar-')).toBe('fooBar');
   });
 
-  test('should handle empty string', () => {
+  it('should handle single word strings', () => {
+    expect(camelCase('hello')).toBe('hello');
+  });
+
+  it('should handle empty strings', () => {
     expect(camelCase('')).toBe('');
-  });
-
-  test('should throw TypeError if argument is not a string', () => {
-    expect(() => camelCase(null)).toThrow(TypeError);
-    expect(() => camelCase(undefined)).toThrow(TypeError);
-    expect(() => camelCase(123)).toThrow(TypeError);
-    expect(() => camelCase({})).toThrow(TypeError);
   });
 });
