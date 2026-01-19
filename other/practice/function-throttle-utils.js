4694f811ca0c39e67bf857209b6ca811967a7ae4
nao-United92 <naoyauver@icloud.com>
2026-01-19 19:11:19 +0900

    feat: Add throttle utility function

--- other/practice/function-throttle-utils.js
+++ other/practice/function-throttle-utils.js
@@ -1,49 +1,14 @@
-export const throttle = (func, wait) => {
-  let timeoutId = null;
-  let lastArgs = null;
-  let lastThis = null;
-  let lastResult = null;
-  let lastExecTime = 0;
-
-  const throttled = function(...args) {
-    const now = Date.now();
-    lastArgs = args;
-    lastThis = this;
-
-    if (!lastExecTime) {
-      lastExecTime = now; // Initialize lastExecTime on first call
+const throttle = (func, limit) => {
+  let inThrottle;
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
     }
-
-    // Calculate time remaining until next allowed execution
-    const remaining = wait - (now - lastExecTime);
-
-    // If remaining time is <= 0 or remaining is greater than wait
-    // (meaning system clock changed or wait is very small)
-    if (remaining <= 0 || remaining > wait) {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-        timeoutId = null;
-      }
-      lastExecTime = now;
-      lastResult = func.apply(lastThis, lastArgs);
-    } else if (!timeoutId) {
-      // If there's a delay, schedule a trailing call
-      timeoutId = setTimeout(() => {
-        lastExecTime = Date.now();
-        timeoutId = null;
-        lastResult = func.apply(lastThis, lastArgs);
-      }, remaining);
-    }
-    return lastResult;
   };
-
-  throttled.cancel = () => {
-    clearTimeout(timeoutId);
-    timeoutId = null;
-    lastExecTime = 0;
-    lastArgs = null;
-    lastThis = null;
-  };
-
-  return throttled;
 };
+
+module.exports = { throttle };
