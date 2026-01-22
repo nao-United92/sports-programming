807ddb2e5a96c0564c2068f0793162d1f967dfdd
nao-United92 <naoyauver@icloud.com>
2026-01-22 12:04:43 +0900

    feat: Add getRandomElement utility function

--- other/practice/array-get-random-element-utils.test.js
+++ other/practice/array-get-random-element-utils.test.js
@@ -1,43 +1,23 @@
-const getRandomElement = require('./array-get-random-element-utils');
+import { getRandomElement } from './array-get-random-element-utils';
 
 describe('getRandomElement', () => {
-  test('should return undefined for an empty array', () => {
+  it('should return an element from the array', () => {
+    const array = [1, 2, 3, 4, 5];
+    const randomElement = getRandomElement(array);
+    expect(array).toContain(randomElement);
+  });
+
+  it('should return undefined for an empty array', () => {
     expect(getRandomElement([])).toBeUndefined();
   });
 
-  test('should return undefined for non-array input', () => {
+  it('should return undefined for a null or undefined array', () => {
     expect(getRandomElement(null)).toBeUndefined();
     expect(getRandomElement(undefined)).toBeUndefined();
-    expect(getRandomElement(123)).toBeUndefined();
-    expect(getRandomElement('string')).toBeUndefined();
-    expect(getRandomElement({})).toBeUndefined();
   });
 
-  test('should return an element from the array', () => {
-    const arr = [1, 2, 3, 4, 5];
-    const randomElement = getRandomElement(arr);
-    expect(arr).toContain(randomElement);
-  });
-
-  test('should return one of the elements for an array with one element', () => {
-    const arr = ['single'];
-    expect(getRandomElement(arr)).toBe('single');
-  });
-
-  test('should return different elements over multiple calls (probabilistic)', () => {
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const results = new Set();
-    // Call multiple times to increase probability of getting different elements
-    for (let i = 0; i < 50; i++) {
-      results.add(getRandomElement(arr));
-    }
-    // Expect to have more than one unique element if the array has many
-    if (arr.length > 1) {
-      expect(results.size).toBeGreaterThan(1);
-    }
-    // All results must be in the original array
-    results.forEach(element => {
-      expect(arr).toContain(element);
-    });
+  it('should return the only element for a single-element array', () => {
+    const array = ['hello'];
+    expect(getRandomElement(array)).toBe('hello');
   });
 });
