cedc183bd887f3a35bbc1cf23b53d405d047a65f
nao-United92 <naoyauver@icloud.com>
2026-01-22 12:06:17 +0900

    feat: Add capitalize utility function

--- other/practice/string-capitalize-utils.js
+++ other/practice/string-capitalize-utils.js
@@ -1,8 +1,6 @@
-const capitalize = (str) => {
+export const capitalize = (str) => {
   if (typeof str !== 'string' || str.length === 0) {
     return '';
   }
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
-
-module.exports = { capitalize };
