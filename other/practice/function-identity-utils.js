f336ab3365edbb6f9255f957fd2e0434e7d15055
nao-United92 <naoyauver@icloud.com>
2026-01-23 23:38:28 +0900

    feat: Add identity utility function

---
+++ other/practice/function-identity-utils.js
@@ -0,0 +1,9 @@
+/**
+ * This method returns the first argument it receives.
+ *
+ * @param {*} value The value to return.
+ * @returns {*} Returns `value`.
+ */
+const identity = (value) => value;
+
+export { identity };
