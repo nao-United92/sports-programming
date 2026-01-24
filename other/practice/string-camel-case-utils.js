6589eea9441b462d76676859dd07a08cc3c5b363
nao-United92 <naoyauver@icloud.com>
2026-01-24 12:19:16 +0900

    feat: Add camelCase function and tests

--- other/practice/string-camel-case-utils.js
+++ other/practice/string-camel-case-utils.js
@@ -1,5 +1,33 @@
-const camelCase = (str) => {
-  return str.replace(/[^a-zA-Z0-9]+(.)?/g, (match, chr) => chr ? chr.toUpperCase() : '').replace(/^./, (match) => match.toLowerCase());
-};
+/**
+ * Converts a string to camelCase.
+ * Example: "Foo Bar" -> "fooBar", "foo-bar" -> "fooBar", "__FOO_BAR__" -> "fooBar"
+ *
+ * @param {string} str The string to convert.
+ * @returns {string} The camelCased string.
+ */
+function camelCase(str) {
+  if (typeof str !== 'string') {
+    throw new TypeError('Expected a string for the input.');
+  }
+  if (str.length === 0) {
+    return '';
+  }
 
-module.exports = { camelCase };
+  const words = str
+    .replace(/[^a-zA-Z0-9]+(.)?/g, ' $1') // Replace non-alphanumeric separators with spaces, capturing next char
+    .replace(/([A-Z])/g, ' $1')          // Add space before uppercase letters
+    .split(/\s+/)                       // Split by multiple spaces
+    .filter(Boolean);                   // Remove empty strings
+
+  if (words.length === 0) {
+    return '';
+  }
+
+  let result = words[0].toLowerCase();
+  for (let i = 1; i < words.length; i++) {
+    result += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+  }
+  return result;
+}
+
+export default camelCase;
