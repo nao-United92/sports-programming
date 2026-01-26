e0f41faffa817d90db94fa95eb8aaf984c5ef10b
nao-United92 <naoyauver@icloud.com>
2026-01-27 00:36:13 +0900

    feat: Add throttle function

---
+++ other/practice/throttle.js
@@ -0,0 +1,23 @@
+const throttle = (func, limit) => {
+  let inThrottle;
+  let lastFunc;
+  let lastRan;
+  return function(...args) {
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      lastRan = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
+
+module.exports = { throttle };
