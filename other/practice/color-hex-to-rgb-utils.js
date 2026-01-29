d7496423cdfd8ce7b6c455d8291bda13466c6d2c
nao-United92 <naoyauver@icloud.com>
2026-01-29 18:37:21 +0900

    feat: Add hexToRgb utility for color conversion

---
+++ other/practice/color-hex-to-rgb-utils.js
@@ -0,0 +1,19 @@
+const hexToRgb = (hex) => {
+  if (typeof hex !== 'string' || !hex) {
+    return null;
+  }
+
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
+
+export default hexToRgb;
