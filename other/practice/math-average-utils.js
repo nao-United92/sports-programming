40202687d45ce4fd28c7adf2eeaceb5d1fae3fe0
nao-United92 <naoyauver@icloud.com>
2026-01-29 18:25:32 +0900

    feat: Add average utility for math

--- other/practice/math-average-utils.js
+++ other/practice/math-average-utils.js
@@ -1,7 +1,9 @@
-export const average = (arr) => {
+const average = (arr) => {
   if (!Array.isArray(arr) || arr.length === 0) {
     return 0;
   }
   const sum = arr.reduce((acc, val) => acc + val, 0);
   return sum / arr.length;
 };
+
+export default average;
