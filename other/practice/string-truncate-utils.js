3af6f01ba45619ef3443a0a58a0520c32c00d3cb
nao-United92 <naoyauver@icloud.com>
2026-01-29 18:30:15 +0900

    feat: Add truncate utility for strings

--- other/practice/string-truncate-utils.js
+++ other/practice/string-truncate-utils.js
@@ -1,9 +1,19 @@
-export const truncate = (str, length, omission = '...') => {
-  if (typeof str !== 'string') {
+const truncate = (str, length, ending = '...') => {
+  if (typeof str !== 'string' || !str) {
     return '';
   }
-  if (str.length <= length) {
+  if (length === null || length === undefined || length < 0) {
     return str;
   }
-  return str.slice(0, length) + omission;
+  if (str.length > length) {
+    // Ensure that the ending is always accounted for in the length
+    if (length <= ending.length) {
+      return ending.substring(0, length) || ''; // Return ending or empty string if length is too small
+    }
+    return str.slice(0, length - ending.length) + ending;
+  } else {
+    return str;
+  }
 };
+
+export default truncate;
