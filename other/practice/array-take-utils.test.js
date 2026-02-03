f7cf6dcd7d7e6068bb0c9b6eafd613cf3340f898
nao-United92 <naoyauver@icloud.com>
2026-02-04 00:21:52 +0900

    feat: Add take utility for arrays

--- other/practice/array-take-utils.test.js
+++ other/practice/array-take-utils.test.js
@@ -1,23 +1,27 @@
 import { take } from './array-take-utils.js';
 
 describe('take', () => {
-  test('should take the first n elements of an array', () => {
-    expect(take([1, 2, 3], 2)).toEqual([1, 2]);
+  it('should take the first n elements from an array', () => {
+    const arr = [1, 2, 3, 4, 5];
+    expect(take(arr, 2)).toEqual([1, 2]);
   });
 
-  test('should take the first element by default', () => {
-    expect(take([1, 2, 3])).toEqual([1]);
+  it('should take the first element if n is not provided', () => {
+    const arr = [1, 2, 3];
+    expect(take(arr)).toEqual([1]);
   });
 
-  test('should return all elements if n is greater than length', () => {
-    expect(take([1, 2, 3], 5)).toEqual([1, 2, 3]);
+  it('should return an empty array if n is 0', () => {
+    const arr = [1, 2, 3];
+    expect(take(arr, 0)).toEqual([]);
   });
 
-  test('should return an empty array if n is 0', () => {
-    expect(take([1, 2, 3], 0)).toEqual([]);
+  it('should return the whole array if n is greater than the array length', () => {
+    const arr = [1, 2, 3];
+    expect(take(arr, 5)).toEqual([1, 2, 3]);
   });
 
-  test('should return an empty array for an empty array', () => {
+  it('should handle an empty array', () => {
     expect(take([], 2)).toEqual([]);
   });
 });
