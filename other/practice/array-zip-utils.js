d8abe669e22dedaedbdeaa78eea9b7fda611295c
nao-United92 <naoyauver@icloud.com>
2026-02-06 22:59:22 +0900

    feat: Add array zip utility

--- other/practice/array-zip-utils.js
+++ other/practice/array-zip-utils.js
@@ -1,9 +1,15 @@
-const zip = (...arrays) => {
-  if (arrays.length === 0) {
-    return [];
+/**
+ * Creates an array of grouped elements, the first of which contains the first elements
+ * of the given arrays, the second of which contains the second elements, and so on.
+ * @param {...Array<any>} arrays The arrays to process.
+ * @returns {Array<Array<any>>} A new array of grouped elements.
+ */
+function zip(...arrays) {
+  if (arrays.some(arr => !Array.isArray(arr))) {
+    throw new TypeError('Expected all arguments to be arrays');
   }
   const maxLength = Math.max(...arrays.map(arr => arr.length));
   return Array.from({ length: maxLength }, (_, i) => arrays.map(arr => arr[i]));
-};
+}
 
-export default zip;
+module.exports = zip;
