08cf05b96c7a34ca5d3996f6ce663c2dd5eaa26f
nao-United92 <naoyauver@icloud.com>
2026-02-07 10:41:05 +0900

    feat: Add capitalize function to uppercase the first letter of a string

---
+++ other/practice/string-case-modifier.js
@@ -0,0 +1,8 @@
+const capitalize = (str) => {
+  if (typeof str !== 'string' || str.length === 0) {
+    return '';
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+module.exports = { capitalize };
