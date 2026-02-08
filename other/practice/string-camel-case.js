54f37a95786381b885e0a5b8b24b21ac82b157b9
nao-United92 <naoyauver@icloud.com>
2026-02-08 10:11:22 +0900

    feat: Add camelCase function to convert strings to camel case

--- other/practice/string-camel-case.js
+++ other/practice/string-camel-case.js
@@ -1,39 +1,6 @@
-// other/practice/string-camel-case.js
-/**
- * Converts a string to camel case.
- *
- * @param {string} str The string to convert.
- * @returns {string} The camel cased string.
- * @example
- *
- * stringCamelCase('hello world');
- * // => 'helloWorld'
- *
- * stringCamelCase('foo-bar');
- * // => 'fooBar'
- *
- * stringCamelCase('baz_qux');
- * // => 'bazQux'
- *
- * stringCamelCase('--foo-bar--');
- * // => 'fooBar'
- *
- * stringCamelCase('foo bar baz');
- * // => 'fooBarBaz'
- *
- * stringCamelCase('alreadyCamelCase');
- * // => 'alreadyCamelCase'
- *
- * stringCamelCase('HTML DOM');
- * // => 'htmlDom'
- */
-function stringCamelCase(str) {
-  if (typeof str !== 'string' || str.length === 0) {
-    return '';
-  }
+const camelCase = (str) =>
+  str
+    .replace(/[^a-zA-Z0-9]+(.)?/g, (match, chr) => (chr ? chr.toUpperCase() : ''))
+    .replace(/^./, (str) => str.toLowerCase());
 
-  return str.replace(/[^a-zA-Z0-9]+(.)?/g, (match, chr) => chr ? chr.toUpperCase() : '')
-            .replace(/^./, (match) => match.toLowerCase());
-}
-
-module.exports = stringCamelCase;
+export { camelCase };
