7ba1269bf93330290f8335a1b10f6ec3cff3a0b1
nao-United92 <naoyauver@icloud.com>
2026-02-11 18:43:02 +0900

    feat: Add pull utility function

--- other/practice/array-pull-utils.js
+++ other/practice/array-pull-utils.js
@@ -1,7 +1,14 @@
-export const pull = (arr, ...args) => {
-  const argState = Array.isArray(args[0]) ? args[0] : args;
-  const pulled = arr.filter((v, i) => !argState.includes(v));
-  arr.length = 0;
-  pulled.forEach(v => arr.push(v));
+const pull = (arr, ...values) => {
+  const valuesToRemove = new Set(values);
+  let i = 0;
+  while (i < arr.length) {
+    if (valuesToRemove.has(arr[i])) {
+      arr.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
   return arr;
 };
+
+export { pull };
