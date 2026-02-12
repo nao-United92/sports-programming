ce836ba5110e8b22ca51f63fd1374f4999fca615
nao-United92 <naoyauver@icloud.com>
2026-02-12 18:53:08 +0900

    feat: Add array-difference utility

--- other/practice/array-difference.js
+++ other/practice/array-difference.js
@@ -1,4 +1,23 @@
-export const difference = (arr1, arr2) => {
-  const set2 = new Set(arr2);
-  return arr1.filter((x) => !set2.has(x));
-};
+/**
+ * Creates an array of unique values that is the [symmetric difference](https://en.wikipedia.org/wiki/Symmetric_difference)
+ * of the given arrays. The order of result values is determined by the order of the
+ * corresponding values in the `arrays`.
+ *
+ * @param {Array} arr1 The first array.
+ * @param {Array} arr2 The second array.
+ * @returns {Array} Returns the new array of filtered values.
+ */
+function difference(arr1, arr2) {
+  if (!Array.isArray(arr1)) {
+    return [];
+  }
+  if (!Array.isArray(arr2)) {
+    return [...new Set(arr1)]; // If arr2 is not an array, return unique elements of arr1
+  }
+
+  const s2 = new Set(arr2);
+  const result = arr1.filter(item => !s2.has(item));
+  return [...new Set(result)]; // Ensure unique values in the result
+}
+
+export { difference };
