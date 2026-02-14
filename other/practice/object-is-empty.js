5a9023889337d9971e4fd89b497709db825b4aa2
nao-United92 <naoyauver@icloud.com>
2026-02-14 10:42:36 +0900

    feat: Add and fix isEmptyObject utility function

--- other/practice/object-is-empty.js
+++ other/practice/object-is-empty.js
@@ -1,8 +1,8 @@
-const objectIsEmpty = (obj) => {
-  if (obj == null) {
-    return true;
+const isEmptyObject = (obj) => {
+  if (typeof obj !== 'object' || obj === null) {
+    return false; // Not an object or is null
   }
-  return Object.keys(obj).length === 0 && obj.constructor === Object;
+  return Object.keys(obj).length === 0 && (obj.constructor === Object || Object.getPrototypeOf(obj) === null);
 };
 
-module.exports = objectIsEmpty;
+module.exports = { isEmptyObject };
