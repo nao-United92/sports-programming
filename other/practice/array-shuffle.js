06e118997617ed6ea7a2c24ef42d102e11d761ee
nao-United92 <naoyauver@icloud.com>
2026-02-17 23:49:59 +0900

    feat: Add arrayShuffle utility function

--- other/practice/array-shuffle.js
+++ other/practice/array-shuffle.js
@@ -1,25 +1,14 @@
-function shuffle(arr) {
+const arrayShuffle = (arr) => {
   if (!Array.isArray(arr)) {
-    throw new Error('Argument must be an array.');
-  }
-  const shuffledArr = [...arr]; // Create a shallow copy to avoid modifying the original array
-  let currentIndex = shuffledArr.length;
-  let randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex !== 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [shuffledArr[currentIndex], shuffledArr[randomIndex]] = [
-      shuffledArr[randomIndex],
-      shuffledArr[currentIndex],
-    ];
+    throw new TypeError('Expected an array for the first argument.');
   }
 
+  const shuffledArr = [...arr];
+  for (let i = shuffledArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArr[i], shuffledArr[j]] = [shuffledArr[j], shuffledArr[i]];
+  }
   return shuffledArr;
-}
+};
 
-module.exports = shuffle;
+export default arrayShuffle;
