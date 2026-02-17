0f15f6c51521a54d9e3aa6cb1477a5f63c569672
nao-United92 <naoyauver@icloud.com>
2026-02-17 23:52:11 +0900

    feat: Add arrayPullAll utility function

--- other/practice/array-pull-all.js
+++ other/practice/array-pull-all.js
@@ -1,26 +1,16 @@
-/**
- * This function is like `pull` except that it accepts an array of values to remove.
- * Note: This function mutates `array`.
- *
- * @param {Array} arr The array to modify.
- * @param {Array} values The values to remove.
- * @returns {Array} Returns `array`.
- */
-function pullAll(arr, values) {
-  if (!Array.isArray(arr) || !Array.isArray(values)) {
-    return arr;
+const arrayPullAll = (arr, valuesToRemove) => {
+  if (!Array.isArray(arr) || !Array.isArray(valuesToRemove)) {
+    throw new TypeError('Expected both arguments to be arrays.');
   }
 
-  const valuesToRemove = new Set(values);
-  let i = 0;
-  while (i < arr.length) {
-    if (valuesToRemove.has(arr[i])) {
+  const valuesSet = new Set(valuesToRemove);
+  let i = arr.length;
+  while (i--) {
+    if (valuesSet.has(arr[i])) {
       arr.splice(i, 1);
-    } else {
-      i++;
     }
   }
   return arr;
-}
+};
 
-export { pullAll };
+export default arrayPullAll;
