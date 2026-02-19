8beec46d07f61db08b8565d15359ebabc00eaab1
nao-United92 <naoyauver@icloud.com>
2026-02-19 11:31:15 +0900

    Add string slugify utility

---
+++ other/practice/string-slugify-text.js
@@ -0,0 +1,12 @@
+
+const stringSlugifyText = (text) => {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')     // Replace spaces with -
+    .replace(/[^\w-]+/g, '')  // Remove all non-word chars
+    .replace(/--+/g, '-');    // Replace multiple - with single -
+};
+
+module.exports = stringSlugifyText;
