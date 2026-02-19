8f86239ef07f231bed27550b5c420d9d79ccd21b
nao-United92 <naoyauver@icloud.com>
2026-02-19 11:32:28 +0900

    Add object pick utility

---
+++ other/practice/object-pick-properties.js
@@ -0,0 +1,11 @@
+
+const objectPickProperties = (obj, keys) => {
+  return keys.reduce((acc, key) => {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      acc[key] = obj[key];
+    }
+    return acc;
+  }, {});
+};
+
+module.exports = objectPickProperties;
