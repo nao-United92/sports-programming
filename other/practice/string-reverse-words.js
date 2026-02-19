263049a98ce8dd6ffd6c2433630800da2adf2c0f
nao-United92 <naoyauver@icloud.com>
2026-02-19 11:32:02 +0900

    Add string reverse words utility

--- other/practice/string-reverse-words.js
+++ other/practice/string-reverse-words.js
@@ -1,12 +1,6 @@
-/**
- * Reverses the order of words in a string.
- * Words are separated by spaces.
- *
- * @param {string} str The string to process.
- * @returns {string} The string with words reversed.
- */
+
 const stringReverseWords = (str) => {
-  return str.split(' ').reverse().join(' ');
+  return str.split(/\s+/).reverse().join(' ');
 };
 
-export default stringReverseWords;
+module.exports = stringReverseWords;
