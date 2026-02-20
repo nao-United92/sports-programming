f04f2fc56d2ebcf8f4021f773029ca2776ec5ce1
nao-United92 <naoyauver@icloud.com>
2026-02-20 20:45:00 +0900

    Add unique and shuffle array utilities with tests

--- other/practice/array-shuffle.test.js
+++ other/practice/array-shuffle.test.js
@@ -1,12 +1,18 @@
-import { arrayShuffle } from './array-shuffle';
+import { shuffle } from './array-shuffle';
 
-test('arrayShuffle shuffles an array', () => {
-  const arr = [1, 2, 3, 4, 5];
-  const shuffled = arrayShuffle(arr);
-  expect(shuffled).toHaveLength(arr.length);
-  expect(shuffled).toEqual(expect.arrayContaining(arr));
-  // Probability of same order is low for large array, but for 5 elements (1/120), it might happen.
-  // Checking distinctness is tricky in deterministic tests without mocking random.
-  // We'll just check it returns a new array.
-  expect(shuffled).not.toBe(arr);
+describe('shuffle', () => {
+  test('shuffles the elements of an array', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const shuffled = shuffle(arr);
+    expect(shuffled).toHaveLength(arr.length);
+    expect(shuffled).toEqual(expect.arrayContaining(arr));
+    // Note: Technically it could be equal by chance, but very unlikely for 10 elements.
+    expect(shuffled).not.toEqual(arr);
+  });
+
+  test('returns a new array', () => {
+    const arr = [1, 2, 3];
+    const shuffled = shuffle(arr);
+    expect(shuffled).not.toBe(arr);
+  });
 });
