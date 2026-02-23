518c4c45503b8eb0a3ed82c1f64affe97a88c203
nao-United92 <naoyauver@icloud.com>
2026-02-23 18:53:49 +0900

    Add is empty utility and tests

---
+++ other/practice/is-empty.js
@@ -0,0 +1,7 @@
+export const isEmpty = (val) => {
+  if (val == null) return true;
+  if (typeof val === 'string' || Array.isArray(val)) return val.length === 0;
+  if (val instanceof Map || val instanceof Set) return val.size === 0;
+  if (typeof val === 'object') return Object.keys(val).length === 0;
+  return false;
+};
