c47bc790220fcbf30a151eaf76b26e05413d1546
nao-United92 <naoyauver@icloud.com>
2026-02-25 08:33:16 +0900

    Add string-slugify-utils.js and tests

--- other/practice/string-slugify-utils.js
+++ other/practice/string-slugify-utils.js
@@ -1,16 +1,9 @@
-const slugify = (str) => {
-  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
-  const p = new RegExp(a.split('').join('|'), 'g')
-
-  return str.toString().toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-\_]+/g, '-') // Replace all non-word chars except _ with -
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
-}
-
-module.exports = slugify;
+export const stringSlugify = (str) => {
+  if (typeof str !== 'string') return '';
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+};
--- other/practice/string-slugify-utils.test.js
+++ other/practice/string-slugify-utils.test.js
@@ -1,31 +1,15 @@
-const slugify = require('./string-slugify-utils');
+import { stringSlugify } from './string-slugify-utils';
 
-describe('slugify', () => {
-  test('should convert a simple string', () => {
-    expect(slugify('Hello World')).toBe('hello-world');
+describe('stringSlugify', () => {
+  test('slugifies a string', () => {
+    expect(stringSlugify('Hello World')).toBe('hello-world');
   });
 
-  test('should handle special characters', () => {
-    expect(slugify('àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż')).toBe('aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz');
+  test('removes special characters', () => {
+    expect(stringSlugify('Hello World!')).toBe('hello-world');
   });
 
-  test('should handle ampersands', () => {
-    expect(slugify('salt & pepper')).toBe('salt-and-pepper');
-  });
-
-  test('should replace non-word characters with hyphens', () => {
-    expect(slugify('foo@bar#baz')).toBe('foo-bar-baz');
-  });
-
-  test('should replace multiple hyphens with a single one', () => {
-    expect(slugify('foo--bar')).toBe('foo-bar');
-  });
-
-  test('should trim hyphens from the start and end', () => {
-    expect(slugify('-foo-bar-')).toBe('foo-bar');
-  });
-
-  test('should handle a complex string', () => {
-    expect(slugify('  A-B/C D_E&F!G?H*I J  ')).toBe('a-b-c-d_e-and-f-g-h-i-j');
+  test('replaces spaces with hyphens', () => {
+    expect(stringSlugify('Hello   World')).toBe('hello-world');
   });
 });
