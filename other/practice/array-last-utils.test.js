9310361a4c232cd250eea0935e81f7ecd55957d7
nao-United92 <naoyauver@icloud.com>
2026-02-25 08:40:52 +0900

    Add array-last-utils.js and tests

--- other/practice/array-last-utils.test.js
+++ other/practice/array-last-utils.test.js
@@ -1,22 +1,11 @@
-import { last } from './array-last-utils.js';
+import { arrayLast } from './array-last-utils';
 
-describe('last', () => {
-  it('should return the last element of an array', () => {
-    expect(last([1, 2, 3])).toBe(3);
+describe('arrayLast', () => {
+  test('returns the last element of an array', () => {
+    expect(arrayLast([1, 2, 3])).toBe(3);
   });
 
-  it('should return undefined for an empty array', () => {
-    expect(last([])).toBeUndefined();
-  });
-
-  it('should return undefined for a non-array input', () => {
-    expect(last(null)).toBeUndefined();
-    expect(last(undefined)).toBeUndefined();
-    expect(last("string")).toBeUndefined();
-    expect(last({})).toBeUndefined();
-  });
-
-  it('should return the only element for a single-element array', () => {
-    expect(last([10])).toBe(10);
+  test('returns undefined for empty array', () => {
+    expect(arrayLast([])).toBeUndefined();
   });
 });
