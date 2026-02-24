c51e7d1bdeef93043803fe5e1fe6c21f79445d69
nao-United92 <naoyauver@icloud.com>
2026-02-25 08:40:44 +0900

    Add array-first-utils.js and tests

--- other/practice/array-first-utils.test.js
+++ other/practice/array-first-utils.test.js
@@ -1,31 +1,15 @@
-// other/practice/array-first-utils.test.js
-
-const arrayFirst = require('./array-first-utils');
+import { arrayFirst } from './array-first-utils';
 
 describe('arrayFirst', () => {
-  test('should return the first element of an array', () => {
-    expect(arrayFirst([1, 2, 3, 4])).toBe(1);
+  test('returns the first element of an array', () => {
+    expect(arrayFirst([1, 2, 3])).toBe(1);
   });
 
-  test('should return undefined for an empty array', () => {
+  test('returns undefined for empty array', () => {
     expect(arrayFirst([])).toBeUndefined();
   });
 
-  test('should return the single element for a single-element array', () => {
-    expect(arrayFirst([10])).toBe(10);
-  });
-
-  test('should handle arrays with mixed types', () => {
-    const obj = { key: 'value' };
-    const arr = [obj, 'hello', 1, null];
-    expect(arrayFirst(arr)).toBe(obj);
-  });
-
-  test('should handle non-array input by returning undefined', () => {
+  test('returns undefined for null', () => {
     expect(arrayFirst(null)).toBeUndefined();
-    expect(arrayFirst(undefined)).toBeUndefined();
-    expect(arrayFirst(123)).toBeUndefined();
-    expect(arrayFirst('string')).toBeUndefined();
-    expect(arrayFirst({})).toBeUndefined();
   });
 });
