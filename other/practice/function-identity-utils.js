20d38cb5444d0cd2cff479af2ac9a0d2ba102d48
nao-United92 <naoyauver@icloud.com>
2026-02-25 08:36:17 +0900

    Add function-identity-utils.js and tests

--- other/practice/function-identity-utils.js
+++ other/practice/function-identity-utils.js
@@ -1,9 +1,3 @@
-/**
- * This method returns the first argument it receives.
- *
- * @param {*} value The value to return.
- * @returns {*} Returns `value`.
- */
-const identity = (value) => value;
-
-export { identity };
+export const functionIdentity = (value) => {
+  return value;
+};
