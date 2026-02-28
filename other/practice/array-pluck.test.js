3414f5a8f9d4fb731c69800d7818f6c640050813
nao-United92 <naoyauver@icloud.com>
2026-02-28 12:23:56 +0900

    Add array pluck utility function

--- other/practice/array-pluck.test.js
+++ other/practice/array-pluck.test.js
@@ -1,23 +1,10 @@
-import pluck from './array-pluck';
 
-describe('pluck', () => {
-  it('should return an array of values for the given key', () => {
-    const arr = [{ a: 1 }, { a: 2 }, { a: 3 }];
-    expect(pluck(arr, 'a')).toEqual([1, 2, 3]);
-  });
+const pluck = require('./array-pluck');
 
-  it('should return an array of undefined for a key that does not exist', () => {
-    const arr = [{ a: 1 }, { a: 2 }, { b: 3 }];
-    expect(pluck(arr, 'b')).toEqual([undefined, undefined, 3]);
-  });
-
-  it('should handle an empty array', () => {
-    const arr = [];
-    expect(pluck(arr, 'a')).toEqual([]);
-  });
-
-  it('should work with different keys', () => {
-    const arr = [{ name: 'Alice' }, { name: 'Bob' }];
-    expect(pluck(arr, 'name')).toEqual(['Alice', 'Bob']);
-  });
+test('pluck extracts property values from array of objects', () => {
+  const users = [
+    { user: 'barney', age: 36 },
+    { user: 'fred', age: 40 }
+  ];
+  expect(pluck(users, 'user')).toEqual(['barney', 'fred']);
 });
