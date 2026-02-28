452746249758790cebfb3e5e241f48c3640fe6eb
nao-United92 <naoyauver@icloud.com>
2026-02-28 12:33:30 +0900

    Add array sample size utility

--- other/practice/array-sample-size.js
+++ other/practice/array-sample-size.js
@@ -1,9 +1,10 @@
-const shuffleFisherYates = require('./array-shuffle-fisher-yates');
 
-const sampleSize = (array, n = 1) => {
-  if (!Array.isArray(array)) return [];
-  const shuffled = shuffleFisherYates(array);
-  return shuffled.slice(0, n);
+const sampleSize = ([...arr], n = 1) => {
+  let m = arr.length;
+  while (m) {
+    const i = Math.floor(Math.random() * m--);
+    [arr[m], arr[i]] = [arr[i], arr[m]];
+  }
+  return arr.slice(0, n);
 };
-
 module.exports = sampleSize;
--- other/practice/array-sample-size.test.js
+++ other/practice/array-sample-size.test.js
@@ -1,13 +1,9 @@
+
 const sampleSize = require('./array-sample-size');
 
-test('sampleSize gets N elements from array', () => {
-  const input = [1, 2, 3, 4, 5];
-  const result = sampleSize(input, 3);
-  expect(result).toHaveLength(3);
-  result.forEach(item => expect(input).toContain(item));
-});
-
-test('sampleSize returns up to array length', () => {
-  const input = [1, 2];
-  expect(sampleSize(input, 5)).toHaveLength(2);
+test('returns n random elements', () => {
+  const arr = [1, 2, 3, 4, 5];
+  const result = sampleSize(arr, 2);
+  expect(result).toHaveLength(2);
+  expect(arr).toEqual(expect.arrayContaining(result));
 });
