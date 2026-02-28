718d26970f1e90901bb7c3e502a9dcba8474b3fd
nao-United92 <naoyauver@icloud.com>
2026-02-28 12:24:14 +0900

    Add array shuffle utility

--- other/practice/array-shuffle-fisher-yates.test.js
+++ other/practice/array-shuffle-fisher-yates.test.js
@@ -1,14 +1,9 @@
-const shuffleFisherYates = require('./array-shuffle-fisher-yates');
 
-test('shuffleFisherYates shuffles an array', () => {
-  const input = [1, 2, 3, 4, 5];
-  const result = shuffleFisherYates(input);
-  expect(result).toHaveLength(input.length);
-  expect(result.sort()).toEqual(input.sort());
-});
+const shuffle = require('./array-shuffle-fisher-yates');
 
-test('shuffleFisherYates returns a new array', () => {
-  const input = [1, 2, 3];
-  const result = shuffleFisherYates(input);
-  expect(result).not.toBe(input);
+test('shuffle randomizes array order', () => {
+  const arr = [1, 2, 3, 4, 5];
+  const shuffled = shuffle(arr);
+  expect(shuffled).toHaveLength(arr.length);
+  expect(shuffled).toEqual(expect.arrayContaining(arr));
 });
