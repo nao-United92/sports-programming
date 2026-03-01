41d8d7e98a8dd960128940b2da6dee999c301df8
nao-United92 <naoyauver@icloud.com>
2026-03-01 09:50:50 +0900

    feat: add arrayFindLast utility

--- other/practice/array-find-last.js
+++ other/practice/array-find-last.js
@@ -1,16 +1,8 @@
-/**
- * Returns the last element in the array that satisfies the provided testing function.
- * If no elements satisfy the testing function, undefined is returned.
- *
- * @param {Array} arr - The array to search.
- * @param {Function} predicate - Function to execute on each value in the array.
- * @returns {*} The last element that passes the test, or undefined.
- */
-export const findLast = (arr, predicate) => {
+const arrayFindLast = (arr, fn) => {
   for (let i = arr.length - 1; i >= 0; i--) {
-    if (predicate(arr[i], i, arr)) {
-      return arr[i];
-    }
+    if (fn(arr[i], i, arr)) return arr[i];
   }
   return undefined;
 };
+
+module.exports = arrayFindLast;
