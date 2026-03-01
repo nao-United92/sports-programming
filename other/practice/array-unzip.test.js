b735c8a20f928e06099fcc3b2d09dc1215c9c8be
nao-United92 <naoyauver@icloud.com>
2026-03-01 09:28:11 +0900

    feat: add arrayUnzip utility

--- other/practice/array-unzip.test.js
+++ other/practice/array-unzip.test.js
@@ -1,10 +1,13 @@
+const arrayUnzip = require('./array-unzip');
 
-const unzip = require('./array-unzip');
+test('unzips grouped elements back into separate arrays', () => {
+  expect(arrayUnzip([['a', 1], ['b', 2], ['c', 3]])).toEqual([['a', 'b', 'c'], [1, 2, 3]]);
+});
 
-test('unzips array of arrays', () => {
-  expect(unzip([['a', 1, true], ['b', 2, false]])).toEqual([
-    ['a', 'b'],
-    [1, 2],
-    [true, false]
-  ]);
+test('handles empty arrays', () => {
+  expect(arrayUnzip([])).toEqual([]);
+});
+
+test('handles different inner array lengths', () => {
+  expect(arrayUnzip([['a', 1], ['b', 2, true]])).toEqual([['a', 'b'], [1, 2]]);
 });
