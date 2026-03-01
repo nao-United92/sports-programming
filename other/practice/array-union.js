4e5e9ed2c282f96ea92bdd89862cb509af77fae5
nao-United92 <naoyauver@icloud.com>
2026-03-01 09:51:15 +0900

    feat: add arrayUnion utility

--- other/practice/array-union.js
+++ other/practice/array-union.js
@@ -1,10 +1,3 @@
-/**
- * Returns an array of unique values, in order, from all given arrays.
- *
- * @param {Array} arr1 - The first array.
- * @param {Array} arr2 - The second array.
- * @returns {Array} The union of the two arrays.
- */
-export const union = (arr1, arr2) => {
-  return [...new Set([...arr1, ...arr2])];
-};
+const arrayUnion = (a, b) => Array.from(new Set([...a, ...b]));
+
+module.exports = arrayUnion;
