50592fb2a58796363598ddee4be5fb2f7df4d51c
nao-United92 <naoyauver@icloud.com>
2026-03-04 00:47:07 +0900

    Add array sample utility and its tests

--- other/practice/array-sample-simple.js
+++ other/practice/array-sample-simple.js
@@ -1,7 +1,10 @@
-
-const sample = (array) => {
+/**
+ * Gets a random element from an array.
+ * 
+ * @param {Array} array - The array to sample.
+ * @returns {*} Returns the random element.
+ */
+export const sample = (array) => {
   const length = array == null ? 0 : array.length;
   return length ? array[Math.floor(Math.random() * length)] : undefined;
 };
-
-module.exports = sample;
