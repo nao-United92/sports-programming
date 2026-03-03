e2c38f897e9756c71418587b887195ab0fdae8da
nao-United92 <naoyauver@icloud.com>
2026-03-04 00:39:45 +0900

    Add array union utility and its tests

--- other/practice/array-union-simple.js
+++ other/practice/array-union-simple.js
@@ -1,4 +1,9 @@
-
-const union = (a, b) => [...new Set([...a, ...b])];
-
-module.exports = union;
+/**
+ * Creates an array of unique values from all given arrays.
+ * 
+ * @param {...Array} arrays - The arrays to inspect.
+ * @returns {Array} A new array of combined unique values.
+ */
+export const union = (...arrays) => {
+  return [...new Set(arrays.flat())];
+};
