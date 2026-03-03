cd498c780ddef3e98acce5edf32603691fa734ae
nao-United92 <naoyauver@icloud.com>
2026-03-04 00:43:06 +0900

    Add array find-last utility and its tests

--- other/practice/array-find-last-simple.js
+++ other/practice/array-find-last-simple.js
@@ -1,5 +1,12 @@
-
-const findLast = (array, predicate) => {
+/**
+ * Iterates over elements of an array from right to left and returns the first 
+ * element that the predicate returns truthy for.
+ * 
+ * @param {Array} array - The array to inspect.
+ * @param {Function} predicate - The function invoked per iteration.
+ * @returns {*} Returns the matched element, else undefined.
+ */
+export const findLast = (array, predicate) => {
   for (let i = array.length - 1; i >= 0; i--) {
     if (predicate(array[i], i, array)) {
       return array[i];
@@ -7,5 +14,3 @@ const findLast = (array, predicate) => {
   }
   return undefined;
 };
-
-module.exports = findLast;
