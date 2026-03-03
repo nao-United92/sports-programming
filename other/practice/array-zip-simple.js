00982b2711e2f9bd3da53d05366e5b85d1aef439
nao-United92 <naoyauver@icloud.com>
2026-03-04 00:41:52 +0900

    Add array zip utility and its tests

--- other/practice/array-zip-simple.js
+++ other/practice/array-zip-simple.js
@@ -1,8 +1,13 @@
-
-const zip = (...arrays) => {
-  if (!arrays.length) return [];
-  const minLength = Math.min(...arrays.map((arr) => arr.length));
-  return Array.from({ length: minLength }, (_, i) => arrays.map((arr) => arr[i]));
+/**
+ * Creates an array of grouped elements, the first of which contains the first elements 
+ * of the given arrays, the second of which contains the second elements of the given arrays, and so on.
+ * 
+ * @param {...Array} arrays - The arrays to process.
+ * @returns {Array[]} A new array of grouped elements.
+ */
+export const zip = (...arrays) => {
+  const maxLength = Math.max(...arrays.map(arr => arr.length));
+  return Array.from({ length: maxLength }).map((_, i) => {
+    return arrays.map(arr => arr[i]);
+  });
 };
-
-module.exports = zip;
