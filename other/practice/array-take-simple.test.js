88edad26e2d466cfd04c0a75fdcd6dffece0b595
nao-United92 <naoyauver@icloud.com>
2026-03-04 00:43:39 +0900

    Add array take utility and its tests

--- other/practice/array-take-simple.test.js
+++ other/practice/array-take-simple.test.js
@@ -1,14 +1,23 @@
+import { take } from './array-take-simple';
 
-const take = require('./array-take-simple');
+describe('take', () => {
+  test('should take 1 element by default', () => {
+    expect(take([1, 2, 3])).toEqual([1]);
+  });
 
-test('take should return first n elements', () => {
-  expect(take([1, 2, 3], 2)).toEqual([1, 2]);
-});
+  test('should take n elements', () => {
+    expect(take([1, 2, 3], 2)).toEqual([1, 2]);
+  });
 
-test('take should return default 1 element if n is omitted', () => {
-  expect(take([1, 2, 3])).toEqual([1]);
-});
+  test('should return an empty array if n is 0', () => {
+    expect(take([1, 2, 3], 0)).toEqual([]);
+  });
 
-test('take should return all elements if n > length', () => {
-  expect(take([1, 2], 5)).toEqual([1, 2]);
+  test('should return the whole array if n > length', () => {
+    expect(take([1, 2, 3], 5)).toEqual([1, 2, 3]);
+  });
+
+  test('should handle negative n', () => {
+    expect(take([1, 2, 3], -1)).toEqual([]);
+  });
 });
