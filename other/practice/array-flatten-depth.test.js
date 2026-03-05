a09d2d69927c8fe7bc1d9c95c21ec3e1387b048d
nao-United92 <naoyauver@icloud.com>
2026-03-05 21:58:38 +0900

    Add array-flatten-depth utility and tests

--- other/practice/array-flatten-depth.test.js
+++ other/practice/array-flatten-depth.test.js
@@ -1,38 +1,22 @@
-import flattenDepth from './array-flatten-depth';
+const flattenDepth = require('./array-flatten-depth');
 
 describe('flattenDepth', () => {
-  test('should flatten an array to a specified depth', () => {
-    const arr = [1, [2, [3, [4]], 5], 6];
-    expect(flattenDepth(arr, 1)).toEqual([1, 2, [3, [4]], 5, 6]);
-    expect(flattenDepth(arr, 2)).toEqual([1, 2, 3, [4], 5, 6]);
-    expect(flattenDepth(arr, 3)).toEqual([1, 2, 3, 4, 5, 6]);
+  const arr = [1, [2, [3, [4]], 5]];
+
+  test('flattens array with default depth (1)', () => {
+    expect(flattenDepth(arr)).toEqual([1, 2, [3, [4]], 5]);
   });
 
-  test('should return the original array if depth is 0', () => {
-    const arr = [1, [2, [3]]];
-    expect(flattenDepth(arr, 0)).toEqual([1, [2, [3]]]);
+  test('flattens array with depth 2', () => {
+    expect(flattenDepth(arr, 2)).toEqual([1, 2, 3, [4], 5]);
   });
 
-  test('should flatten completely if depth is greater than array nesting', () => {
-    const arr = [1, [2, [3]]];
-    expect(flattenDepth(arr, 10)).toEqual([1, 2, 3]);
+  test('flattens array completely with large depth', () => {
+    expect(flattenDepth(arr, 10)).toEqual([1, 2, 3, 4, 5]);
   });
 
-  test('should handle empty arrays', () => {
-    expect(flattenDepth([], 1)).toEqual([]);
-    expect(flattenDepth([[]], 1)).toEqual([]);
-    expect(flattenDepth([[], []], 2)).toEqual([]);
-  });
-
-  test('should handle arrays with non-array elements', () => {
-    const arr = [1, 'a', null, undefined, {
-      key: 'value'
-    }, 5];
-    expect(flattenDepth(arr, 1)).toEqual(arr);
-  });
-
-  test('should default to depth 1 if depth is not provided', () => {
-    const arr = [1, [2, [3]]];
-    expect(flattenDepth(arr)).toEqual([1, 2, [3]]);
+  test('returns a shallow copy if depth is 0', () => {
+    expect(flattenDepth(arr, 0)).toEqual(arr);
+    expect(flattenDepth(arr, 0)).not.toBe(arr);
   });
 });
