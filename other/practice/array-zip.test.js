a8a8f6f6e67c3e4fa481ac8dbe49cefd2eabce33
nao-United92 <naoyauver@icloud.com>
2026-03-05 21:59:29 +0900

    Add array-zip utility and tests

--- other/practice/array-zip.test.js
+++ other/practice/array-zip.test.js
@@ -1,11 +1,21 @@
-import { zip } from './array-zip';
+const zip = require('./array-zip');
 
 describe('zip', () => {
   test('zips multiple arrays together', () => {
-    expect(zip(['a', 'b'], [1, 2], [true, false])).toEqual([['a', 1, true], ['b', 2, false]]);
+    expect(zip(['a', 'b'], [1, 2], [true, false])).toEqual([
+      ['a', 1, true],
+      ['b', 2, false],
+    ]);
   });
 
   test('handles arrays of different lengths', () => {
-    expect(zip(['a'], [1, 2])).toEqual([['a', 1], [undefined, 2]]);
+    expect(zip(['a', 'b'], [1])).toEqual([
+      ['a', 1],
+      ['b', undefined],
+    ]);
+  });
+
+  test('returns an empty array if no input', () => {
+    expect(zip()).toEqual([]);
   });
 });
