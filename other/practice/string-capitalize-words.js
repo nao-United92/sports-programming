02c3d52e7c0754051a7456400abf1a580d7e705b
nao-United92 <naoyauver@icloud.com>
2026-03-05 21:48:49 +0900

    Add string-capitalize-words utility and tests

--- other/practice/string-capitalize-words.js
+++ other/practice/string-capitalize-words.js
@@ -1,4 +1,11 @@
-export const stringCapitalizeWords = (str) => {
-  if (!str) return '';
-  return str.replace(/\b\w/g, (char) => char.toUpperCase());
-};
+/**
+ * Capitalizes the first letter of every word in a string.
+ * @param {string} str - The string to capitalize.
+ * @returns {string} - The capitalized string.
+ */
+function capitalizeWords(str) {
+  if (typeof str !== 'string') return '';
+  return str.replace(/\b[a-z]/g, (char) => char.toUpperCase());
+}
+
+module.exports = capitalizeWords;
