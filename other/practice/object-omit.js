c3be06a75a349bdfd012301c0c2fa55b9cf2c3e5
nao-United92 <naoyauver@icloud.com>
2026-03-05 21:49:19 +0900

    Add object-omit utility and tests

--- other/practice/object-omit.js
+++ other/practice/object-omit.js
@@ -1,8 +1,16 @@
-export const omit = (obj, keys) => {
+/**
+ * Creates an object composed of the object properties that are not omitted.
+ * @param {Object} obj - The source object.
+ * @param {Array} keys - The property keys to omit.
+ * @returns {Object} - The resulting object.
+ */
+function omit(obj, keys) {
   if (!obj) return {};
   const result = { ...obj };
   keys.forEach((key) => {
     delete result[key];
   });
   return result;
-};
+}
+
+module.exports = omit;
