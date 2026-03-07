02a9d18682f5435ef00dd3e882f30762f4d73d1c
nao-United92 <naoyauver@icloud.com>
2026-03-07 23:43:30 +0900

    Add math is-prime-utils and tests

--- other/practice/math-is-prime-utils.test.js
+++ other/practice/math-is-prime-utils.test.js
@@ -1,15 +1,31 @@
-import { mathIsPrime } from './math-is-prime-utils';
+const isPrime = require('./math-is-prime-utils');
 
-describe('mathIsPrime', () => {
+describe('isPrime', () => {
   test('returns true for prime numbers', () => {
-    expect(mathIsPrime(2)).toBe(true);
-    expect(mathIsPrime(3)).toBe(true);
-    expect(mathIsPrime(17)).toBe(true);
+    expect(isPrime(2)).toBe(true);
+    expect(isPrime(3)).toBe(true);
+    expect(isPrime(5)).toBe(true);
+    expect(isPrime(7)).toBe(true);
+    expect(isPrime(11)).toBe(true);
+    expect(isPrime(13)).toBe(true);
+    expect(isPrime(17)).toBe(true);
+    expect(isPrime(19)).toBe(true);
+    expect(isPrime(23)).toBe(true);
   });
 
   test('returns false for non-prime numbers', () => {
-    expect(mathIsPrime(1)).toBe(false);
-    expect(mathIsPrime(4)).toBe(false);
-    expect(mathIsPrime(100)).toBe(false);
+    expect(isPrime(1)).toBe(false);
+    expect(isPrime(4)).toBe(false);
+    expect(isPrime(6)).toBe(false);
+    expect(isPrime(8)).toBe(false);
+    expect(isPrime(9)).toBe(false);
+    expect(isPrime(10)).toBe(false);
+    expect(isPrime(15)).toBe(false);
+    expect(isPrime(21)).toBe(false);
+    expect(isPrime(25)).toBe(false);
+  });
+
+  test('returns false for negative numbers', () => {
+    expect(isPrime(-7)).toBe(false);
   });
 });
