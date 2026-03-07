c1cbf4a59abeb9ce4c8b12fabf8cd8dfa4cfc3d2
nao-United92 <naoyauver@icloud.com>
2026-03-07 23:41:33 +0900

    Add math clamp-number utility and tests

--- other/practice/math-clamp-number.js
+++ other/practice/math-clamp-number.js
@@ -1,6 +1,13 @@
-
-const mathClampNumber = (num, min, max) => {
+/**
+ * Clamps a number between a minimum and maximum value.
+ * 
+ * @param {number} num - The number to clamp.
+ * @param {number} min - The lower boundary.
+ * @param {number} max - The upper boundary.
+ * @returns {number} The clamped value.
+ */
+function clampNumber(num, min, max) {
   return Math.min(Math.max(num, min), max);
-};
+}
 
-module.exports = mathClampNumber;
+module.exports = clampNumber;
