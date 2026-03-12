c9e3d91624d5a36b41c17cfdf3eb4bf275d55f08
nao-United92 <naoyauver@icloud.com>
2026-03-12 22:01:57 +0900

    Add average utility

--- other/practice/math-average-util.js
+++ other/practice/math-average-util.js
@@ -1,5 +1,6 @@
-export const average = (...nums) => {
-  const numbers = nums.flat();
-  if (numbers.length === 0) return 0;
-  return numbers.reduce((acc, val) => acc + val, 0) / numbers.length;
+const average = (...args) => {
+  if (args.length === 0) return 0;
+  return args.reduce((acc, val) => acc + val, 0) / args.length;
 };
+
+module.exports = average;
--- other/practice/math-average-util.test.js
+++ other/practice/math-average-util.test.js
@@ -1,20 +1,10 @@
-import { average } from './math-average-util';
+const average = require('./math-average-util');
 
 describe('average', () => {
-  test('calculates the average of numbers', () => {
-    expect(average(1, 2, 3, 4, 5)).toBe(3);
+  test('calculates average of numbers', () => {
+    expect(average(1, 2, 3)).toBe(2);
   });
-
-  test('handles array input (flat)', () => {
-    expect(average([10, 20, 30])).toBe(20);
-  });
-
-  test('returns 0 for empty input', () => {
+  test('returns 0 for no arguments', () => {
     expect(average()).toBe(0);
-    expect(average([])).toBe(0);
-  });
-
-  test('handles negative numbers', () => {
-    expect(average(-10, 10)).toBe(0);
   });
 });
