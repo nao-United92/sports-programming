2af08430ad0c38acb4703d0f64f8421a1aaffa65
nao-United92 <naoyauver@icloud.com>
2026-03-13 18:12:29 +0900

    Add array-group-by method and tests

--- other/practice/array-group-by.js
+++ other/practice/array-group-by.js
@@ -1,19 +1,6 @@
-const arrayGroupBy = (arr, keyFn) => {
-  if (!Array.isArray(arr)) {
-    throw new TypeError('Expected an array for the first argument.');
-  }
-  if (typeof keyFn !== 'function' && typeof keyFn !== 'string') {
-    throw new TypeError('Expected a function or a string for the second argument.');
-  }
-
-  return arr.reduce((acc, item) => {
-    const key = typeof keyFn === 'function' ? keyFn(item) : item[keyFn];
-    if (!acc[key]) {
-      acc[key] = [];
-    }
-    acc[key].push(item);
+export const groupBy = (arr, fn) =>
+  arr.reduce((acc, val) => {
+    const key = typeof fn === 'function' ? fn(val) : val[fn];
+    (acc[key] || (acc[key] = [])).push(val);
     return acc;
   }, {});
-};
-
-export default arrayGroupBy;
