45a3cf4a4ece12618860bee52b604de421f8fd0b
nao-United92 <naoyauver@icloud.com>
2026-03-13 18:16:15 +0900

    Add array-symmetric-difference method and tests

--- other/practice/array-symmetric-difference.js
+++ other/practice/array-symmetric-difference.js
@@ -1,7 +1,8 @@
-const arraySymmetricDifference = (a, b) => {
-  const sA = new Set(a);
-  const sB = new Set(b);
-  return [...a.filter(x => !sB.has(x)), ...b.filter(x => !sA.has(x))];
+export const symmetricDifference = (arr1, arr2) => {
+  const s1 = new Set(arr1);
+  const s2 = new Set(arr2);
+  return [
+    ...arr1.filter((v) => !s2.has(v)),
+    ...arr2.filter((v) => !s1.has(v)),
+  ];
 };
-
-module.exports = arraySymmetricDifference;
--- other/practice/array-symmetric-difference.test.js
+++ other/practice/array-symmetric-difference.test.js
@@ -1,9 +1,19 @@
-const arraySymmetricDifference = require('./array-symmetric-difference');
+import { symmetricDifference } from './array-symmetric-difference.js';
 
-test('returns the symmetric difference of two arrays', () => {
-  expect(arraySymmetricDifference([1, 2, 3], [1, 2, 4])).toEqual([3, 4]);
-});
+describe('symmetricDifference', () => {
+  test('returns the symmetric difference of two arrays', () => {
+    expect(symmetricDifference([1, 2, 3], [1, 2, 4])).toEqual([3, 4]);
+  });
 
-test('handles no symmetric difference', () => {
-  expect(arraySymmetricDifference([1, 2], [1, 2])).toEqual([]);
+  test('returns the union of two arrays if they are disjoint', () => {
+    expect(symmetricDifference([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
+  });
+
+  test('returns an empty array if both arrays are identical', () => {
+    expect(symmetricDifference([1, 2], [1, 2])).toEqual([]);
+  });
+
+  test('works with duplicate values', () => {
+    expect(symmetricDifference([1, 2, 2], [1, 3])).toEqual([2, 2, 3]);
+  });
 });
