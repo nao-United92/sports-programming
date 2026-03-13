8e4af8d3a4aa1aade4bf8f2d676319cf16ebecc5
nao-United92 <naoyauver@icloud.com>
2026-03-13 18:08:43 +0900

    Add array-tail method and tests

--- other/practice/array-tail.test.js
+++ other/practice/array-tail.test.js
@@ -1,15 +1,21 @@
-import { tail } from './array-tail';
+import { tail } from './array-tail.js';
 
 describe('tail', () => {
-  test('returns all but the first element of an array', () => {
+  it('returns all elements of an array except the first', () => {
     expect(tail([1, 2, 3])).toEqual([2, 3]);
+    expect(tail(['a', 'b', 'c'])).toEqual(['b', 'c']);
   });
 
-  test('returns an empty array for an empty array', () => {
+  it('returns an empty array for an array with one element', () => {
+    expect(tail([1])).toEqual([]);
+  });
+
+  it('returns an empty array for an empty array', () => {
     expect(tail([])).toEqual([]);
   });
 
-  test('returns an empty array for a single element array', () => {
-    expect(tail([1])).toEqual([]);
+  it('returns an empty array for null or undefined', () => {
+    expect(tail(null)).toEqual([]);
+    expect(tail(undefined)).toEqual([]);
   });
 });
