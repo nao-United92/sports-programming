1b3cce01265ce0ffd79e945c154010216dbe4b0e
nao-United92 <naoyauver@icloud.com>
2026-03-13 18:09:20 +0900

    Add array-last method and tests

--- other/practice/array-last.js
+++ other/practice/array-last.js
@@ -1,10 +1,1 @@
-/**
- * Gets the last element of an array.
- *
- * @param {Array} arr - The array to query.
- * @returns {*} The last element of the array.
- */
-export const last = (arr) => {
-  const length = arr === null ? 0 : arr.length;
-  return length ? arr[length - 1] : undefined;
-};
+export const last = (arr) => (arr && arr.length > 0 ? arr[arr.length - 1] : undefined);
