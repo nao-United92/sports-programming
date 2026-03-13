efc00fe88dc3767a0c7233c022d01a88ed2f55e6
nao-United92 <naoyauver@icloud.com>
2026-03-13 18:08:13 +0900

    Add array-head method and tests

--- other/practice/array-head.js
+++ other/practice/array-head.js
@@ -1,11 +1,1 @@
-
-/**
- * Gets the first element of array.
- * @param {Array} array The array to query.
- * @returns {*} Returns the first element of array.
- */
-function head(array) {
-  return (array && array.length) ? array[0] : undefined;
-}
-
-module.exports = head;
+export const head = (arr) => (arr && arr.length > 0 ? arr[0] : undefined);
