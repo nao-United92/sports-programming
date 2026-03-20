7e290b19361cb696a3bb3718f382772ddcd5617e
nao-United92 <naoyauver@icloud.com>
2026-03-20 10:46:17 +0900

    feat: add math-is-odd utility

--- other/practice/math-is-odd.js
+++ other/practice/math-is-odd.js
@@ -1,7 +1,1 @@
-/**
- * Checks if a number is odd.
- *
- * @param {number} n - The number to check.
- * @returns {boolean} True if odd, false otherwise.
- */
-export const isOdd = (n) => n % 2 !== 0;
+export const isOdd = (num) => num % 2 !== 0;
