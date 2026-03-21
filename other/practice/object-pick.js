d09351d6801ffff1030e80c39dd6c8912c4f1691
nao-United92 <naoyauver@icloud.com>
2026-03-21 12:05:49 +0900

    Add object-pick helper

--- other/practice/object-pick.js
+++ other/practice/object-pick.js
@@ -1,16 +1,9 @@
-/**
- * Creates an object composed of the picked object properties.
- * @param {Object} obj - The source object.
- * @param {Array} keys - The property keys to pick.
- * @returns {Object} - The resulting object.
- */
-function pick(obj, keys) {
+const pick = (obj, keys) => {
   return keys.reduce((acc, key) => {
     if (obj && Object.prototype.hasOwnProperty.call(obj, key)) {
       acc[key] = obj[key];
     }
     return acc;
   }, {});
-}
-
+};
 module.exports = pick;
