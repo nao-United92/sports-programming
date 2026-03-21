265679f29f1cb5c021caecc09d96f8d3d671cf11
nao-United92 <naoyauver@icloud.com>
2026-03-21 12:05:43 +0900

    Add math-is-prime helper

--- other/practice/math-is-prime.js
+++ other/practice/math-is-prime.js
@@ -1,16 +1,10 @@
-/**
- * Checks if a number is prime.
- * @param {number} num - The number to check.
- * @returns {boolean} - Returns true if the number is prime, else false.
- */
-function isPrime(num) {
-  if (num <= 1) return false;
-  if (num <= 3) return true;
-  if (num % 2 === 0 || num % 3 === 0) return false;
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
+const isPrime = (n) => {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
   }
   return true;
-}
-
+};
 module.exports = isPrime;
--- other/practice/math-is-prime.test.js
+++ other/practice/math-is-prime.test.js
@@ -1,33 +1,6 @@
-const isPrime = require('./math-is-prime');
-
-describe('isPrime', () => {
-  test('identifies prime numbers', () => {
-    expect(isPrime(2)).toBe(true);
-    expect(isPrime(3)).toBe(true);
-    expect(isPrime(5)).toBe(true);
-    expect(isPrime(7)).toBe(true);
-    expect(isPrime(11)).toBe(true);
-    expect(isPrime(13)).toBe(true);
-    expect(isPrime(17)).toBe(true);
-    expect(isPrime(19)).toBe(true);
-    expect(isPrime(23)).toBe(true);
-  });
-
-  test('identifies non-prime numbers', () => {
-    expect(isPrime(1)).toBe(false);
-    expect(isPrime(4)).toBe(false);
-    expect(isPrime(6)).toBe(false);
-    expect(isPrime(8)).toBe(false);
-    expect(isPrime(9)).toBe(false);
-    expect(isPrime(10)).toBe(false);
-    expect(isPrime(15)).toBe(false);
-    expect(isPrime(21)).toBe(false);
-    expect(isPrime(25)).toBe(false);
-  });
-
-  test('handles zero and negative numbers', () => {
-    expect(isPrime(0)).toBe(false);
-    expect(isPrime(-1)).toBe(false);
-    expect(isPrime(-7)).toBe(false);
-  });
+const isPrime = require("./math-is-prime");
+test("isPrime checks primality", () => {
+  expect(isPrime(7)).toBe(true);
+  expect(isPrime(4)).toBe(false);
+  expect(isPrime(1)).toBe(false);
 });
