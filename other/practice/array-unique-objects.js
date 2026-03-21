8bd1d2df193d87100434edcb093f3fa0f276a073
nao-United92 <naoyauver@icloud.com>
2026-03-21 12:03:13 +0900

    Add array-unique-objects helper

---
+++ other/practice/array-unique-objects.js
@@ -0,0 +1,10 @@
+const uniqueObjects = (arr, key) => {
+  const seen = new Set();
+  return arr.filter(item => {
+    const val = item[key];
+    if (seen.has(val)) return false;
+    seen.add(val);
+    return true;
+  });
+};
+module.exports = uniqueObjects;
