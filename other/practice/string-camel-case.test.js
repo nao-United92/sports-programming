0d7e34d789f6b07d08b4ebd0480c987c95671247
nao-United92 <naoyauver@icloud.com>
2026-03-21 12:08:30 +0900

    Add string-camel-case helper

--- other/practice/string-camel-case.test.js
+++ other/practice/string-camel-case.test.js
@@ -1,20 +1,5 @@
-import { camelCase } from './string-camel-case';
-
-describe('camelCase', () => {
-  test('converts space-separated words to camelCase', () => {
-    expect(camelCase('hello world')).toBe('helloWorld');
-    expect(camelCase('foo bar baz')).toBe('fooBarBaz');
-  });
-
-  test('handles single word', () => {
-    expect(camelCase('hello')).toBe('hello');
-  });
-
-  test('capitalizes existing camelCase correctly (simple case)', () => {
-    expect(camelCase('Hello World')).toBe('helloWorld');
-  });
-
-  test('returns empty string for non-string input', () => {
-    expect(camelCase(null)).toBe('');
-  });
+const camelCase = require("./string-camel-case");
+test("camelCase converts string", () => {
+  expect(camelCase("Foo Bar")).toBe("fooBar");
+  expect(camelCase("--foo-bar--")).toBe("fooBar"); 
 });
