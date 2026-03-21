2e13e69a71b7e80f9dfb9c3fd60b92822c51fa0a
nao-United92 <naoyauver@icloud.com>
2026-03-21 12:04:47 +0900

    Add string-truncate helper

--- other/practice/string-truncate.js
+++ other/practice/string-truncate.js
@@ -1,15 +1,5 @@
-/**
- * Truncates a string if it's longer than the given maximum string length.
- * The last characters of the truncated string are replaced with the omission string which defaults to "...".
- * @param {string} str - The string to truncate.
- * @param {number} length - The maximum string length.
- * @param {string} [omission='...'] - The string to indicate omission.
- * @returns {string} - The truncated string.
- */
-function truncate(str, length, omission = '...') {
-  if (typeof str !== 'string') return '';
+const truncate = (str, length, ending = "...") => {
   if (str.length <= length) return str;
-  return str.slice(0, length - omission.length) + omission;
-}
-
+  return str.substring(0, length - ending.length) + ending;
+};
 module.exports = truncate;
