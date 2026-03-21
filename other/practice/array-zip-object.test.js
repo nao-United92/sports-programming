c09d6c6517248988fc4e553dea1825e644a21b33
nao-United92 <naoyauver@icloud.com>
2026-03-21 12:03:15 +0900

    Add array-zip-object helper

--- other/practice/array-zip-object.test.js
+++ other/practice/array-zip-object.test.js
@@ -1,13 +1,4 @@
-const arrayZipObject = require('./array-zip-object');
-
-test('creates an object from arrays of properties and values', () => {
-  expect(arrayZipObject(['a', 'b', 'c'], [1, 2, 3])).toEqual({ a: 1, b: 2, c: 3 });
-});
-
-test('handles arrays of different lengths (values shorter)', () => {
-  expect(arrayZipObject(['a', 'b', 'c'], [1, 2])).toEqual({ a: 1, b: 2, c: undefined });
-});
-
-test('handles arrays of different lengths (props shorter)', () => {
-  expect(arrayZipObject(['a', 'b'], [1, 2, 3])).toEqual({ a: 1, b: 2 });
+const zipObject = require("./array-zip-object");
+test("zipObject creates object from keys and values", () => {
+  expect(zipObject(["a", "b"], [1, 2])).toEqual({ a: 1, b: 2 });
 });
