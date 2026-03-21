c02805a202fb17ade433188fa3d93b0d406a922e
nao-United92 <naoyauver@icloud.com>
2026-03-21 12:03:30 +0900

    Add array-sorted-index helper

--- other/practice/array-sorted-index.test.js
+++ other/practice/array-sorted-index.test.js
@@ -1,17 +1,5 @@
-const arraySortedIndex = require('./array-sorted-index');
-
-test('returns the lowest index where value should be inserted', () => {
-  expect(arraySortedIndex([30, 50], 40)).toBe(1);
-});
-
-test('handles descending order', () => {
-  expect(arraySortedIndex([50, 30], 40)).toBe(1);
-});
-
-test('handles value smaller than all elements', () => {
-  expect(arraySortedIndex([30, 50], 10)).toBe(0);
-});
-
-test('handles value larger than all elements', () => {
-  expect(arraySortedIndex([30, 50], 60)).toBe(2);
+const sortedIndex = require("./array-sorted-index");
+test("sortedIndex finds insertion index", () => {
+  expect(sortedIndex([30, 50], 40)).toBe(1);
+  expect(sortedIndex([10, 20, 30], 40)).toBe(3);
 });
