b3dff324b38c3597ccad062099ef09c401bf5427
nao-United92 <naoyauver@icloud.com>
2026-03-21 12:08:34 +0900

    Add string-kebab-case helper

--- other/practice/string-kebab-case.test.js
+++ other/practice/string-kebab-case.test.js
@@ -1,19 +1,5 @@
-import { kebabCase } from './string-kebab-case';
-
-describe('kebabCase', () => {
-  test('converts space-separated words to kebab-case', () => {
-    expect(kebabCase('hello world')).toBe('hello-world');
-  });
-
-  test('converts camelCase to kebab-case', () => {
-    expect(kebabCase('helloWorld')).toBe('hello-world');
-  });
-
-  test('handles multiple spaces and special characters (simple)', () => {
-    expect(kebabCase('hello  world')).toBe('hello-world');
-  });
-
-  test('returns empty string for non-string input', () => {
-    expect(kebabCase(null)).toBe('');
-  });
+const kebabCase = require("./string-kebab-case");
+test("kebabCase converts string", () => {
+  expect(kebabCase("fooBar")).toBe("foo-bar");
+  expect(kebabCase("Foo Bar")).toBe("foo-bar");
 });
