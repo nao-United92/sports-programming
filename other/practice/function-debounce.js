5937f9bd56a86caaf5372a53dfde621a72e20ac0
nao-United92 <naoyauver@icloud.com>
2026-03-24 00:16:16 +0900

    Add function-debounce utility

--- other/practice/function-debounce.js
+++ other/practice/function-debounce.js
@@ -1,8 +1,12 @@
-const debounce = (fn, ms) => {
-  let timeoutId;
-  return function(...args) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+// Debounce function
+export const debounce = (func, wait) => {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
   };
 };
-module.exports = debounce;
