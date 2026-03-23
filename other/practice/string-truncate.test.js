a526460ccffdaba9e3fa42bc7e87ea768968452d
nao-United92 <naoyauver@icloud.com>
2026-03-24 00:20:16 +0900

    Add string-truncate utility

--- other/practice/string-truncate.test.js
+++ other/practice/string-truncate.test.js
@@ -1,5 +1,19 @@
-const truncate = require("./string-truncate");
-test("truncate shortens string", () => {
-  expect(truncate("hello world", 8)).toBe("hello...");
-  expect(truncate("hello", 10)).toBe("hello");
+import { truncate } from './string-truncate.js';
+
+describe('truncate', () => {
+  test('should truncate string to specified length', () => {
+    expect(truncate('hello world', 5)).toBe('hello...');
+  });
+
+  test('should not truncate if string is shorter than length', () => {
+    expect(truncate('hello', 10)).toBe('hello');
+  });
+
+  test('should use custom omission', () => {
+    expect(truncate('hello world', 5, '!')).toBe('hello!');
+  });
+  
+  test('should handle empty string', () => {
+    expect(truncate('', 5)).toBe('');
+  });
 });
