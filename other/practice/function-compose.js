55ae0d8e0e66be4448bed6163ee72e0d44bb8e86
nao-United92 <naoyauver@icloud.com>
2026-03-24 00:12:37 +0900

    Add function-compose utility

--- other/practice/function-compose.js
+++ other/practice/function-compose.js
@@ -1,3 +1,4 @@
-const compose = (...fns) => (x) => 
-  fns.reduceRight((y, f) => f(y), x);
-module.exports = compose;
+// Performs right-to-left function composition
+export const compose = (...fns) => {
+  return (x) => fns.reduceRight((v, f) => f(v), x);
+};
--- other/practice/function-compose.test.js
+++ other/practice/function-compose.test.js
@@ -1,6 +1,20 @@
-const compose = require("./function-compose");
-test("compose composes functions right-to-left", () => {
-  const add1 = x => x + 1;
-  const double = x => x * 2;
-  expect(compose(add1, double)(2)).toBe(5); // (2*2)+1
+import { compose } from './function-compose.js';
+
+describe('compose', () => {
+  test('should compose functions right to left', () => {
+    const add5 = (x) => x + 5;
+    const multiply2 = (x) => x * 2;
+    const composed = compose(add5, multiply2);
+    expect(composed(5)).toBe(15); // (5 * 2) + 5 = 15
+  });
+
+  test('should return identity for no functions', () => {
+    const composed = compose();
+    expect(composed(5)).toBe(5);
+  });
+  
+  test('should work with a single function', () => {
+      const add1 = x => x + 1;
+      expect(compose(add1)(1)).toBe(2);
+  });
 });
