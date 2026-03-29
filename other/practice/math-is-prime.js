4f13e72be8b247a193af7cf1d5811a4157cfe42e
nao-United92 <naoyauver@icloud.com>
2026-03-29 09:19:28 +0900

    add math-is-prime utility and tests

--- other/practice/math-is-prime.js
+++ other/practice/math-is-prime.js
@@ -1,10 +1,6 @@
-const isPrime = (n) => {
-  if (n <= 1) return false;
-  if (n <= 3) return true;
-  if (n % 2 === 0 || n % 3 === 0) return false;
-  for (let i = 5; i * i <= n; i += 6) {
-    if (n % i === 0 || n % (i + 2) === 0) return false;
-  }
-  return true;
+const isPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++) if (num % i === 0) return false;
+  return num > 1;
 };
-module.exports = isPrime;
+
+export { isPrime };
