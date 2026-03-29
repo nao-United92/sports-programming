4065797f5c85e7ed249b5de4d2b1cf8e8037f4a3
nao-United92 <naoyauver@icloud.com>
2026-03-29 09:19:10 +0900

    add object-pick utility and tests

--- other/practice/object-pick.test.js
+++ other/practice/object-pick.test.js
@@ -1,18 +1,6 @@
 import { pick } from './object-pick.js';
-
 describe('pick', () => {
-  test('should pick specified keys from object', () => {
-    const obj = { a: 1, b: 2, c: 3 };
-    expect(pick(obj, ['a', 'c'])).toEqual({ a: 1, c: 3 });
-  });
-
-  test('should return empty object if input is invalid', () => {
-    expect(pick(null, ['a'])).toEqual({});
-    expect(pick(undefined, ['a'])).toEqual({});
-  });
-
-  test('should ignore keys not present in object', () => {
-    const obj = { a: 1 };
-    expect(pick(obj, ['b'])).toEqual({});
+  it('should pick specified keys', () => {
+    expect(pick({ a: 1, b: 2, c: 3 }, ['a', 'c'])).toEqual({ a: 1, c: 3 });
   });
 });
