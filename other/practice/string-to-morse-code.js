2117095d296d297aedc04be880df4b9549076852
nao-United92 <naoyauver@icloud.com>
2026-04-03 13:39:47 +0900

    Add Morse code conversion utilities and tests

---
+++ other/practice/string-to-morse-code.js
@@ -0,0 +1,20 @@
+const morseCodeMap = {
+  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+  'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+  'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+  'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+  '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+  '8': '---..', '9': '----.', ' ': ' '
+};
+
+/**
+ * Converts a string to Morse code.
+ * @param {string} str - The string to convert.
+ * @returns {string} The Morse code representation.
+ */
+function stringToMorseCode(str) {
+  return str.toUpperCase().split('').map(char => morseCodeMap[char] || '').join(' ').trim();
+}
+
+module.exports = stringToMorseCode;
