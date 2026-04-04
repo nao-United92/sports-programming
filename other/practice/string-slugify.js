e8707b8ca00eee8211a8262438e53160e4d0cf5e
nao-United92 <naoyauver@icloud.com>
2026-04-04 12:45:26 +0900

    Implement string-slugify utility and its tests

--- other/practice/string-slugify.js
+++ other/practice/string-slugify.js
@@ -1,9 +1,16 @@
-const slugify = (str) => {
+/**
+ * Converts a string into a URL-safe slug.
+ * 
+ * @param {string} str - The string to slugify.
+ * @returns {string} The slugified string.
+ */
+function slugify(str) {
   return str
     .toLowerCase()
     .trim()
-    .replace(/[^\w\s-]/g, "")
-    .replace(/[\s_-]+/g, "-")
-    .replace(/^-+|-+$/g, "");
-};
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+}
+
 module.exports = slugify;
