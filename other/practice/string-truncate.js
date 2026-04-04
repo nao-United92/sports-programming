ad435a3a5c07261c9e0b6673316007d43007a847
nao-United92 <naoyauver@icloud.com>
2026-04-04 12:46:42 +0900

    Implement string-truncate utility and its tests

--- other/practice/string-truncate.js
+++ other/practice/string-truncate.js
@@ -1,3 +1,16 @@
-const truncate = (str, len) => str.length > len ? str.slice(0, len) + '...' : str;
+/**
+ * Truncates a string to a specified length and adds an ellipsis.
+ * 
+ * @param {string} str - The string to truncate.
+ * @param {number} length - The maximum length of the string including ellipsis.
+ * @param {string} [ending='...'] - The string to append to the end.
+ * @returns {string} The truncated string.
+ */
+function truncate(str, length, ending = '...') {
+  if (str.length <= length) {
+    return str;
+  }
+  return str.slice(0, length - ending.length) + ending;
+}
 
-export { truncate };
+module.exports = truncate;
