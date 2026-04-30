8c8903171fc2e4caf3725236b548bef59e69cc79
nao-United92 <naoyauver@icloud.com>
2026-04-30 12:23:01 +0900

    Add array union utility

--- other/practice/array-union.js
+++ other/practice/array-union.js
@@ -1,1 +1,11 @@
-export const union = (...args) => [...new Set(args.flat())];
+/**
+ * Returns an array of unique values that are in one or more of the provided arrays.
+ * 
+ * @param {...Array} arrays - The arrays to inspect.
+ * @returns {Array} The new array of combined unique values.
+ */
+function union(...arrays) {
+  return [...new Set(arrays.flat())];
+}
+
+module.exports = union;
--- other/practice/array-union.test.js
+++ other/practice/array-union.test.js
@@ -1,15 +1,19 @@
-import { union } from './array-union.js';
+const union = require('./array-union');
 
 describe('union', () => {
-  it('returns the union of two arrays', () => {
-    expect(union([1, 2, 3], [4, 3, 2])).toEqual([1, 2, 3, 4]);
+  test('should return unique values from multiple arrays', () => {
+    expect(union([2, 1], [2, 3])).toEqual([2, 1, 3]);
   });
 
-  it('returns the union of multiple arrays', () => {
+  test('should handle more than two arrays', () => {
     expect(union([1, 2], [2, 3], [3, 4])).toEqual([1, 2, 3, 4]);
   });
 
-  it('handles empty arrays', () => {
-    expect(union([], [1, 2])).toEqual([1, 2]);
+  test('should return an empty array if no arrays are provided', () => {
+    expect(union()).toEqual([]);
+  });
+
+  test('should handle empty arrays', () => {
+    expect(union([], [1, 2], [])).toEqual([1, 2]);
   });
 });
